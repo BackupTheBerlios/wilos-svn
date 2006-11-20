f665ef5d64f7f6f0e9d762cf9a3aaf135f57d579
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-20 13:44:10 +0000

    Rajout de la notion de processus.

---
+++ woops2/trunk/src/woops2/model/process/Process.java
@@ -0,0 +1,21 @@
+package woops2.model.process;
+
+import woops2.model.activity.Activity;
+
+/**
+ * @author deder
+ * 
+ * A Process is a special Activity that describes a structure for particular
+ * types of development projects. To perform such a development project a
+ * Processes would be 'instantiated' and adapted for the specific situation.
+ * Process is an abstract class and this meta-model defines different special
+ * types of Processes for different process management applications and
+ * different situations of process reuse.
+ * 
+ */
+public class Process extends Activity {
+
+	public Process() {
+		super();
+	}
+}
