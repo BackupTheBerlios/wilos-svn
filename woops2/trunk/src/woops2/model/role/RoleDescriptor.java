c91d41491ccd49c9dc8437297136f5d6ad78f458
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-21 18:55:53 +0000

    --mise a jour de RoleDescriptor

--- woops2/trunk/src/woops2/model/role/RoleDescriptor.java
+++ woops2/trunk/src/woops2/model/role/RoleDescriptor.java
@@ -3,6 +3,9 @@ package woops2.model.role ;
 
 import java.util.Set;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 import woops2.model.breakdownelement.BreakdownElement;
 import woops2.model.task.TaskDescriptor;
 
@@ -88,5 +91,149 @@ public class RoleDescriptor extends BreakdownElement {
 	 */
 	public void setAdditionalTasks(Set<TaskDescriptor> _additionalTasks) {
 		this.additionalTasks = _additionalTasks ;
+	}
+	
+	/**
+	 * Indicates whether another object is "equal to" this one.
+	 * 
+	 * @return true if equal else false
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof RoleDescriptor == false) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		RoleDescriptor role = (RoleDescriptor) obj;
+		return new EqualsBuilder().appendSuper(super.equals(role)).append(
+				this.roleDefinition, role.roleDefinition).isEquals();
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return a hash code
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.append(this.getName()).toHashCode();
+	}
+
+	/**
+	 * relation between RoleDescriptor and RoleDefinition
+	 */
+
+	/**
+	 * add a RoleDefinition to the Set
+	 * 
+	 * @param _role
+	 */
+	public void addToRoleDefinition(RoleDefinition _role) {
+		this.roleDefinition = _role;
+		_role.getRoleDescriptors().add(this);
+	}
+
+	/**
+	 * remove a RoleDefinition
+	 * 
+	 * @param _role
+	 */
+	public void removeFromRoleDefinition(RoleDefinition _role) {
+		this.roleDefinition = null;
+		_role.getRoleDescriptors().remove(this);
+	}
+
+	/**
+	 * relation mainrole
+	 */
+
+	/**
+	 * add a taskDescritor to the Set
+	 * 
+	 * @param _task
+	 */
+	public void addTaskDescriptor(TaskDescriptor _task) {
+		this.primaryTasks.add(_task);
+		_task.setMainRole(this);
+	}
+
+	/**
+	 * remove a taskDescriptor
+	 * 
+	 * @param _task
+	 */
+	public void removeTaskDescriptor(TaskDescriptor _task) {
+		_task.setMainRole(null);
+		this.primaryTasks.remove(_task);
+	}
+
+	/**
+	 * Remove from an RoleDescriptor all its primaryTask
+	 * 
+	 */
+	public void removeAllPrimaryTasks() {
+		for (TaskDescriptor _task : this.primaryTasks) {
+			_task.removeFromRoleDescriptor(this);
+		}
+		this.primaryTasks.clear();
+
+	}
+
+	/**
+	 * Add a RoleDescriptor collection to the RoleDescriptor collection of an
+	 * RoleDefinition
+	 * 
+	 * @param _role
+	 */
+	public void addToAllTaskDescriptors(Set<TaskDescriptor> _role) {
+		for (TaskDescriptor _role1 : _role) {
+			_role1.addToRoleDescriptor(this);
+		}
+	}
+
+	/**
+	 * relation additionnalTask
+	 */
+
+	/**
+	 * add a TaskDescriptor to the Set
+	 * 
+	 * @param _task
+	 */
+	public void addToTaskDescriptor(TaskDescriptor _task) {
+		this.additionalTasks.add(_task);
+		_task.getAdditionalRoles().add(this);
+	}
+
+	/**
+	 * remove a TaskDescriptor
+	 * 
+	 * @param _task
+	 */
+	public void removeFromTaskDescriptor(TaskDescriptor _task) {
+		_task.getAdditionalRoles().remove(this);
+		this.additionalTasks.remove(_task);
+	}
+
+	/**
+	 * remove all the roleDescriptor
+	 * 
+	 */
+	public void removeAllAdditionalTasks() {
+		for (TaskDescriptor _task : this.additionalTasks) {
+			_task.removeFromRoleDescriptor(this);
+		}
+		this.additionalTasks.clear();
+	}
+
+	/**
+	 * Remove from an RoleDescriptor all its AdditionalTask
+	 * 
+	 */
+	public void removeFromAllTaskDescriptor() {
+		for (TaskDescriptor _task : this.additionalTasks) {
+			this.removeFromTaskDescriptor(_task);
+		}
 	}
 }
