7aee908671a6b941b03ea7c5aadbe9086e9e3d64
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-21 18:03:10 +0000

    [Woops2] corrections sur l'appli test <-> applicationContext <=> Bean Factory

--- woops2/trunk/applitest/woops2/application/console/HibernateSpringConsoleTest.java
+++ woops2/trunk/applitest/woops2/application/console/HibernateSpringConsoleTest.java
@@ -1,8 +1,7 @@
 package woops2.application.console;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import woops2.hibernate.activity.ActivityDao;
 import woops2.model.activity.Activity;
@@ -16,15 +15,14 @@ public class HibernateSpringConsoleTest {
 	
 	public static void main(String[] args) {
 		// Getback the application context from the spring configuration file
-		Resource res = new FileSystemResource("src/applicationContext.xml");
-		XmlBeanFactory factory = new XmlBeanFactory(res);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// Show what is in the factory
-		System.out.println("factory => "+factory);
+		System.out.println("factory => "+ctx);
 		// Getback the hibernateTemplate bean
-		org.springframework.orm.hibernate3.HibernateTemplate hibTempl = (org.springframework.orm.hibernate3.HibernateTemplate) factory.getBean("hibernateTemplate");
+		org.springframework.orm.hibernate3.HibernateTemplate hibTempl = (org.springframework.orm.hibernate3.HibernateTemplate) ctx.getBean("hibernateTemplate");
 		System.out.println("HibTemplate => "+hibTempl);
 		// Get the ActivityDao Singleton for managing Activity data
-		ActivityDao dao = (ActivityDao) factory.getBean("ActivityDao");
+		ActivityDao dao = (ActivityDao) ctx.getBean("ActivityDao");
 		// Create empty Activity
 		Activity a = new Activity();
 		// Save it
