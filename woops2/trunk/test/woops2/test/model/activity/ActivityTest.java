3a0edc58f449987eaa5d5f7f2ac4937b5b841f3f
themorpheus <themorpheus@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-21 14:35:31 +0000

    [Woops2] corection activity set vs list

--- woops2/trunk/test/woops2/test/model/activity/ActivityTest.java
+++ woops2/trunk/test/woops2/test/model/activity/ActivityTest.java
@@ -1,8 +1,12 @@
-package woops2.test.model.activity;
 
-import junit.framework.TestCase;
-import woops2.model.activity.Activity;
-import woops2.model.breakdownelement.BreakdownElement;
+package woops2.test.model.activity ;
+
+import java.util.ArrayList ;
+import java.util.List ;
+
+import junit.framework.TestCase ;
+import woops2.model.activity.Activity ;
+import woops2.model.breakdownelement.BreakdownElement ;
 
 /**
  * @author morpheus
@@ -12,23 +16,134 @@ import woops2.model.breakdownelement.BreakdownElement;
  */
 public class ActivityTest extends TestCase {
 
-	public static final String PREFIX = "prefix";
+	private Activity activity ;
 
-	public static final Boolean IS_OPTIONAL = true;
+	public static final String PREFIX = "prefix" ;
 
+	public static final Boolean IS_OPTIONAL = true ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp() ;
+		this.activity = new Activity() ;
+		this.activity.setPrefix(PREFIX) ;
+		this.activity.setIsOptional(IS_OPTIONAL) ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+	}
+
+	/**
+	 * 
+	 * TODO Method description
+	 * 
+	 */
+	public void testEquals() {
+
+		Activity tmp = new Activity() ;
+		tmp.setPrefix(PREFIX) ;
+		tmp.setIsOptional(IS_OPTIONAL) ;
+
+		assertTrue(this.activity.equals(tmp)) ;
+	}
+
+	/**
+	 * 
+	 * TODO Method description
+	 * 
+	 */
 	public void testAddToBreakdownElement() {
 
-		Activity activity = new Activity();
-		activity.setPrefix(PREFIX);
-		activity.setIsOptional(IS_OPTIONAL);
+		BreakdownElement breakdownElement = new BreakdownElement() ;
+		breakdownElement.setPrefix(PREFIX) ;
+		breakdownElement.setIsOptional(IS_OPTIONAL) ;
 
-		BreakdownElement breakdownElement = new BreakdownElement();
-		breakdownElement.setPrefix(PREFIX);
-		breakdownElement.setIsOptional(IS_OPTIONAL);
+		this.activity.addToBreakdownElement(breakdownElement) ;
 
-		activity.addToBreakdownElement(breakdownElement);
+		assertFalse(this.activity.getBreakDownElements().isEmpty()) ;
+		assertFalse(breakdownElement.getSuperActivities().isEmpty()) ;
+		assertTrue(this.activity.getBreakDownElements().size() == 1) ;
+		assertTrue(breakdownElement.getSuperActivities().size() == 1) ;
+	}
 
-		assertTrue(activity.getBreakDownElements().size() != 0);
-		assertTrue(breakdownElement.getSuperActivities().size() != 0);
+	/**
+	 * 
+	 * TODO Method description
+	 * 
+	 */
+	public void testAddToAllBreakdownElements() {
+
+		BreakdownElement breakdownElement = new BreakdownElement() ;
+		breakdownElement.setPrefix(PREFIX) ;
+		breakdownElement.setIsOptional(IS_OPTIONAL) ;
+
+		BreakdownElement tmp = new BreakdownElement() ;
+		tmp.setPrefix(PREFIX) ;
+		tmp.setIsOptional(IS_OPTIONAL) ;
+
+		List<BreakdownElement> list = new ArrayList<BreakdownElement>() ;
+		list.add(breakdownElement) ;
+		list.add(tmp) ;
+
+		//this.activity.addToAllBreakdownElement(list) ;
+
+		assertFalse(this.activity.getBreakDownElements().isEmpty()) ;
+		assertTrue(this.activity.getBreakDownElements().size() == 2);
+		assertFalse(breakdownElement.getSuperActivities().isEmpty()) ;
+		assertTrue(breakdownElement.getSuperActivities().size() == 1);
+		assertFalse(tmp.getSuperActivities().isEmpty()) ;
+		assertTrue(tmp.getSuperActivities().size() == 1);
+	}
+
+	/**
+	 * 
+	 * TODO Method description
+	 * 
+	 */
+	public void testRemoveFromBreakdownElement() {
+
+		BreakdownElement breakdownElement = new BreakdownElement() ;
+		breakdownElement.setPrefix(PREFIX) ;
+		breakdownElement.setIsOptional(IS_OPTIONAL) ;
+
+		this.activity.addToBreakdownElement(breakdownElement) ;
+		this.activity.removeFromBreakdownElement(breakdownElement) ;
+
+		assertTrue(this.activity.getBreakDownElements().isEmpty()) ;
+		assertTrue(breakdownElement.getSuperActivities().isEmpty()) ;
+	}
+
+	/**
+	 * 
+	 * TODO Method description
+	 * 
+	 */
+	public void testRemoveFromAllBreakdownElements() {
+
+		BreakdownElement breakdownElement = new BreakdownElement() ;
+		breakdownElement.setPrefix(PREFIX) ;
+		breakdownElement.setIsOptional(IS_OPTIONAL) ;
+
+		BreakdownElement tmp = new BreakdownElement() ;
+		tmp.setPrefix(PREFIX) ;
+		tmp.setIsOptional(IS_OPTIONAL) ;
+
+		this.activity.addToBreakdownElement(breakdownElement) ;
+		this.activity.addToBreakdownElement(tmp) ;
+		this.activity.removeFromAllBreakdownElements() ;
+
+		assertTrue(this.activity.getBreakDownElements().isEmpty()) ;
+		assertTrue(breakdownElement.getSuperActivities().isEmpty()) ;
+		assertTrue(tmp.getSuperActivities().isEmpty()) ;
 	}
 }
