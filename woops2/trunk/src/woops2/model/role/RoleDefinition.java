15f6cbed5cfa71d1f4307b76bc37d29da4f27965
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-21 18:17:35 +0000

    - mise a jour du modele pour RoleDefinition

--- woops2/trunk/src/woops2/model/role/RoleDefinition.java
+++ woops2/trunk/src/woops2/model/role/RoleDefinition.java
@@ -3,10 +3,14 @@ package woops2.model.role;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 import woops2.model.element.Element;
 
 /**
  * @author deder
+ * @author soosuske
  * 
  * A RoleDefinition is a content element that defines a set of related skills,
  * competencies, and responsibilities. Roles are used by Tasks to define who
@@ -45,7 +49,82 @@ public class RoleDefinition extends Element {
 	 * @param _roleDescriptors
 	 *            The roleDescriptors to set.
 	 */
-	public void setRoleDescriptors(Set<RoleDescriptor> _roleDescriptors) {
+	private void setRoleDescriptors(Set<RoleDescriptor> _roleDescriptors) {
 		this.roleDescriptors = _roleDescriptors;
+	}
+
+	/**
+	 * Indicates whether another object is "equal to" this one.
+	 * 
+	 * @return true if equal else false
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof RoleDefinition == false) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		RoleDefinition role = (RoleDefinition) obj;
+		return new EqualsBuilder().appendSuper(super.equals(role)).append(
+				this.roleDescriptors, role.roleDescriptors).isEquals();
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return a hash code
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.append(this.getName()).toHashCode();
+	}
+
+	/**
+	 * relation between RoleDefinition and RoleDescriptor
+	 */
+	/**
+	 * add a roleDescriptor to the set
+	 * 
+	 * @param _role
+	 */
+	public void addRoleDescriptor(RoleDescriptor _role) {
+		this.roleDescriptors.add(_role);
+		_role.setRoleDefinition(this);
+	}
+
+	/**
+	 * remove a RoleDescriptor
+	 * 
+	 * @param _role
+	 */
+	public void removeRoleDescriptor(RoleDescriptor _role) {
+		_role.setRoleDefinition(null);
+		this.roleDescriptors.remove(_role);
+
+	}
+
+	/**
+	 * Remove from an RoleDefinition all its RoleDescriptor
+	 * 
+	 */
+	public void removeAllRoleDescriptor() {
+
+		for (RoleDescriptor _role : this.roleDescriptors) {
+			_role.removeFromRoleDefinition(this);
+		}
+		this.roleDescriptors.clear();
+	}
+
+	/**
+	 * Add a RoleDescriptor collection to the RoleDescriptor collection of an
+	 * RoleDefinition
+	 * 
+	 * @param _role
+	 */
+	public void addToAllRoleDescriptors(Set<RoleDescriptor> _role) {
+		for (RoleDescriptor _role1 : _role) {
+			_role1.addToRoleDefinition(this);
+		}
 	}
 }
