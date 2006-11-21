3f64f4bd4397319fe56c1b7310b74e17d477e089
eperico <eperico@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-21 22:01:32 +0000

    ajout des classes test TestDefinition et TestDescriptor

--- woops2/trunk/test/woops2/test/model/task/AllTests.java
+++ woops2/trunk/test/woops2/test/model/task/AllTests.java
@@ -5,16 +5,18 @@ import junit.framework.TestSuite;
 
 /**
  * @author Sebastien.
- *
+ * 
  */
 public class AllTests {
 
-	public static Test suite () {
-		TestSuite suite = new TestSuite("Test for woops2.test.model.task") ;
-		//$JUnit-BEGIN$
-		suite.addTestSuite(StepTest.class) ;
-		//$JUnit-END$
-		return suite ;
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for woops2.test.model.task");
+		// $JUnit-BEGIN$
+		suite.addTestSuite(StepTest.class);
+		suite.addTestSuite(TaskDescriptorTest.class);
+		suite.addTestSuite(TaskDefinitionTest.class);
+		// $JUnit-END$
+		return suite;
 	}
 
 }
