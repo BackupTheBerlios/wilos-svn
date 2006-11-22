8d4a5918f125075f666ce0c521a5ea161a73755b
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-22 13:11:23 +0000

    [Woops2] Ajout des tests sur wbde.

---
+++ woops2/trunk/test/woops2/test/model/workbreakdownelement/WorkBreakdownElementTest.java
@@ -0,0 +1,77 @@
+package woops2.test.model.workbreakdownelement;
+
+import junit.framework.TestCase;
+import woops2.model.workbreakdownelement.WorkBreakdownElement;
+
+/**
+ * @author deder
+ * 
+ */
+public class WorkBreakdownElementTest extends TestCase {
+
+	private WorkBreakdownElement workBreakdownElement;
+
+	public static final String PREFIX = "prefix";
+
+	public static final Boolean IS_OPTIONAL = true;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.workBreakdownElement = new WorkBreakdownElement();
+		this.workBreakdownElement.setPrefix("prefix");
+		this.workBreakdownElement.setIsOptional(IS_OPTIONAL);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for
+	 * {@link woops2.model.workbreakdownelement.WorkBreakdownElement#hashCode()}.
+	 */
+	public final void testHashCode() {
+		// Rk: the setUp method is called here.
+
+		assertNotNull(this.workBreakdownElement.hashCode());
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for
+	 * {@link woops2.model.workbreakdownelement.WorkBreakdownElement#equals(java.lang.Object)}.
+	 */
+	public final void testEqualsObject() {
+		// Rk: the setUp method is called here.
+
+		// Assert if it's equal by references.
+		assertTrue("By references", this.workBreakdownElement
+				.equals(this.workBreakdownElement));
+
+		// Assert if it's equal field by field.
+		WorkBreakdownElement bdeTmp1 = new WorkBreakdownElement();
+		bdeTmp1.setPrefix(PREFIX);
+		bdeTmp1.setIsOptional(IS_OPTIONAL);
+		assertTrue("Field by field", this.workBreakdownElement.equals(bdeTmp1));
+
+		// Assert if it's not equal.
+		WorkBreakdownElement bdeTmp2 = new WorkBreakdownElement();
+		bdeTmp2.setPrefix("prefixFalse");
+		bdeTmp2.setIsOptional(true);
+		assertFalse("Not equals", this.workBreakdownElement.equals(bdeTmp2));
+
+		// Rk: the tearDown method is called here.
+	}
+
+}
