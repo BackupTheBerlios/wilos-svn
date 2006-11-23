af3403287f855c250fd8275f4804853dd8adec52
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-23 19:41:29 +0000

    [Woops2]Upload branche Woops2 - Ajax

---
+++ woops2/branches/Woops2Ajax/trunk/test/woops2/test/hibernate/activity/ActivityDaoTest.java
@@ -0,0 +1,182 @@
+
+package woops2.test.hibernate.activity ;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import woops2.hibernate.activity.ActivityDao;
+import woops2.model.activity.Activity;
+import woops2.test.TestConfiguration;
+
+/**
+ * Unit test for ActivityDao
+ * 
+ * @author deder
+ * @author garwind
+ */
+public class ActivityDaoTest extends TestCase {
+	
+	private ActivityDao activityDao = null ;
+
+	private Activity activity = null ;
+
+	public static final String NAME = "thisActivity" ;
+
+	public static final String DESCRIPTION = "activity description" ;
+
+	public static final String PREFIX = "prefix" ;
+
+	public static final Boolean IS_OPTIONAL = true ;
+
+	public static final Boolean HAS_MULTIPLE_OCCURENCES = true ;
+
+	public static final Boolean IS_EVEN_DRIVEN = true ;
+
+	public static final Boolean IS_ON_GOING = true ;
+
+	public static final Boolean IS_PLANNED = true ;
+
+	public static final Boolean IS_REPEATABLE = true ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@ Override
+	protected void setUp() throws Exception {
+		super.setUp() ;
+
+		// Get the ActivityDao Singleton for managing Activity data
+		this.activityDao = (ActivityDao) TestConfiguration.getInstance().getApplicationContext().getBean("ActivityDao") ;
+
+		// Create empty Activity
+		this.activity = new Activity() ;
+
+		// Save the activity with the method to test.
+		this.activityDao.saveOrUpdateActivity(this.activity) ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@ Override
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+
+		// Delete the tmp activity from the database.
+		try{
+			this.activityDao.deleteActivity(this.activity) ;
+		}
+		catch(Exception exception){
+			System.err.println("exception e ="+exception);
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link woops2.hibernate.activity.ActivityDao#saveOrUpdateActivity(woops2.model.activity.Activity)}.
+	 * 
+	 * PRINCIPLE Create a tmp activity, save it into the database with the method to test. Then look
+	 * for the database to check if this tmp activity exists. To finish delete this tmp activity
+	 * from the database.
+	 */
+	public void testSaveOrUpdateActivity() {
+		// Rk: the setUp method is called here.
+
+
+		// Check the saving.
+		String id = this.activity.getId() ;
+		Activity activityTmp = (Activity) this.activityDao.getActivity(id) ;
+		assertNotNull(activityTmp) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link woops2.hibernate.activity.ActivityDao#getAllActivities()}.
+	 * 
+	 * PRINCIPLE Create a tmp activity, save it into the database. Then get all activities from the
+	 * database with the method to test, and look if the size of the activities set got is >= 1. To
+	 * finish delete this tmp activity from the database.
+	 */
+	public void testGetAllActivities() {
+
+		// Look if this activity is also into the database and look if the size of the set is >= 1.
+		List<Activity> activities = this.activityDao.getAllActivities() ;
+		assertNotNull(activities) ;
+		assertTrue(activities.size() >= 1) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link woops2.hibernate.activity.ActivityDao#getActivity()}.
+	 * 
+	 */
+	public void testGetActivity() {
+		// Rk: the setUp method is called here.
+
+		// Add properties to the activity.
+		this.activity.setName(NAME) ;
+		this.activity.setDescription(DESCRIPTION) ;
+		this.activity.setPrefix(PREFIX) ;
+		this.activity.setHasMultipleOccurrences(HAS_MULTIPLE_OCCURENCES) ;
+		this.activity.setIsEvenDriven(IS_EVEN_DRIVEN) ;
+		this.activity.setIsOngoing(IS_ON_GOING) ;
+		this.activity.setIsOptional(IS_OPTIONAL) ;
+		this.activity.setIsPlanned(IS_PLANNED) ;
+		this.activity.setIsRepeatable(IS_REPEATABLE) ;
+
+		// Save the activity into the database.
+		this.activityDao.saveOrUpdateActivity(this.activity) ;
+		String id = this.activity.getId() ;
+
+		// Test the method getActivity with an existing activity.
+		Activity activityTmp = this.activityDao.getActivity(id) ;
+		assertNotNull(activityTmp) ;
+		assertEquals("Name", activityTmp.getName(), NAME) ;
+		assertEquals("Description", activityTmp.getDescription(), DESCRIPTION) ;
+		assertEquals("Prefix", activityTmp.getPrefix(), PREFIX) ;
+		assertEquals("HasMultipleOccurences", activityTmp.getHasMultipleOccurrences(), HAS_MULTIPLE_OCCURENCES) ;
+		assertEquals("IsEvenDriven", activityTmp.getIsEvenDriven(), IS_EVEN_DRIVEN) ;
+		assertEquals("IsOnGoing", activityTmp.getIsOngoing(), IS_ON_GOING) ;
+		assertEquals("IsOptional", activityTmp.getIsOptional(), IS_OPTIONAL) ;
+		assertEquals("IsPlanned", activityTmp.getIsPlanned(), IS_PLANNED) ;
+		assertEquals("IsRepeatale", activityTmp.getIsRepeatable(), IS_REPEATABLE) ;
+
+		// Test the method getActivity with an unexisting activity.
+		this.activityDao.deleteActivity(this.activity) ;
+		activityTmp = this.activityDao.getActivity(id) ;
+		assertNull(activityTmp) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link woops2.hibernate.activity.ActivityDao#deleteActivity()}.
+	 * 
+	 */
+	public void testDeleteActivity() {
+		// Rk: the setUp method is called here.
+
+		// Save the activity into the database.
+		this.activityDao.saveOrUpdateActivity(this.activity) ;
+		String id = this.activity.getId() ;
+
+		// Test the method deleteActivity with an activity existing into the db.
+		this.activityDao.deleteActivity(this.activity) ;
+
+		// See if this.activity is now absent in the db.
+		Activity activityTmp = (Activity) this.activityDao.getActivity(id) ;
+		assertNull(activityTmp) ;
+
+		// Test the method deleteActivity with an activity unexisting into the db.
+		// Normally here there are no exception thrown.
+		this.activityDao.deleteActivity(this.activity) ;
+
+		// Rk: the tearDown method is called here.
+	}
+}
---
+++ woops2/branches/Woops2Ajax/trunk/test/woops2/test/hibernate/task/StepDaoTest.java
@@ -0,0 +1,141 @@
+
+package woops2.test.hibernate.task ;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import woops2.hibernate.task.StepDao;
+import woops2.model.task.Step;
+import woops2.model.task.TaskDefinition;
+import woops2.test.TestConfiguration;
+
+/**
+ * Test for DAO That manage Step objets
+ * 
+ * @author garwind
+ * 
+ */
+public class StepDaoTest extends TestCase {
+	
+	private StepDao stepDao = null ;
+
+	private Step step = null ;
+
+	/**
+	 * attributes from Element
+	 */
+	public static final String ID = "thisId" ;
+
+	public static final String NAME = "thisStep" ;
+
+	public static final String DESCRIPTION = "step" ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@ Override
+	protected void setUp() throws Exception {
+		super.setUp() ;
+
+		// Get the TaskDefinitionDao Singleton for managing TaskDefinition data
+		this.stepDao = (StepDao) TestConfiguration.getInstance().getApplicationContext().getBean("StepDao") ;
+
+		// Create empty TaskDefinition
+		this.step = new Step() ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@ Override
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+
+		// Delete the tmp task from the database.
+		try{
+			this.stepDao.getHibernateTemplate().delete(this.step) ;
+		}
+		catch(Exception exception){
+			exception.printStackTrace() ;
+		}
+	}
+
+	public void testSaveOrUpdateTask() {
+		// Rk: the setUp method is called here.
+
+		// Save the task with the method to test.
+		this.stepDao.saveOrUpdateStep(this.step) ;
+
+		// Check the saving.
+		String id = step.getId() ;
+		TaskDefinition taskTmp = (TaskDefinition) this.stepDao.getHibernateTemplate().load(TaskDefinition.class, id) ;
+		assertNotNull(taskTmp) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	public void testGetAllTasks() {
+		// Rk: the setUp method is called here.
+
+		// Save the task into the database.
+		this.stepDao.getHibernateTemplate().saveOrUpdate(this.step) ;
+
+		// Look if this task is also into the database and look if the size of
+		// the set is >= 1.
+		List<Step> steps = this.stepDao.getAllSteps() ;
+		assertNotNull(steps) ;
+		assertTrue(steps.size() >= 1) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	public void testGetTask() {
+		// Rk: the setUp method is called here.
+
+		// Add prooperties to the task.
+		this.step.setName(NAME) ;
+		this.step.setDescription(DESCRIPTION) ;
+
+		// Save the task into the database.
+		this.stepDao.getHibernateTemplate().saveOrUpdate(this.step) ;
+		String id = this.step.getId() ;
+
+		// Test the method getTask with an existing task.
+		Step sectionTmp = this.stepDao.getStep(id) ;
+		assertNotNull(sectionTmp) ;
+		assertEquals("Name", sectionTmp.getName(), NAME) ;
+		assertEquals("Description", sectionTmp.getDescription(), DESCRIPTION) ;
+
+		// Test the method getTask with an unexisting task.
+		this.stepDao.getHibernateTemplate().delete(step) ;
+		sectionTmp = this.stepDao.getStep(id) ;
+		assertNull(sectionTmp) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	public void testDeleteTask() {
+		// Rk: the setUp method is called here.
+
+		// Save the task into the database.
+		this.stepDao.getHibernateTemplate().saveOrUpdate(this.step) ;
+		String id = this.step.getId() ;
+
+		// Test the method deleteTask with an acitivity existing into the db.
+		this.stepDao.deleteStep(this.step) ;
+
+		// See if this.task is now absent in the db.
+		TaskDefinition taskTmp = (TaskDefinition) this.stepDao.getHibernateTemplate().get(TaskDefinition.class, id) ;
+		assertNull(taskTmp) ;
+
+		// Test the method deleteTask with a task unexisting into the db.
+		// Normally here there are no exception thrown.
+		this.stepDao.deleteStep(this.step) ;
+
+		// Rk: the tearDown method is called here.
+	}
+}
