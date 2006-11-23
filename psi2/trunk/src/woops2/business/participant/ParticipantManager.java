80bf7cd9faa970c91d79d29dafd56c510ed98b36
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-23 14:19:04 +0000

    [PSI2] Ajout de loginExist

--- psi2/trunk/src/woops2/business/participant/ParticipantManager.java
+++ psi2/trunk/src/woops2/business/participant/ParticipantManager.java
@@ -1,62 +1,72 @@
-package woops2.business.participant;
-import java.util.Set;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+package woops2.business.participant ;
 
-import woops2.hibernate.participant.ParticipantDao;
-import woops2.model.participant.Participant;
-import woops2.model.role.RoleDescriptor;
+import java.util.Set ;
+
+import org.apache.commons.logging.Log ;
+import org.apache.commons.logging.LogFactory ;
+import org.springframework.transaction.annotation.Propagation ;
+import org.springframework.transaction.annotation.Transactional ;
+
+import woops2.hibernate.participant.ParticipantDao ;
+import woops2.model.participant.Participant ;
+import woops2.model.role.RoleDescriptor ;
 
 /**
+ * This class represents ... TODO
+ * 
  * @author BlackMilk
  * @author Mikamikaze
  * @author Sakamakak
- *
- * This class represents ... TODO
- *
  */
-@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+@ Transactional (readOnly = false, propagation = Propagation.REQUIRED)
 public class ParticipantManager {
 
 	private ParticipantDao participantDao ;
-	
+
 	protected final Log logger = LogFactory.getLog(this.getClass()) ;
-	
+
 	/**
-	 * Return participant list
+	 * Return participant roles
 	 * 
-	 * @return
+	 * @return the roles 
 	 */
-	@Transactional(readOnly = true)
+	@ Transactional (readOnly = true)
 	public Set<RoleDescriptor> getRolesList() {
 		return this.participantDao.getAllRoles();
 	}
+
 	
 	/**
-	 * Function that make some test on transactionnal lazy loadings
+	 * Check if the login is already used
 	 *
-	 
-	public void Test(){
-		Activity a = this.participantDao.getActivityFromPrefix("test");
-		if (a == null){
-			a = new Activity();
-			a.setPrefix("test");
-			this.activityDao.saveOrUpdateActivity(a);
-			BreakdownElement b = new BreakdownElement();
-			this.activityDao.getHibernateTemplate().save(b);
-			a.getBreakDownElements().add(b);
-			this.activityDao.saveOrUpdateActivity(a);
+	 * @param _login
+	 * @return True is the login is already present 
+	 */
+	public boolean loginExist(String _login) {
+		boolean found = false;
+		Set<Participant> setParticipant = this.participantDao.getAllParticipants() ;
+		for (Participant parti : setParticipant) {
+			if(parti.getLogin().equals(_login)){
+				found = true;
+			}	
 		}
-		List<BreakdownElement> liste = new ArrayList<BreakdownElement>(a.getBreakDownElements());
-		logger.debug("### ActivityManager - TEST ###  liste size = "+liste.size());
-		for (BreakdownElement b : liste){
-			System.out.println("b="+b);
-		}
-	}*/
-	
+		return found;
+	}
+
+	/**
+	 * Function that make some test on transactionnal lazy loadings
+	 * 
+	 * 
+	 * public void Test(){ Activity a = this.participantDao.getActivityFromPrefix("test"); if (a ==
+	 * null){ a = new Activity(); a.setPrefix("test"); this.activityDao.saveOrUpdateActivity(a);
+	 * BreakdownElement b = new BreakdownElement(); this.activityDao.getHibernateTemplate().save(b);
+	 * a.getBreakDownElements().add(b); this.activityDao.saveOrUpdateActivity(a); } List<BreakdownElement>
+	 * liste = new ArrayList<BreakdownElement>(a.getBreakDownElements()); logger.debug("###
+	 * ActivityManager - TEST ### liste size = "+liste.size()); for (BreakdownElement b : liste){
+	 * System.out.println("b="+b); } }
+	 */
+
 	/**
 	 * Save participant
 	 * 
@@ -68,8 +78,9 @@ public class ParticipantManager {
 
 	/**
 	 * Setter of participantDao.
-	 *
-	 * @param _participantDao The participantDao to set.
+	 * 
+	 * @param _participantDao
+	 *            The participantDao to set.
 	 */
 	public void setParticipantDao(ParticipantDao _participantDao) {
 		this.participantDao = _participantDao ;
