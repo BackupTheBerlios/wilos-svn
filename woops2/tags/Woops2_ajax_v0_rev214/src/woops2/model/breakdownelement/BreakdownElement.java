19d0383960fda347d652ab5a0411787ae0541132
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-24 09:49:04 +0000

    [Woops2] Tag Woops2_Ajax

---
+++ woops2/tags/Woops2_ajax_v0_rev214/src/woops2/model/breakdownelement/BreakdownElement.java
@@ -0,0 +1,234 @@
+
+package woops2.model.breakdownelement ;
+
+import java.util.HashSet ;
+import java.util.Set ;
+
+import org.apache.commons.lang.builder.EqualsBuilder ;
+import org.apache.commons.lang.builder.HashCodeBuilder ;
+
+import woops2.model.activity.Activity ;
+import woops2.model.element.Element ;
+
+/**
+ * @author deder.
+ * 
+ * Breakdown Element is an abstract generalization for any type of Element that is part of a
+ * breakdown structure. It defines a set of properties available to all of its specializations.
+ * 
+ */
+public class BreakdownElement extends Element implements Cloneable {
+
+	private String prefix ;
+
+	private Boolean isPlanned ;
+
+	private Boolean hasMultipleOccurrences ;
+
+	private Boolean isOptional ;
+
+	private Set<Activity> superActivities ;
+
+	/**
+	 * Constructor.
+	 * 
+	 */
+	public BreakdownElement() {
+		super() ;
+		this.prefix = "";
+		this.isOptional = false;
+		this.isPlanned = true;
+		this.hasMultipleOccurrences = false;
+		this.superActivities = new HashSet<Activity>() ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see woops2.model.element.Element#clone()
+	 */
+	@ Override
+	public BreakdownElement clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		BreakdownElement breakdownElement = new BreakdownElement() ;
+		breakdownElement.copy(this) ;
+		return breakdownElement ;
+	}
+
+	/**
+	 * Copy the object.
+	 */
+	protected void copy(final BreakdownElement _breakdownElement) {
+		super.copy(_breakdownElement) ;
+		this.prefix = _breakdownElement.prefix ;
+		this.hasMultipleOccurrences = _breakdownElement.hasMultipleOccurrences ;
+		this.isPlanned = _breakdownElement.isPlanned ;
+		this.isOptional = _breakdownElement.isOptional ;
+		this.setSuperActivities(_breakdownElement.getSuperActivities());
+
+	}
+
+	/**
+	 * Indicates whether another object is "equal to" this one.
+	 * 
+	 * @return true if equal else false
+	 */
+	public boolean equals(Object obj) {
+		if(obj instanceof BreakdownElement == false){
+			return false ;
+		}
+		if(this == obj){
+			return true ;
+		}
+		BreakdownElement breakdownElement = (BreakdownElement) obj ;
+		return new EqualsBuilder().appendSuper(super.equals(breakdownElement)).append(this.prefix, breakdownElement.prefix).append(this.isPlanned,
+				breakdownElement.isPlanned).append(this.hasMultipleOccurrences, breakdownElement.hasMultipleOccurrences).append(this.isOptional,
+				breakdownElement.isOptional).append(this.superActivities, breakdownElement.superActivities).isEquals() ;
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return a hash code
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).append(this.prefix).append(this.hasMultipleOccurrences).append(this.isOptional)
+				.append(this.isPlanned)/*.append(this.getSuperActivities())*/.toHashCode() ; // TODO a debattre si on fé le hashcode sur les collections
+	}
+
+	/**
+	 * Add an Activity to the superActivities collection of a BreakdownElement
+	 * 
+	 * @param _activity
+	 */
+	public void addToActivity(Activity _activity) {
+		this.getSuperActivities().add(_activity) ;
+		_activity.getBreakDownElements().add(this) ;
+	}
+
+	/**
+	 * Add an activity collection to the activity collection of a breakdownelement
+	 * 
+	 * @param _activities
+	 */
+	public void addToAllActivities(Set<Activity> _activities) {
+		for(Activity activity : _activities){
+			activity.addToBreakdownElement(this) ;
+		}
+	}
+
+	/**
+	 * Remove from a breakdownelement one of these superActivities
+	 * 
+	 * @param _activity
+	 */
+	public void removeFromActivity(Activity _activity) {
+		_activity.getBreakDownElements().remove(this) ;
+		this.getSuperActivities().remove(_activity) ;
+	}
+
+	/**
+	 * Remove from a breakdownelement all its superActivities.
+	 * 
+	 */
+	public void removeFromAllActivities() {
+		for(Activity activity : this.getSuperActivities())
+			this.removeFromActivity(activity) ;
+	}
+
+	/**
+	 * Getter of hasMultipleOccurrences.
+	 * 
+	 * @return the hasMultipleOccurrences.
+	 */
+	public Boolean getHasMultipleOccurrences() {
+		return this.hasMultipleOccurrences ;
+	}
+
+	/**
+	 * Setter of hasMultipleOccurrences.
+	 * 
+	 * @param _hasMultipleOccurrences
+	 *            The hasMultipleOccurrences to set.
+	 */
+	public void setHasMultipleOccurrences(Boolean _hasMultipleOccurrences) {
+		this.hasMultipleOccurrences = _hasMultipleOccurrences ;
+	}
+
+	/**
+	 * Getter of isOptional.
+	 * 
+	 * @return the isOptional.
+	 */
+	public Boolean getIsOptional() {
+		return this.isOptional ;
+	}
+
+	/**
+	 * Setter of isOptional.
+	 * 
+	 * @param _isOptional
+	 *            The isOptional to set.
+	 */
+	public void setIsOptional(Boolean _isOptional) {
+		this.isOptional = _isOptional ;
+	}
+
+	/**
+	 * Getter of isPlanned.
+	 * 
+	 * @return the isPlanned.
+	 */
+	public Boolean getIsPlanned() {
+		return this.isPlanned ;
+	}
+
+	/**
+	 * Setter of isPlanned.
+	 * 
+	 * @param _isPlanned
+	 *            The isPlanned to set.
+	 */
+	public void setIsPlanned(Boolean _isPlanned) {
+		this.isPlanned = _isPlanned ;
+	}
+
+	/**
+	 * Getter of prefix.
+	 * 
+	 * @return the prefix.
+	 */
+	public String getPrefix() {
+		return this.prefix ;
+	}
+
+	/**
+	 * Setter of prefix.
+	 * 
+	 * @param _prefix
+	 *            The prefix to set.
+	 */
+	public void setPrefix(String _prefix) {
+		this.prefix = _prefix ;
+	}
+
+	/**
+	 * Getter of superActivities.
+	 * 
+	 * @return the superActivities.
+	 */
+	public Set<Activity> getSuperActivities() {
+		return this.superActivities ;
+	}
+
+	/**
+	 * Setter of superActivities.
+	 * 
+	 * @param _superActivities
+	 *            The superActivities to set.
+	 */
+	@ SuppressWarnings ("unused")
+	private void setSuperActivities(Set<Activity> _superActivities) {
+		this.superActivities = _superActivities ;
+	}
+}
---
+++ woops2/tags/Woops2_ajax_v0_rev214/src/woops2/model/task/TaskDefinition.java
@@ -0,0 +1,212 @@
+package woops2.model.task;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import woops2.model.element.Element;
+
+/**
+ * @author Sebastien BALARD
+ * @author eperico
+ * 
+ * This class represents a task is a content element that describes work being
+ * performed by Roles. It defines one default performing RoleDefinition as well
+ * as many additional performers.
+ * 
+ */
+public class TaskDefinition extends Element implements Cloneable {
+
+	/**
+	 * Collection of Step
+	 */
+	private Set<Step> steps;
+
+	/**
+	 * Collection of TaskDescriptor
+	 */
+	private Set<TaskDescriptor> taskDescriptors;
+
+	/**
+	 * Default constructor
+	 */
+	public TaskDefinition() {
+		super();
+		this.steps = new HashSet<Step>();
+		this.taskDescriptors = new HashSet<TaskDescriptor>();
+	}
+
+	/**
+	 * Indicates whether another object is "equal to" this one.
+	 * 
+	 * @return true if equal else false
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof TaskDefinition == false) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		TaskDefinition task = (TaskDefinition) obj;
+		return new EqualsBuilder().appendSuper(super.equals(task)).append(
+				this.steps, task.steps).append(this.taskDescriptors,
+				task.taskDescriptors).isEquals();
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return a hash code
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.append(this.steps).append(this.taskDescriptors).toHashCode();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public TaskDefinition clone() throws CloneNotSupportedException {
+		TaskDefinition taskDefinition = new TaskDefinition();
+		taskDefinition.copy(this);
+		return taskDefinition;
+	}
+
+	/**
+	 * Copy the _taskDefinition into this.
+	 */
+	protected void copy(final TaskDefinition _taskDefinition) {
+		super.copy(_taskDefinition);
+		this.setSteps(_taskDefinition.getSteps());
+		this.setTaskDescriptors(_taskDefinition.getTaskDescriptors());
+	}
+
+	/**
+	 * Getter of steps.
+	 * 
+	 * @return the steps.
+	 */
+	public Set<Step> getSteps() {
+		return this.steps;
+	}
+
+	/**
+	 * Setter of steps.
+	 * 
+	 * @param _steps
+	 *            The steps to set.
+	 */
+	@SuppressWarnings("unused")
+	private void setSteps(Set<Step> _steps) {
+		this.steps = _steps;
+	}
+
+	/**
+	 * Getter of taskDescriptors.
+	 * 
+	 * @return the taskDescriptors.
+	 */
+	public Set<TaskDescriptor> getTaskDescriptors() {
+		return this.taskDescriptors;
+	}
+
+	/**
+	 * Setter of taskDescriptors.
+	 * 
+	 * @param _taskDescriptors
+	 *            The taskDescriptors to set.
+	 */
+	@SuppressWarnings("unused")
+	private void setTaskDescriptors(Set<TaskDescriptor> _taskDescriptors) {
+		this.taskDescriptors = _taskDescriptors;
+	}
+
+	/**
+	 * Add a step from a taskDefinition
+	 * 
+	 * @param _step
+	 */
+	public void addStep(Step _step) {
+		this.steps.add(_step);
+		_step.setTaskDefinition(this);
+	}
+
+	/**
+	 * Add a step collection to the step collection of a TaskDefinition
+	 * 
+	 * @param _steps
+	 */
+	public void addToAllSteps(Set<Step> _steps) {
+		for (Step s : _steps) {
+			s.addToTaskDefinition(this);
+		}
+	}
+
+	/**
+	 * Add a taskDescriptor from a taskDefinition
+	 * 
+	 * @param _taskDescriptor
+	 */
+	public void addTaskDescriptor(TaskDescriptor _taskDescriptor) {
+		this.taskDescriptors.add(_taskDescriptor);
+		_taskDescriptor.setTaskDefinition(this);
+	}
+
+	/**
+	 * Add a taskDesciptor collection to the taskDesciptor collection of a
+	 * TaskDefinition
+	 * 
+	 * @param _taskDesciptor
+	 */
+	public void addToAllTaskDesciptors(Set<TaskDescriptor> _taskDesciptor) {
+		for (TaskDescriptor td : _taskDesciptor) {
+			td.addToTaskDefinition(this);
+		}
+	}
+
+	/**
+	 * Remove a step from a taskDefinition
+	 * 
+	 * @param _step
+	 */
+	public void removeStep(Step _step) {
+		_step.setTaskDefinition(null);
+		this.steps.remove(_step);
+	}
+
+	/**
+	 * Remove all steps from a TaskDefinition
+	 */
+	public void removeAllSteps() {
+		for (Step tmp : this.steps) {
+			tmp.removeFromTaskDefinition(this);
+		}
+		this.steps.clear();
+	}
+
+	/**
+	 * Remove a taskDescriptor to its taskDefinition
+	 * 
+	 * @param _taskDescriptor
+	 */
+	public void removeTaskDescriptor(TaskDescriptor _taskDescriptor) {
+		_taskDescriptor.setTaskDefinition(null);
+		this.taskDescriptors.remove(_taskDescriptor);
+	}
+
+	/**
+	 * Remove all taskDescriptors to its TaskDefinition
+	 */
+	public void removeAllTaskDescriptors() {
+		for (TaskDescriptor tmp : this.taskDescriptors) {
+			tmp.removeFromTaskDefinition(this);
+		}
+		this.taskDescriptors.clear();
+	}
+}
