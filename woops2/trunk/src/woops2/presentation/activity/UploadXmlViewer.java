463006ed50883440a3b6e636f80205d1301bc3dd
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-24 13:08:47 +0000

    [Woops2] uploadXmlBean

--- woops2/trunk/src/woops2/presentation/activity/UploadXmlViewer.java
+++ woops2/trunk/src/woops2/presentation/activity/UploadXmlViewer.java
@@ -1,42 +1,6 @@
 package woops2.presentation.activity;
 
-import java.io.InputStream;
-
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
-
-import org.apache.myfaces.custom.fileupload.UploadedFile;
 
 public class UploadXmlViewer {
-	
-	private UploadedFile uploadedFile;
-	
-	public UploadXmlViewer(){
-		
-	}
-
-	public UploadedFile getUploadedFile() {
-		return uploadedFile;
-	}
-
-	public void setUploadedFile(UploadedFile uploadedFile) {
-		this.uploadedFile = uploadedFile;
-	}
-	
-	public String UploadAction()
-    {
-        FacesContext context = FacesContext.getCurrentInstance();
-        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
-        try {
-          InputStream stream = uploadedFile.getInputStream();
-          long fSize = uploadedFile.getSize();
-          byte [] buffer = new byte[(int)fSize];
-          stream.read(buffer, 0, (int)fSize);
-          stream.close();
-        } catch (Exception ioe) {
-           ioe.printStackTrace();
-        }
-        return "success";
-    } 
 	
 }
