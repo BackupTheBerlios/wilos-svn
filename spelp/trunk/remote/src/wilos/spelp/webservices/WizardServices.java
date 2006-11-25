55e0ce217e260723c6df553fcc14d98031580a91
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 14:41:46 +0000

    [SPELP] modif d'une méthode getAllProcess

--- spelp/trunk/remote/src/wilos/spelp/webservices/WizardServices.java
+++ spelp/trunk/remote/src/wilos/spelp/webservices/WizardServices.java
@@ -35,30 +35,9 @@ import woops2.model.task.TaskDescriptor;
  */
 @WebService()
 public class WizardServices {
-    @WebMethod
-	public List<RoleDescriptor> getRolesByUser (@WebParam(name="login") String login,@WebParam(name="password")  String password) {
-		List<RoleDescriptor> r = new ArrayList<RoleDescriptor>();
-		if(login.equals("toto") && password.equals("toto")) {
-			ProcessService p = new ProcessService();
-			List<Process> lp = p.getProcessesList(); 
-			
-			Set<BreakdownElement> bdes = new HashSet<BreakdownElement>();
-			for (Process pr : lp) {
-				bdes = pr.getBreakDownElements();
-				for (BreakdownElement bd : bdes) {
-					if (bd instanceof RoleDescriptor) {
-						r.add((RoleDescriptor)bd);
-					}
-				}
-			}
-		}
-		return r;
-	}
-    
-     @WebMethod
-	public List<String> getRolesByUserXstream (@WebParam(name="login") String login,@WebParam(name="password")  String password) {
-                XStream xstream = new XStream();
-		List<String> r = new ArrayList<String>();
+//    @WebMethod
+//	public List<RoleDescriptor> getRolesByUser (@WebParam(name="login") String login,@WebParam(name="password")  String password) {
+//		List<RoleDescriptor> r = new ArrayList<RoleDescriptor>();
 //		if(login.equals("toto") && password.equals("toto")) {
 //			ProcessService p = new ProcessService();
 //			List<Process> lp = p.getProcessesList(); 
@@ -68,45 +47,125 @@ public class WizardServices {
 //				bdes = pr.getBreakDownElements();
 //				for (BreakdownElement bd : bdes) {
 //					if (bd instanceof RoleDescriptor) {
-//						r.add(xstream.toXML((RoleDescriptor)bd));
+//						r.add((RoleDescriptor)bd);
 //					}
 //				}
 //			}
 //		}
-                RoleDescriptor rd = new RoleDescriptor();
-                rd.setName("TestRoleDescriptor");
-                TaskDescriptor td = new TaskDescriptor();
-                td.setMainRole(rd);
-                td.setName("TestTaskDescriptor");
-                rd.addPrimaryTask(td);
-
-                r.add(xstream.toXML(rd));
-                return r;
-	}
+//		return r;
+//	}
+//    
+//     @WebMethod
+//	public List<String> getRolesByUserXstream (@WebParam(name="login") String login,@WebParam(name="password")  String password) {
+//                XStream xstream = new XStream();
+//		List<String> r = new ArrayList<String>();
+////		if(login.equals("toto") && password.equals("toto")) {
+////			ProcessService p = new ProcessService();
+////			List<Process> lp = p.getProcessesList(); 
+////			
+////			Set<BreakdownElement> bdes = new HashSet<BreakdownElement>();
+////			for (Process pr : lp) {
+////				bdes = pr.getBreakDownElements();
+////				for (BreakdownElement bd : bdes) {
+////					if (bd instanceof RoleDescriptor) {
+////						r.add(xstream.toXML((RoleDescriptor)bd));
+////					}
+////				}
+////			}
+////		}
+//                RoleDescriptor rd = new RoleDescriptor();
+//                rd.setName("TestRoleDescriptor");
+//                TaskDescriptor td = new TaskDescriptor();
+//                td.setMainRole(rd);
+//                td.setName("TestTaskDescriptor");
+//                rd.addPrimaryTask(td);
+//
+//                r.add(xstream.toXML(rd));
+//                return r;
+//	}
     
     @WebMethod
-    public List<Process> getAllProcess () {
-    	        List<Process> lp = new ArrayList<Process>();
-                
-                 // Getback the application context from the spring configuration file
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-                // Show what is in the factory
-		System.out.println("factory => "+ctx);
-                // Getback the hibernateTemplate bean
-		org.springframework.orm.hibernate3.HibernateTemplate hibTempl = (org.springframework.orm.hibernate3.HibernateTemplate) ctx.getBean("hibernateTemplate");
-                System.out.println("HibTemplate => "+hibTempl);
-                // Get the ActivityDao Singleton for managing Activity data
-		//ActivityDao dao = (ActivityDao) ctx.getBean("ActivityDao");
-                
-                ProcessService p = (ProcessService)ctx.getBean("ProcessService");
-
-            // Create empty Activity
-		//Activity a = new Activity();
-            // Save it
-		//dao.saveOrUpdateActivity(a);
-            
-             lp = p.getProcessesList();
-
-		return lp;
+    public List<String> getAllProcess (@WebParam(name="login") String login,@WebParam(name="password")  String password) {
+        List<String> result = new ArrayList<String>();
+        if(login.equals("testBD") && password.equals("testBD")) {
+            List<Process> lp = new ArrayList<Process>();
+            // Getback the application context from the spring configuration file
+            ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+            // Show what is in the factory
+            System.out.println("factory => "+ctx);
+            // Getback the hibernateTemplate bean
+            org.springframework.orm.hibernate3.HibernateTemplate hibTempl = (org.springframework.orm.hibernate3.HibernateTemplate) ctx.getBean("hibernateTemplate");
+            System.out.println("HibTemplate => "+hibTempl);
+            // Get the ProcessService Singleton for managing Activity data   
+            ProcessService p = (ProcessService)ctx.getBean("ProcessService");
+            lp = p.getProcessesList();
+            XStream xstream = new XStream();
+            for (Process pro : lp) {
+                result.add(xstream.toXML(pro));
+            }          
+        } else {
+            if(login.equals("testSansBD") && password.equals("testSansBD")) {
+                ArrayList<RoleDescriptor> myRoleListe;
+		RoleDescriptor aTmpRole;
+		TaskDescriptor aTmpTask;
+		
+		aTmpRole = new RoleDescriptor();
+		aTmpTask = new TaskDescriptor();
+		myRoleListe = new ArrayList<RoleDescriptor>();
+                Process pro = new Process();
+		
+		aTmpRole.setName("Developper");
+		aTmpRole.setDescription("Un gars qui développe");
+		
+		aTmpTask.setName("Coder le programme");
+		aTmpTask.setDescription("Un grand moment de solitude");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Aimer son programme");
+		aTmpTask.setDescription("Un grand moment d'amour");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Passer le balai");
+		aTmpTask.setDescription("Et c'est plus propre");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		
+                pro.addBreakdownElement(aTmpRole);	
+		
+		aTmpRole = new RoleDescriptor();
+		aTmpTask = new TaskDescriptor();
+		aTmpRole.setName("Tester");
+		aTmpRole.setDescription("Faire des essais, en gros");
+		aTmpTask.setName("Tester le programme");
+		aTmpTask.setDescription("Un grand moment de solitude");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Detester le programme");
+		aTmpTask.setDescription("Un grand moment de haine");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Passer la serpillière");
+		aTmpTask.setDescription("Un grand moment de solitude");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		
+		pro.addBreakdownElement(aTmpRole);	
+		
+		aTmpRole = new RoleDescriptor();
+		aTmpTask = new TaskDescriptor();
+		aTmpRole.setName("Conceptualisateur");
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Conceptualiser les concepts du programme");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Rever du programme");
+		aTmpRole.addPrimaryTask(aTmpTask);
+		aTmpTask = new TaskDescriptor();
+		aTmpTask.setName("Faire le café concept");
+		aTmpRole.addPrimaryTask(aTmpTask);		
+		pro.addBreakdownElement(aTmpRole);
+                XStream xstream = new XStream();
+                result.add(xstream.toXML(pro));
+            }
+        }
+        return result;
     }
 }
