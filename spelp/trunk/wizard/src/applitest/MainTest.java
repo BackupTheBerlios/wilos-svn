678a03edb5744ae993e445cb69e784112045a31c
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 15:31:54 +0000

    [SPELP] Ajout du test de getAllProcess

--- spelp/trunk/wizard/src/applitest/MainTest.java
+++ spelp/trunk/wizard/src/applitest/MainTest.java
@@ -1,16 +1,11 @@
 package applitest;
 
 import com.thoughtworks.xstream.XStream;
+import woops2.model.breakdownelement.BreakdownElement;
 import woops2.model.role.RoleDescriptor;
 import woops2.model.activity.Activity;
 import woops2.model.task.TaskDescriptor;
-
-
-//import wilos.spelp.webservices.RoleDescriptor;
-//import wilos.spelp.webservices.WizardServices;
-//import wilos.spelp.webservices.WizardServicesService;
-
-
+import woops2.model.process.Process;
 
 public class MainTest {
 
@@ -18,54 +13,25 @@ public class MainTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-            try { // Call Web Service Operation
-                webservices.WizardServicesService service = new webservices.WizardServicesService();
-                webservices.WizardServices port = service.getWizardServicesPort();
-                // TODO initialize WS operation arguments here
-                java.lang.String login = "";
-                java.lang.String password = "";
-                // TODO process result here
-                java.util.List<java.lang.String> result = port.getRolesByUserXstream(login, password);
-                System.out.println("Result = "+result);
-                XStream xstream = new XStream();
-                //xstream.setClassLoader(RoleDescriptor.class.getClassLoader());
-                RoleDescriptor rd = (RoleDescriptor)xstream.fromXML(result.get(0));
-                System.out.println(rd.getName());
-                System.out.println(rd.getPrimaryTasks().iterator().next().getName());
-            } catch (Exception ex) {
-                // TODO handle custom exceptions here
-                ex.printStackTrace();
-            }
 
-//            try { // Call Web Service Operation
-//                wilos.spelp.webservices.WizardServicesService service = new wilos.spelp.webservices.WizardServicesService();
-//                // TODO initialize WS operation arguments here
-//                java.lang.String login = "toto";
-//                java.lang.String password = "toto";
-//                // TODO process result here
-//                
-//                 Service servic2 = WizardServicesService.create(new URL("http://localhost:8084/remote/WizardServices?wsdl"),service.getServiceName());
-//            
-//                WizardServices port = servic2.getPort(WizardServices.class);
-//                java.util.List<RoleDescriptor> result = port.getRolesByUser(login, password);
-//                System.out.println("Result = "+result);
-//            } catch (Exception ex) {
-//                // TODO handle custom exceptions here
-//                ex.printStackTrace();
-//            }
-//		// on instancie le proxy local
-//		WizardServicesProxy px = new WizardServicesProxy();
-//		// on specifie l'adresse du serveur
-//		px.setEndpoint("http://localhost:8080/wilos_remote/services/WizardServices");
-//		// on appelle la m�thode sur le proxy local comme on le ferait sur l'objet distant
-//		try {
-//			RoleDescriptor [] tabRoles = px.getRolesByUser("toto", "toto");
-//			for (int i=0; i<tabRoles.length; i++) {
-//				System.out.println(tabRoles[i].getName());
-//			}
-//		} catch (RemoteException e) {
-//			e.printStackTrace();
-//		}
+                try { // Call Web Service Operation
+                    services.WizardServicesService service = new services.WizardServicesService();
+                    services.WizardServices port = service.getWizardServicesPort();
+                    // TODO initialize WS operation arguments here
+                    java.lang.String login = "testSansBD";
+                    java.lang.String password = "testSansBD";
+                    // TODO process result here
+                    java.util.List<String> result = port.getAllProcess(login, password);
+                    System.out.println("Result = "+result);
+                    XStream xstream = new XStream();                
+                    Process pro = (Process)xstream.fromXML(result.get(0));
+                    System.out.println("NomProcessus : "+pro.getName());
+                    for (BreakdownElement bk : pro.getBreakDownElements()) {
+                        System.out.println("BreakDownElement : "+bk.getName());
+                    }
+                } catch (Exception ex) {
+                    // TODO handle custom exceptions here
+                }
             
 	
         
