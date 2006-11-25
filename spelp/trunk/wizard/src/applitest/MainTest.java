87ae67f5c46823674c650cb6a14f824fb50f90d8
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 13:59:10 +0000

    [SPELP] Utilisation de Xtream

--- spelp/trunk/wizard/src/applitest/MainTest.java
+++ spelp/trunk/wizard/src/applitest/MainTest.java
@@ -1,70 +1,58 @@
 package applitest;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.rmi.RemoteException;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import wilos.spelp.webservices.RoleDescriptor;
-import wilos.spelp.webservices.WizardServices;
-import wilos.spelp.webservices.WizardServicesService;
+import com.thoughtworks.xstream.XStream;
+import woops2.model.role.RoleDescriptor;
+import woops2.model.activity.Activity;
+import woops2.model.task.TaskDescriptor;
+
+
+//import wilos.spelp.webservices.RoleDescriptor;
+//import wilos.spelp.webservices.WizardServices;
+//import wilos.spelp.webservices.WizardServicesService;
 
 
 
 public class MainTest {
-        private static String url ="http://localhost:8084/remote/WizardServices?wsdl";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-           /* try { // Call Web Service Operation
-                wilos.spelp.webservices.WizardServicesService service = new wilos.spelp.webservices.WizardServicesService();
-                // TODO initialize WS operation arguments here
-                java.lang.String login = "toto";
-                java.lang.String password = "toto";
-                // TODO process result here
-                
-                 Service servic2 = WizardServicesService.create(new URL("http://localhost:8084/remote/WizardServices?wsdl"), service.getServiceName());
-            
-                WizardServices port = servic2.getPort(WizardServices.class);
-                java.util.List<RoleDescriptor> result = port.getRolesByUser(login, password);
-                System.out.println("Result = "+result);
-            } catch (Exception ex) {
-                // TODO handle custom exceptions here
-                ex.printStackTrace();
-            }*/
             try { // Call Web Service Operation
-                //services.WizardServicesService service = new services.WizardServicesService();
+                webservices.WizardServicesService service = new webservices.WizardServicesService();
+                webservices.WizardServices port = service.getWizardServicesPort();
+                // TODO initialize WS operation arguments here
+                java.lang.String login = "";
+                java.lang.String password = "";
                 // TODO process result here
-                
-                Service serv = Service.create(new URL(url), new QName("http://webservices.spelp.wilos/", "WizardServicesService"));
-                
-                services.WizardServices port = serv.getPort(services.WizardServices.class);
-                
-                woops2.model.role.RoleDescriptor rd = null;
-                
-                
-                
-                //java.util.List<services.Process> result = port.getAllProcess();
-                java.util.List result = port.getRolesByUser("toto","toto");
+                java.util.List<java.lang.String> result = port.getRolesByUserXstream(login, password);
                 System.out.println("Result = "+result);
-                System.out.println(((woops2.model.role.RoleDescriptor)result.get(0)).getName());
-                
-                
-                
-                
-                
+                XStream xstream = new XStream();
+                //xstream.setClassLoader(RoleDescriptor.class.getClassLoader());
+                RoleDescriptor rd = (RoleDescriptor)xstream.fromXML(result.get(0));
+                System.out.println(rd.getName());
+                System.out.println(rd.getPrimaryTasks().iterator().next().getName());
             } catch (Exception ex) {
                 // TODO handle custom exceptions here
                 ex.printStackTrace();
             }
-            
-            
-            
-            
-            
+
+//            try { // Call Web Service Operation
+//                wilos.spelp.webservices.WizardServicesService service = new wilos.spelp.webservices.WizardServicesService();
+//                // TODO initialize WS operation arguments here
+//                java.lang.String login = "toto";
+//                java.lang.String password = "toto";
+//                // TODO process result here
+//                
+//                 Service servic2 = WizardServicesService.create(new URL("http://localhost:8084/remote/WizardServices?wsdl"),service.getServiceName());
+//            
+//                WizardServices port = servic2.getPort(WizardServices.class);
+//                java.util.List<RoleDescriptor> result = port.getRolesByUser(login, password);
+//                System.out.println("Result = "+result);
+//            } catch (Exception ex) {
+//                // TODO handle custom exceptions here
+//                ex.printStackTrace();
+//            }
 //		// on instancie le proxy local
 //		WizardServicesProxy px = new WizardServicesProxy();
 //		// on specifie l'adresse du serveur
@@ -79,6 +67,8 @@ public class MainTest {
 //			e.printStackTrace();
 //		}
             
-	}
+	
+        
+        }
 
 }
