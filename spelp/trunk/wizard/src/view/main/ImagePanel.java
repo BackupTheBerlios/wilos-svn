fb91f790c473cff80526296a5a7c016baeb4d9ea
toine <toine@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 18:14:21 +0000

    [SPELP]: ajout d'une classe pour le logo

---
+++ spelp/trunk/wizard/src/view/main/ImagePanel.java
@@ -0,0 +1,63 @@
+/*
+ * ImagePanel.java
+ *
+ * Created on 25 novembre 2006, 17:51
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package view.main;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import org.jdesktop.swingx.JXImagePanel;
+import java.lang.Exception;
+       
+
+
+/**
+ *
+ * @author toine
+ */
+public class ImagePanel extends JXImagePanel {
+    
+    private static String imagePath = "ressources"+java.io.File.separator+"logo.png";
+    //private static String imagePath = "ressources/logo.png";
+    
+    
+    /** Creates a new instance of ImagePanel */
+    public ImagePanel() {
+        
+        init();
+    }
+    
+    public void init() {
+        Image image;
+        try {
+            image = ImageIO.read(getClass().getResource(imagePath));
+            this.setImage(image);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+            
+            //com.sun.org.apache.bcel.internal.util.ClassLoader.getSystemResources(imagePath);
+            //Image image = ImageIO.read(new File(imagePath));
+//            Image image;
+//            image = Toolkit.getDefaultToolkit().createImage(imagePath);
+           
+            
+            
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }        
+    }    
+}
