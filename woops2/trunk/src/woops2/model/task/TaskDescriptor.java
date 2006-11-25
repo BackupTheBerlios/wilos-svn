138f4a559cb6b0a64cbe03ff0e967d87b482b810
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 17:45:51 +0000

    [Woops2] trunk / merge # 1

---
+++ woops2/trunk/src/woops2/model/task/TaskDescriptor.java
@@ -0,0 +1,244 @@
+package woops2.model.task;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import woops2.model.role.RoleDescriptor;
+import woops2.model.workbreakdownelement.WorkBreakdownElement;
+
+/**
+ * @author Sebastien BALARD
+ * @author eperico
+ * 
+ * A Task Descriptor is a Descriptor and Work Breakdown Element that represents
+ * a proxy for a Task in the context of one specific Activity. Every breakdown
+ * structure can define different relationships of Task Descriptors to Work
+ * Product Descriptors and Role Descriptors. Therefore one Task can be
+ * represented by many Task Descriptors each within the context of an Activity
+ * with its own set of relationships.
+ * 
+ */
+public class TaskDescriptor extends WorkBreakdownElement implements Cloneable {
+
+	/**
+	 * the attached taskDefinition
+	 */
+	private TaskDefinition taskDefinition;
+
+	/**
+	 * The additional roles of the role
+	 */
+	private Set<RoleDescriptor> additionalRoles;
+
+	/**
+	 * The main role of the taskDefinition
+	 */
+	private RoleDescriptor mainRole;
+
+	/**
+	 * Default constructor.
+	 */
+	public TaskDescriptor() {
+		super();
+		this.additionalRoles = new HashSet<RoleDescriptor>();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public TaskDescriptor clone() throws CloneNotSupportedException {
+		TaskDescriptor taskDescriptor = new TaskDescriptor();
+		taskDescriptor.copy(this);
+		return taskDescriptor;
+	}
+
+	/**
+	 * Copy the _roleDescriptor into this.
+	 */
+	protected void copy(final TaskDescriptor _taskDescriptor) {
+		super.copy(_taskDescriptor);
+		this.setAdditionalRoles(_taskDescriptor.getAdditionalRoles());
+		this.setTaskDefinition(_taskDescriptor.getTaskDefinition());
+		this.setMainRole(_taskDescriptor.getMainRole());
+	}
+
+	/**
+	 * Getter of taskDefinition.
+	 * 
+	 * @return the taskDefinition.
+	 */
+	public TaskDefinition getTaskDefinition() {
+		return this.taskDefinition;
+	}
+
+	/**
+	 * Setter of taskDefinition.
+	 * 
+	 * @param _taskDefinition
+	 *            The taskDefinition to set.
+	 */
+	protected void setTaskDefinition(TaskDefinition _taskDefinition) {
+		this.taskDefinition = _taskDefinition;
+	}
+
+	/**
+	 * Getter of additionalRoles.
+	 * 
+	 * @return the additionalRoles.
+	 */
+	public Set<RoleDescriptor> getAdditionalRoles() {
+		return this.additionalRoles;
+	}
+
+	/**
+	 * Setter of additionalRoles.
+	 * 
+	 * @param _additionalRoles
+	 *            The additionalRoles to set.
+	 */
+	@SuppressWarnings("unused")
+	private void setAdditionalRoles(Set<RoleDescriptor> _additionalRoles) {
+		this.additionalRoles = _additionalRoles;
+	}
+
+	/**
+	 * Getter of mainRole.
+	 * 
+	 * @return the mainRole.
+	 */
+	public RoleDescriptor getMainRole() {
+		return this.mainRole;
+	}
+
+	/**
+	 * Setter of mainRole.
+	 * 
+	 * @param _mainRole
+	 *            The mainRole to set.
+	 */
+	public void setMainRole(RoleDescriptor _mainRole) {
+		this.mainRole = _mainRole;
+	}
+
+	/**
+	 * Indicates whether another object is "equal to" this one.
+	 * 
+	 * @return true if equal else false
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof TaskDescriptor == false) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		TaskDescriptor taskDescriptor = (TaskDescriptor) obj;
+		return new EqualsBuilder().appendSuper(super.equals(taskDescriptor))
+				.append(this.taskDefinition, taskDescriptor.taskDefinition)
+				.append(this.additionalRoles, taskDescriptor.additionalRoles)
+				.append(this.mainRole, taskDescriptor.mainRole).isEquals();
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return a hash code
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.append(this.taskDefinition).append(this.additionalRoles)
+				.append(this.mainRole).toHashCode();
+	}
+
+	/**
+	 * Attach a taskDescriptor to a taskDefinition
+	 * 
+	 * @param _taskDefinition
+	 */
+	public void addToTaskDefinition(TaskDefinition _taskDefinition) {
+		this.taskDefinition = _taskDefinition;
+		_taskDefinition.getTaskDescriptors().add(this);
+	}
+
+	/**
+	 * Detach a taskDescriptor to its taskDefinition
+	 * 
+	 * @param _taskDefinition
+	 */
+	public void removeFromTaskDefinition(TaskDefinition _taskDefinition) {
+		_taskDefinition.getTaskDescriptors().remove(this);
+		this.taskDefinition = null;
+	}
+
+	/**
+	 * Attach a taskDescriptor to a roleDescriptor
+	 * 
+	 * @param _roleDescriptor
+	 */
+	public void addToMainRole(RoleDescriptor _roleDescriptor) {
+		this.mainRole = _roleDescriptor;
+		_roleDescriptor.getPrimaryTasks().add(this);
+	}
+
+	/**
+	 * Detach a taskDescriptor to its roleDescriptor
+	 * 
+	 * @param _roleDescriptor
+	 */
+	public void removeFromMainRole(RoleDescriptor _roleDescriptor) {
+		this.mainRole = null;
+		_roleDescriptor.getAdditionalTasks().remove(this);
+	}
+
+	/**
+	 * @param _roleDescriptor
+	 */
+	public void addToRoleDescriptor(RoleDescriptor _roleDescriptor) {
+		this.additionalRoles.add(_roleDescriptor);
+		_roleDescriptor.getAdditionalTasks().add(this);
+	}
+
+	/**
+	 * 
+	 * @param _role
+	 */
+	public void addToAllRoleDescriptors(Set<RoleDescriptor> _role) {
+		for (RoleDescriptor role : _role) {
+			role.addToTaskDescriptor(this);
+		}
+	}
+
+	/**
+	 * @param _roleDescriptor
+	 */
+	public void removeFromRoleDescriptor(RoleDescriptor _roleDescriptor) {
+		_roleDescriptor.getAdditionalTasks().remove(this);
+		this.additionalRoles.remove(_roleDescriptor);
+	}
+
+	/**
+	 * Remove from a roleDescriptor all its taskDescriptors
+	 */
+	public void removeAllRoleDescriptors() {
+		for (RoleDescriptor tmp : this.additionalRoles) {
+			tmp.removeFromTaskDescriptor(this);
+		}
+		this.additionalRoles.clear();
+	}
+
+	/**
+	 * Remove from a taskDescriptor all its roleDescriptors
+	 */
+	public void removeFromAllRoleDescriptors() {
+		for (RoleDescriptor tmp : this.getAdditionalRoles()) {
+			this.removeFromRoleDescriptor(tmp);
+		}
+	}
+
+}
