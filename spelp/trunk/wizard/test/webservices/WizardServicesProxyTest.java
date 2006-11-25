62bfc4c57fab89c64e8a33d086a87b18907699ff
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-25 17:12:24 +0000

    [SPELP] Ajout d'une Junit pour le WizardServicesProxy

---
+++ spelp/trunk/wizard/test/webservices/WizardServicesProxyTest.java
@@ -0,0 +1,62 @@
+/*
+ * WizardServicesProxyTest.java
+ * JUnit based test
+ *
+ * Created on 25 novembre 2006, 17:58
+ */
+
+package webservices;
+
+import junit.framework.*;
+import com.thoughtworks.xstream.XStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import woops2.model.breakdownelement.BreakdownElement;
+import woops2.model.role.RoleDescriptor;
+import woops2.model.process.Process;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class WizardServicesProxyTest extends TestCase {
+    
+    public WizardServicesProxyTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+
+    /**
+     * Test of getRolesByUser method, of class webservices.WizardServicesProxy.
+     */
+    public void testGetRolesByUser() {
+        System.out.println("getRolesByUser");
+        
+        String login = "";
+        String password = "";
+        String adresseServeur = "localhost:8084/remote";
+        
+
+        ArrayList<RoleDescriptor> result = WizardServicesProxy.getRolesByUser(login, password, adresseServeur);
+        assertNotNull(result);
+        assertTrue(result.size()==0);       
+        
+        login = "testSansBD";
+        password = "testSansBD";
+        adresseServeur = "http://localhost:8084/remote";        
+
+        result = WizardServicesProxy.getRolesByUser(login, password, adresseServeur);
+        assertNotNull(result);
+        assertTrue(result.size()>=1);       
+
+    }
+    
+}
