21467ff88a34dcd61436cbfea332d58bbfb7a14b
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-11-30 18:53:14 +0000

    [PSI2] Creation du tag pre-revue 20061130

---
+++ psi2/tags/trunk/src/woops2/business/wilosuser/LoginService.java
@@ -0,0 +1,95 @@
+
+package woops2.business.wilosuser ;
+
+import java.util.Set ;
+
+import org.apache.commons.logging.Log ;
+import org.apache.commons.logging.LogFactory ;
+import org.springframework.transaction.annotation.Propagation ;
+import org.springframework.transaction.annotation.Transactional ;
+
+import woops2.hibernate.wilosuser.WilosUserDao ;
+import woops2.model.wilosuser.Participant ;
+import woops2.model.wilosuser.WilosUser ;
+
+/**
+ * @author Marseyeah
+ * @author Sakamakak
+ * 
+ * This class represents ... TODO
+ * 
+ */
+@ Transactional (readOnly = false, propagation = Propagation.REQUIRED)
+public class LoginService {
+
+	private WilosUserDao wilosUserDao ;
+
+	protected final Log logger = LogFactory.getLog(this.getClass()) ;
+
+	/**
+	 * Getter of wilosUserDao.
+	 * 
+	 * @return the wilosUserDao.
+	 */
+	public WilosUserDao getWilosUserDao() {
+		return this.wilosUserDao ;
+	}
+
+	/**
+	 * Setter of wilosUserDao.
+	 * 
+	 * @param _wilosUserDao
+	 *            The wilosUserDao to set.
+	 */
+	public void setWilosUserDao(WilosUserDao _wilosUserDao) {
+		this.wilosUserDao = _wilosUserDao ;
+	}
+
+	/**
+	 * TODO Method description
+	 * 
+	 * @param _login
+	 * @param _password
+	 * @return true if the login and the password exist
+	 */
+	public WilosUser getAuthentifiedUser(String _login, String _password) {
+		Set<WilosUser> wilosUsers = this.wilosUserDao.getAllWilosUsers() ;
+		for(WilosUser user : wilosUsers){
+			if(user.getLogin().equals(_login) && user.getPassword().equals(_password)){
+				this.logger.debug("### connection OK ###") ;
+				return user ;
+			}
+			else{
+				this.logger.debug("### connection PAS OK ###") ;
+			}
+		}
+		return null ;
+	}
+	
+	/**
+	 * Check if the login is already used
+	 *
+	 * @param _login
+	 * @return True is the login is already present 
+	 */
+	public boolean loginExist(String _login) {
+		boolean found = false;
+		Set<WilosUser> wilosUsers = this.wilosUserDao.getAllWilosUsers() ;
+		for(WilosUser user : wilosUsers)
+		{
+			if(user.getLogin().equals(_login))
+			{
+				this.logger.debug("### new login already exists ###") ;
+				found = true ;
+			}
+			else{
+				this.logger.debug("### new login is ok ###") ;
+				found = false;
+				
+			}
+		}
+		return found;
+	}
+
+
+}
---
+++ psi2/tags/trunk/src/woops2/hibernate/wilosuser/AdministratorDao.java
@@ -0,0 +1,63 @@
+
+package woops2.hibernate.wilosuser ;
+
+import java.util.HashSet ;
+import java.util.Set ;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport ;
+
+import woops2.model.wilosuser.Administrator ;
+
+/**
+ * AdministratorDao manage requests from the system to store Administrator to the database.
+ * 
+ * @author Yoann Lopes
+ * @author Martial Lapeyre
+ */
+public class AdministratorDao extends HibernateDaoSupport {
+
+	/**
+	 * Save or update an Administator.
+	 * 
+	 * @param _administrator
+	 */
+	public void saveOrUpdateAdministrator(Administrator _administrator) {
+		this.getHibernateTemplate().saveOrUpdate(_administrator) ;
+	}
+
+	/**
+	 * Return a list of administrators.
+	 * 
+	 * @return
+	 */
+	public Set<Administrator> getAllAdministrators() {
+		Set<Administrator> loadAll = new HashSet<Administrator>() ;
+		loadAll.addAll(this.getHibernateTemplate().loadAll(Administrator.class)) ;
+		return loadAll ;
+	}
+
+	/**
+	 * Return the administrator which have the id _id.
+	 * 
+	 * @param _id
+	 * @return
+	 */
+	public Administrator getAdministrator(String _id) {
+		return (Administrator) this.getHibernateTemplate().get(Administrator.class, _id) ;
+	}
+
+	/**
+	 * Delete the administrator.
+	 * 
+	 * @param _administrator
+	 */
+	public void deleteAdministrator(Administrator _administrator) {
+		try{
+			this.getHibernateTemplate().delete(_administrator) ;
+		}
+		catch(Exception sose){
+			// Catch normally errors when we delete an unexisting administrator into the db.
+			logger.error("#### ERROR #### --- AdministratorDao => deleteAdministrator : trying to delete unexisting object \n" + sose) ;
+		}
+	}
+}
