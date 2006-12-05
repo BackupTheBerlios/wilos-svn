e15236babeb22efeb6d3c16914b88cd0d52f78c2
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-05 20:51:19 +0000

    [Woops2] Passage du projet en préparation du merge == Wilos

---
+++ woops2/trunk/src/wilos/model/spem2/role/RoleDescriptor.java
@@ -0,0 +1,322 @@
+
+package wilos.model.spem2.role ;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import wilos.model.spem2.breakdownelement.BreakdownElement;
+import wilos.model.spem2.task.TaskDescriptor;
+import woops2.model.wilosuser.Participant;
+
+/**
+ * 
+ * A RoleDefinition Descriptor represents a RoleDefinition in the context of one specific Activity.
+ * Every breakdown structure can define different relationships of RoleDefinition Descriptors to
+ * TaskDefinition Descriptors and Work Product Descriptors. Therefore one RoleDefinition can be
+ * represented by many RoleDefinition Descriptors each within the context of an Activity with its
+ * own set of relationships.
+ * 
+ * @author deder
+ * 
+ */
+public class RoleDescriptor extends BreakdownElement implements Cloneable {
+
+	private RoleDefinition roleDefinition ;
+
+	/**
+	 * The main tasks of the roleDefinition
+	 */
+	private Set<TaskDescriptor> primaryTasks ;
+
+	/**
+	 * The additional tasks of the roleDefinition
+	 */
+	private Set<TaskDescriptor> additionalTasks ;
+
+	/**
+	 * The participants of the RoleDescriptor.
+	 */
+	private Set<Participant> participants ;
+
+	/**
+	 * Getter of participant.
+	 * 
+	 * @return t
+	 * he participants.
+	 */
+	public Set<Participant> getParticipants() {
+		return this.participants ;
+	}
+
+	/**
+	 * Setter of participants.
+	 * 
+	 * @param _participants
+	 *            The participants to set.
+	 */
+	@ SuppressWarnings ("unused")
+	private void setParticipants(Set<Participant> _participants) {
+		this.participants = _participants ;
+	}
+
+	public RoleDescriptor() {
+		super() ;
+		this.primaryTasks = new HashSet<TaskDescriptor>() ;
+		this.additionalTasks = new HashSet<TaskDescriptor>() ;
+		this.participants = new HashSet<Participant>() ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@ Override
+	public RoleDescriptor clone() throws CloneNotSupportedException {
+		RoleDescriptor roleDescriptor = new RoleDescriptor() ;
+		roleDescriptor.copy(this) ;
+		return roleDescriptor ;
+	}
+
+	/**
+	 * Copy the object.
+	 * 
+	 * @param _roleDescriptor
+	 *            The RoleDescriptor to copy.
+	 */
+	protected void copy(final RoleDescriptor _roleDescriptor) {
+		super.copy(_roleDescriptor) ;
+		this.setAdditionalTasks(_roleDescriptor.getAdditionalTasks()) ;
+		this.setPrimaryTasks(_roleDescriptor.getPrimaryTasks()) ;
+		this.setRoleDefinition(_roleDescriptor.getRoleDefinition()) ;
+		this.setParticipants(_roleDescriptor.getParticipants()) ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see woops2.model.breakdownelement.BreakdownElement#equals(java.lang.Object)
+	 */
+	public boolean equals(Object obj) {
+		if(obj instanceof RoleDescriptor == false){
+			return false ;
+		}
+		if(this == obj){
+			return true ;
+		}
+
+		RoleDescriptor roleDescriptor = (RoleDescriptor) obj ;
+		return new EqualsBuilder().appendSuper(super.equals(roleDescriptor)).append(this.roleDefinition, roleDescriptor.roleDefinition).isEquals() ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see woops2.model.breakdownelement.BreakdownElement#hashCode()
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).append(this.roleDefinition).toHashCode() ;
+	}
+
+	/*
+	 * relation between RoleDescriptor and RoleDefinition
+	 */
+
+	/**
+	 * Add a RoleDefinition to the Set
+	 * 
+	 * @param _role
+	 */
+	public void addRoleDefinition(RoleDefinition _role) {
+		this.roleDefinition = _role ;
+		_role.getRoleDescriptors().add(this) ;
+	}
+
+	/**
+	 * remove a RoleDefinition
+	 * 
+	 * @param _role
+	 */
+	public void removeRoleDefinition(RoleDefinition _role) {
+		_role.getRoleDescriptors().remove(this) ;
+		this.roleDefinition = null ;
+	}
+
+	/*
+	 * relation mainrole between RoleDescriptor and TaskDescriptor
+	 */
+
+	/**
+	 * add a taskDescritor to the Set
+	 * 
+	 * @param _task
+	 */
+	public void addPrimaryTask(TaskDescriptor _task) {
+		this.primaryTasks.add(_task) ;
+		_task.setMainRole(this) ;
+	}
+
+	/**
+	 * remove a taskDescriptor
+	 * 
+	 * @param _task
+	 */
+	public void removePrimaryTask(TaskDescriptor _task) {
+		_task.setMainRole(null) ;
+		this.primaryTasks.remove(_task) ;
+	}
+
+	/**
+	 * 
+	 * @param _role
+	 */
+	public void addAllPrimaryTasks(Set<TaskDescriptor> _role) {
+		for(TaskDescriptor _role1 : _role){
+			_role1.addMainRole(this) ;
+		}
+	}
+
+	/**
+	 * Remove from an RoleDescriptor all its primaryTask
+	 * 
+	 */
+	public void removeAllPrimaryTasks() {
+		for(TaskDescriptor _task : this.primaryTasks){
+			_task.setMainRole(null) ;
+		}
+		this.primaryTasks.clear() ;
+	}
+
+	/*
+	 * relation additionnalRoles between RoleDescriptor and TaskDescriptor
+	 */
+
+	/**
+	 * add a TaskDescriptor to the Set
+	 * 
+	 * @param _task
+	 */
+	public void addAdditionalTask(TaskDescriptor _task) {
+		this.additionalTasks.add(_task) ;
+		_task.getAdditionalRoles().add(this) ;
+	}
+
+	/**
+	 * remove a TaskDescriptor
+	 * 
+	 * @param _task
+	 */
+	public void removeAdditionalTask(TaskDescriptor _task) {
+		_task.getAdditionalRoles().remove(this) ;
+		this.additionalTasks.remove(_task) ;
+	}
+
+	/**
+	 * 
+	 * @param _role
+	 */
+	public void addAllAdditionalTasks(Set<TaskDescriptor> _task) {
+		for(TaskDescriptor task : _task){
+			task.addAdditionalRole(this) ;
+		}
+	}
+
+	/**
+	 * remove all the roleDescriptors
+	 * 
+	 */
+	public void removeAllAdditionalTasks() {
+		for(TaskDescriptor _task : this.additionalTasks){
+			_task.getAdditionalRoles().remove(this) ;
+		}
+		this.additionalTasks.clear() ;
+	}
+
+	public void addParticipant(Participant _participant) {
+		this.participants.add(_participant) ;
+		//_participant.getRolesListForAProject().add(this) ;
+	}
+	
+	public void addAllParticipants(Set<Participant> _participants) {
+		for(Participant participant : _participants){
+			//participant.addToRoleDescriptor(this) ;
+		}
+	}
+
+	public void removeParticipant(Participant _participant) {
+		//_participant.getRolesListForAProject().remove(this) ;
+		this.participants.remove(_participant) ;
+	}
+
+	public void removeAllParticipants() {
+		for(Participant _participant : this.participants){
+			//_participant.getRolesListForAProject().remove(this) ;
+		}
+		this.participants.clear() ;
+	}
+
+	/**
+	 * Getter of roleDefinition.
+	 * 
+	 * @return the roleDefinition.
+	 */
+	public RoleDefinition getRoleDefinition() {
+		return this.roleDefinition ;
+	}
+
+	/**
+	 * Setter of roleDefinition.
+	 * 
+	 * @param _roleDefinition
+	 *            The roleDefinition to set.
+	 */
+	public void setRoleDefinition(RoleDefinition _roleDefinition) {
+		this.roleDefinition = _roleDefinition ;
+	}
+
+	/**
+	 * Getter of primaryTasks.
+	 * 
+	 * @return the primaryTasks.
+	 */
+	@ SuppressWarnings ("unused")
+	public Set<TaskDescriptor> getPrimaryTasks() {
+		return this.primaryTasks ;
+	}
+
+	/**
+	 * Setter of primaryTasks.
+	 * 
+	 * @param _primaryTasks
+	 *            The primaryTasks to set.
+	 */
+	@ SuppressWarnings ("unused")
+	private void setPrimaryTasks(Set<TaskDescriptor> _primaryTasks) {
+		this.primaryTasks = _primaryTasks ;
+	}
+
+	/**
+	 * Getter of additionalTasks.
+	 * 
+	 * @return the additionalTasks.
+	 */
+	public Set<TaskDescriptor> getAdditionalTasks() {
+		return this.additionalTasks ;
+	}
+	
+
+	/**
+	 * Setter of additionalTasks.
+	 * 
+	 * @param _additionalTasks
+	 *            The additionalTasks to set.
+	 */
+	@ SuppressWarnings ("unused")
+	private void setAdditionalTasks(Set<TaskDescriptor> _additionalTasks) {
+		this.additionalTasks = _additionalTasks ;
+	}
+
+}
