d2f0a40de8ed09c9096182072e52633f863644c9
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-05 20:52:48 +0000

    [Woops2] Passage du projet en préparation du merge == Wilos

---
+++ woops2/trunk/test/wilos/test/hibernate/element/ElementDaoTest.java
@@ -0,0 +1,154 @@
+package wilos.test.hibernate.element;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import wilos.hibernate.spem2.element.ElementDao;
+import wilos.model.spem2.element.Element;
+import wilos.test.TestConfiguration;
+
+/**
+ * Unit test for ElementDao
+ * 
+ * @author deder
+ */
+public class ElementDaoTest extends TestCase {
+	
+	private ElementDao elementDao = null;
+
+	private Element element = null;
+
+	public static final String NAME = "thisElt";
+
+	public static final String DESCRIPTION = "elt description";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// Get the ElementDao Singleton for managing Element data
+		this.elementDao = (ElementDao) TestConfiguration.getInstance().getApplicationContext().getBean(
+				"ElementDao");
+
+		// Create empty Activity
+		this.element = new Element();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+
+		// Delete the tmp element from the database.
+		try {
+			this.elementDao.getHibernateTemplate().delete(this.element);
+		} catch (Exception exception) {
+			// None.
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link wilos.hibernate.spem2.element.ElementDao#saveOrUpdateActivity(wilos.model.spem2.element.Element)}.
+	 * 
+	 */
+	public void testSaveOrUpdateElement() {
+		// Rk: the setUp method is called here.
+
+		// Save the element with the method to test.
+		this.elementDao.saveOrUpdateElement(this.element);
+
+		// Check the saving.
+		String id = this.element.getId();
+		Element elementTmp = (Element) this.elementDao.getHibernateTemplate()
+				.load(Element.class, id);
+		assertNotNull(elementTmp);
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for
+	 * {@link wilos.hibernate.spem2.element.ElementDao#getAllElements()}.
+	 * 
+	 */
+	public void testgetAllElements() {
+		// Rk: the setUp method is called here.
+
+		// Save the element into the database.
+		this.elementDao.getHibernateTemplate().saveOrUpdate(this.element);
+
+		// Look if this element is also into the database and look if the size
+		// of the set is >= 1.
+		List<Element> elements = this.elementDao.getAllElements();
+		assertNotNull(elements);
+		assertTrue(elements.size() >= 1);
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link wilos.hibernate.spem2.element.ElementDao#getElement()}.
+	 * 
+	 */
+	public void testGetElement() {
+		// Rk: the setUp method is called here.
+
+		// Add properties to the element.
+		this.element.setName(NAME);
+		this.element.setDescription(DESCRIPTION);
+
+		// Save the element into the database.
+		this.elementDao.getHibernateTemplate().saveOrUpdate(this.element);
+		String id = this.element.getId();
+
+		// Test the method getElement with an existing element.
+		Element elementTmp = this.elementDao.getElement(id);
+		assertNotNull(elementTmp);
+		assertEquals("Name", elementTmp.getName(), NAME);
+		assertEquals("Description", elementTmp.getDescription(), DESCRIPTION);
+
+		// Test the method getElement with an unexisting element.
+		this.elementDao.getHibernateTemplate().delete(this.element);
+		elementTmp = this.elementDao.getElement(id);
+		assertNull(elementTmp);
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for
+	 * {@link wilos.hibernate.spem2.element.ElementDao#deleteElement()}.
+	 * 
+	 */
+	public void testDeleteElement() {
+		// Rk: the setUp method is called here.
+
+		// Save the element into the database.
+		this.elementDao.getHibernateTemplate().saveOrUpdate(this.element);
+		String id = this.element.getId();
+
+		// Test the method deleteElement with an element existing into the db.
+		this.elementDao.deleteElement(this.element);
+
+		// See if this.element is now absent in the db.
+		Element elementTmp = (Element) this.elementDao.getHibernateTemplate()
+				.get(Element.class, id);
+		assertNull(elementTmp);
+
+		// Test the method deleteElement with an element unexisting into the db.
+		// Normally here there are no exception thrown.
+		this.elementDao.deleteElement(this.element);
+
+		// Rk: the tearDown method is called here.
+	}
+}
