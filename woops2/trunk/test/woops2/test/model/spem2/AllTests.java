ff4bf2e7fc3c455555ac7186e33566480e9d48af
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-05 20:56:14 +0000

    [Woops2] Passage du projet en préparation du merge == Wilos

---
+++ woops2/trunk/test/woops2/test/model/spem2/AllTests.java
@@ -0,0 +1,27 @@
+
+package wilos.test.model.spem2 ;
+
+import junit.framework.Test ;
+import junit.framework.TestSuite ;
+
+/**
+ * @author deder
+ * 
+ */
+public class AllTests {
+
+	public static Test suite () {
+		TestSuite suite = new TestSuite("Test for woops2.test.model") ;
+		// $JUnit-BEGIN$
+		suite.addTest(wilos.test.model.spem2.process.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.activity.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.breakdownelement.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.workbreakdownelement.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.task.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.role.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.spem2.element.AllTests.suite()) ;
+		// $JUnit-END$
+		return suite ;
+	}
+
+}
