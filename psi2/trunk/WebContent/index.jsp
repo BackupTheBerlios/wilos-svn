e6272d8a2d747bf0bf0d444a315f2d9a7520ee35
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-08 18:07:16 +0000

    [PSI2] Reconstruction du trunk a partir de la branche à jour

---
+++ psi2/trunk/WebContent/index.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Wilos - feat. ajax</title>
+    </head>
+    <body>
+        <jsp:forward page="connect.iface" />
+    </body>
+</html>
---
+++ psi2/trunk/test/wilos/test/business/services/wilosuser/ProjectDirectorServiceTest.java
@@ -0,0 +1,52 @@
+package wilos.test.business.services.wilosuser;
+
+import junit.framework.TestCase;
+import wilos.business.services.wilosuser.ProjectDirectorService;
+import wilos.business.util.Security;
+import wilos.model.misc.wilosuser.ProjectDirector;
+import wilos.test.configuration.TestConfiguration;
+
+/**
+ * @author Martial
+ *
+ * This class represents ... TODO
+ *
+ */
+public class ProjectDirectorServiceTest extends TestCase {
+
+	private ProjectDirectorService projectDirectorService;
+	private ProjectDirector projectDirector;
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp() ;
+		this.projectDirectorService= (ProjectDirectorService) TestConfiguration.getInstance().getApplicationContext().getBean("ProjectDirectorService") ;
+		this.projectDirector=new ProjectDirector();
+		this.projectDirector.setLogin("john");
+		this.projectDirector.setName("georges");
+		this.projectDirector.setPassword("pass") ;
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+		this.projectDirectorService.getProjectDirectorDao().deleteProjectDirector(this.projectDirector) ;
+	}
+
+	/**
+	 * Test method for {@link woops2.business.wilosuser.ProjectDirectorService#saveProjectDirector(woops2.model.wilosuser.ProjectDirector)}.
+	 */
+	public void testSaveProjectDirector() {
+		this.projectDirectorService.saveProjectDirector(this.projectDirector);
+		ProjectDirector ProjectDirectorTmp = (ProjectDirector) this.projectDirectorService.getProjectDirectorDao().getProjectDirector("john");
+		assertNotNull(ProjectDirectorTmp);
+		assertEquals(ProjectDirectorTmp.getName(), "georges") ;
+		assertEquals(ProjectDirectorTmp.getLogin(), "john") ;
+		assertEquals(ProjectDirectorTmp.getPassword(), Security.encode("pass")) ;
+	}
+
+}
