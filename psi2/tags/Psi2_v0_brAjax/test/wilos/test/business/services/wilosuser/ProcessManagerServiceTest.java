47296f61b7ca636ab58931944f98a244d1850838
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-09 11:24:04 +0000

    [PSI2] Creation tag a partir de la branche Ajax

---
+++ psi2/tags/Psi2_v0_brAjax/test/wilos/test/business/services/wilosuser/ProcessManagerServiceTest.java
@@ -0,0 +1,52 @@
+package wilos.test.business.services.wilosuser;
+
+import junit.framework.TestCase;
+import wilos.test.configuration.TestConfiguration;
+import wilos.business.services.wilosuser.ProcessManagerService;
+import wilos.business.util.Security;
+import wilos.model.misc.wilosuser.ProcessManager;
+
+/**
+ * @author Martial
+ *
+ * This class represents ... TODO
+ *
+ */
+public class ProcessManagerServiceTest extends TestCase {
+
+	private ProcessManagerService processManagerService;
+	private ProcessManager processManager;
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp() ;
+		this.processManagerService= (ProcessManagerService) TestConfiguration.getInstance().getApplicationContext().getBean("ProcessManagerService") ;
+		this.processManager=new ProcessManager();
+		this.processManager.setLogin("john");
+		this.processManager.setName("georges");
+		this.processManager.setPassword("pass") ;
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+		this.processManagerService.getProcessManagerDao().deleteProcessManager(this.processManager) ;
+	}
+
+	/**
+	 * Test method for {@link woops2.business.wilosuser.ProcessManagerService#saveProcessManager(woops2.model.wilosuser.ProcessManager)}.
+	 */
+	public void testSaveProcessManager() {
+		this.processManagerService.saveProcessManager(this.processManager);
+		ProcessManager ProcessManagerTmp = (ProcessManager) this.processManagerService.getProcessManagerDao().getProcessManager("john");
+		assertNotNull(ProcessManagerTmp);
+		assertEquals(ProcessManagerTmp.getName(), "georges") ;
+		assertEquals(ProcessManagerTmp.getLogin(), "john") ;
+		assertEquals(ProcessManagerTmp.getPassword(), Security.encode("pass")) ;
+	}
+
+}
---
+++ psi2/tags/Psi2_v0_brAjax/test/wilos/test/configuration/TestConfiguration.java
@@ -0,0 +1,33 @@
+package wilos.test.configuration;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Martial
+ *
+ * This class represents ... TODO
+ *
+ */
+public class TestConfiguration {
+
+	// Getback the application context from the spring configuration file
+	private ApplicationContext applicationContext = null;
+
+	private static TestConfiguration testConfiguration = null;
+
+	private TestConfiguration() {
+		this.applicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+	}
+
+	public static TestConfiguration getInstance() {
+		if (testConfiguration == null)
+			testConfiguration = new TestConfiguration();
+		return testConfiguration;
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return this.applicationContext;
+	}
+}
