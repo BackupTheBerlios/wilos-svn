ad988b8713e24c75b34b06b801ccc27eb1d67a27
toine <toine@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-09 20:13:15 +0000

    [SPELP] MOdication du proxy pour faire le lien avec getParticipant

--- spelp/trunk/wizard/src/wilos/presentation/assistant/webservices/WizardServicesProxy.java
+++ spelp/trunk/wizard/src/wilos/presentation/assistant/webservices/WizardServicesProxy.java
@@ -13,37 +13,48 @@ import wilos.model.spem2.breakdownelement.BreakdownElement;
 import wilos.model.spem2.role.RoleDescriptor;
 import wilos.model.spem2.process.Process;
 import wilos.business.webservices.*;
+import wilos.model.spem2.task.TaskDescriptor;
 
 public class WizardServicesProxy {
         public static String ENDPOINT = "/WizardServices?wsdl";
         public static String URLWebService = "http://webservices.business.wilos/";
         public static String nameWebService = "WizardServicesService";
     
-	public static ArrayList<RoleDescriptor> getRolesByUser(String login, String password, String adresseServeur) {	
+	public static ArrayList<RoleDescriptor> getRolesByUser(String login, String password, String address) {	
             ArrayList<RoleDescriptor> myRoleListe = new  ArrayList<RoleDescriptor>();
-            List<Process> pros = getAllProcess(login, password,adresseServeur);
+            /*List<Process> pros = getAllProcess(login, password,adresseServeur);
             for (Process pro : pros) {
                 for (BreakdownElement bre: pro.getBreakDownElements()) {
                     if (bre instanceof RoleDescriptor) {
                         myRoleListe.add((RoleDescriptor)bre);
                     }
                 }
+            }*/
+            Participant myParticipant = getParticipant(login,password,address);
+            if (myParticipant!= null) {
+                for (RoleDescriptor rd : myParticipant.getRolesListForAProject()) {
+                    myRoleListe.add(rd);
+                }
             }
-
             return myRoleListe;
 	}
         
         public static Participant getParticipant(String login, String password, String address)
         {
             Participant myParticipant = null;
-            
             try { 
                 Service service = Service.create(new URL(address+ENDPOINT), new QName(URLWebService, nameWebService));
                 WizardServices port = service.getPort(WizardServices.class);
                 //myWilosUser = port.getWilosUser(login,password);
-
+                  XStream xstream = new XStream(); 
+                  xstream.alias("wilos.business.transfertobject.ParticipantTO",Participant.class);
+                  xstream.alias("wilos.business.transfertobject.RoleDescriptorTO",RoleDescriptor.class);
+                  xstream.alias("wilos.business.transfertobject.TaskDescriptorTO",TaskDescriptor.class);
+                  String result = port.getParticipant(login,password);
+                  /*System.out.println(result);*/
+                myParticipant = (Participant)xstream.fromXML(result);
             }
-            catch (Exception e) {
+            catch (java.lang.Exception e) {
                 e.printStackTrace();
             }
             return myParticipant;
@@ -61,7 +72,7 @@ public class WizardServicesProxy {
                     pros.add((Process)xstream.fromXML(strxml));
                 }
 
-            } catch (Exception ex) {
+            } catch (java.lang.Exception ex) {
                 ex.printStackTrace();
             }
              return pros;
