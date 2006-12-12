dbae576991e615ab8cbac19ce3f25057259846e8
jf_doya <jf_doya@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-12 16:03:43 +0000

    [SPELP] creation de l'arbre dans StepPanel redimensionnement de l'icone du bouton

--- spelp/trunk/wizard/src/wilos/presentation/assistant/view/panels/tabs/StepPanel.java
+++ spelp/trunk/wizard/src/wilos/presentation/assistant/view/panels/tabs/StepPanel.java
@@ -1,16 +1,28 @@
 package wilos.presentation.assistant.view.panels.tabs;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.util.Iterator;
+import java.util.SortedSet;
 
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import wilos.model.spem2.task.Step;
+import wilos.model.spem2.task.TaskDefinition;
 import wilos.model.spem2.task.TaskDescriptor;
+
+
 
 public class StepPanel extends JPanel {
 	private static StepPanel panel = null ;
 	private TaskDescriptor currentElement = null ;
+	private JTree stepTree = null;
+	DefaultMutableTreeNode rootNode;
+	DefaultMutableTreeNode taskNode;
+
+	
 	private StepPanel() {
 		super();
-		this.add(new JLabel("coucou"));
 		this.setVisible(false);
 	}
 
@@ -25,10 +37,35 @@ public class StepPanel extends JPanel {
 		return currentElement;
 	}
 
+	
+	
+	/**
+	 * create the tree of the new TaskDescriptor
+	 * @param currentElement : the new TaskDescriptor
+	 */
 	public void setCurrentElement(TaskDescriptor currentElement) {
 		this.currentElement = currentElement;
-		this.add(new JLabel(currentElement.getName()));
-	}
-	
 
+		// if stepTree exists, we remove it to replace it
+		if(stepTree != null)
+			this.remove(stepTree);
+		
+		// the title of the tree is the name of the task
+		rootNode = new DefaultMutableTreeNode(currentElement.getName());
+		stepTree = new JTree(rootNode);
+	
+		TaskDefinition td = currentElement.getTaskDefinition();
+		if(td != null) {
+			SortedSet stepList = td.getSteps();
+			if(stepList != null) {
+				Iterator itSteps = stepList.iterator();
+			
+				while(itSteps.hasNext()) {
+					taskNode= new DefaultMutableTreeNode(((Step)itSteps.next()).getName(),false);
+					rootNode.add(taskNode);
+				}
+			}
+		}
+		this.add(stepTree);
+	}
 }
