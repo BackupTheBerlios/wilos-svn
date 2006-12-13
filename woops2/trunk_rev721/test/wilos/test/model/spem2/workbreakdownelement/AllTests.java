f6fb2bdee95e74577e7855e31b2dcced1177bf0a
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-13 09:33:32 +0000

    [Woops2] prepare rev721

---
+++ woops2/trunk_rev721/test/wilos/test/model/spem2/workbreakdownelement/AllTests.java
@@ -0,0 +1,20 @@
+package wilos.test.model.spem2.workbreakdownelement;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author deder
+ *
+ */
+public class AllTests {
+
+	public static Test suite () {
+		TestSuite suite = new TestSuite("Test for wilos.test.model.workbreakdownelement") ;
+		//$JUnit-BEGIN$
+		suite.addTestSuite(WorkBreakdownElementTest.class) ;
+		//$JUnit-END$
+		return suite ;
+	}
+
+}
