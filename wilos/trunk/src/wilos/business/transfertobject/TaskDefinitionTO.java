1f2db400f88529be3ec7e6955e9e5bcefb1af3bf
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-13 16:32:44 +0000

    [wilos] Integration: ajouts sources remote spelp

---
+++ wilos/trunk/src/wilos/business/transfertobject/TaskDefinitionTO.java
@@ -0,0 +1,48 @@
+/*
+ * TaskDefinitionTO.java
+ *
+ * Created on 9 décembre 2006, 14:57
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package wilos.business.transfertobject;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import wilos.model.spem2.task.Step;
+import wilos.model.spem2.task.TaskDefinition;
+
+/**
+ *
+ * @author toine
+ */
+public class TaskDefinitionTO extends TaskDefinition implements Serializable {
+    
+    
+    /** Creates a new instance of TaskDefinitionTO */
+    public TaskDefinitionTO() {
+        
+    }
+    
+    public TaskDefinitionTO(TaskDefinition myTD) {
+        this.setName(myTD.getName());
+        this.setGuid(myTD.getGuid());
+        this.setDescription(myTD.getDescription());                
+        
+        SortedSet<Step> stepTos= new TreeSet<Step>();
+        for (Step s : myTD.getSteps()) {
+            stepTos.add(new StepTO(s));
+        }
+        this.addAllSteps(stepTos);
+    }
+
+    
+    
+
+}
