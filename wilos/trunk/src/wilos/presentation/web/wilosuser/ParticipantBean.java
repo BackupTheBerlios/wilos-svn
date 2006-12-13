49f1e9967b16178e74fb3f29074d6819e16fdb83
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2006-12-13 16:13:05 +0000

    [wilos] Integration: ajouts sources et webcontent psi2

---
+++ wilos/trunk/src/wilos/presentation/web/wilosuser/ParticipantBean.java
@@ -0,0 +1,197 @@
+package wilos.presentation.web.wilosuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import wilos.business.services.wilosuser.LoginService;
+import wilos.business.services.wilosuser.ParticipantService;
+import wilos.model.spem2.role.RoleDescriptor;
+import wilos.model.misc.wilosuser.Participant;
+import wilos.presentation.web.template.MenuBean;
+
+/**
+ * Managed-Bean link to participantSubscribe.jspx
+ * 
+ * @author BlackMilk
+ * @author Mikamikaze
+ * @author Sakamakak
+ */
+public class ParticipantBean {
+
+	private List<RoleDescriptor> rolesList;
+
+	private ParticipantService participantService;
+
+	private LoginService loginService;
+
+	private Participant participant;
+
+	private String passwordConfirmation;
+
+	protected final Log logger = LogFactory.getLog(this.getClass());
+
+	/**
+	 * Constructor.
+	 * 
+	 */
+	public ParticipantBean() {
+		this.logger.debug("--- ParticipantBean --- == creating ..." + this);
+		this.participant = new Participant();
+	}
+
+	/**
+	 * Method for saving participant data from form
+	 * 
+	 * @return
+	 */
+	public String saveParticipantAction() {
+		String url = "connect";
+		boolean loginExists = this.loginService.loginExist(this.participant
+				.getLogin());
+		if (loginExists == true) {
+			FacesMessage message = new FacesMessage();
+			message.setDetail("Ce Login existe deja");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			facesContext.addMessage(null, message);
+			url = "createParticipant";			
+		} else {
+			this.participantService.saveParticipant(this.participant);
+			//url = "connect";
+			url="wilos"; //Passer eventuellement sur une page de confirmation
+			changeContentPage(url);
+		}
+		//return url;
+		/*Navigation*/
+		return "";
+	}
+
+	/**
+	 * Method designed to change main page content after
+	 * participant subscription
+	 * @param url
+	 */
+	public void changeContentPage(String url) {
+		// TODO factoriser cette fonction pr eviter de l'avoir
+		// dans chaque Bean ;)
+		FacesContext facesContext = FacesContext.getCurrentInstance() ;
+		Object menuObject =
+            facesContext.getApplication()
+                    .createValueBinding("#{menu}")
+                    .getValue(facesContext);
+		if (menuObject != null &&
+                menuObject instanceof MenuBean) {
+			
+                MenuBean menuBean = (MenuBean) menuObject;
+                menuBean.changePage(url);
+		}
+	}
+
+	public void testTransactionActionListener(ActionEvent e) {
+		// this.participantManager.Test();
+	}
+
+	/**
+	 * Getter of rolesList.
+	 * 
+	 * @return the rolesList.
+	 */
+	public List<RoleDescriptor> getRolesList() {
+		this.rolesList = new ArrayList<RoleDescriptor>();
+		rolesList.addAll(this.participantService.getRolesList());
+		this.logger.debug("roles list =" + this.rolesList);
+		return this.rolesList;
+	}
+
+	/**
+	 * Getter of participant.
+	 * 
+	 * @return the participant.
+	 */
+	public Participant getParticipant() {
+		return this.participant;
+	}
+
+	/**
+	 * Setter of participant.
+	 * 
+	 * @param _participant
+	 *            The participant to set.
+	 */
+	public void setParticipant(Participant _participant) {
+		this.logger.debug("### Participant = " + _participant + " ###");
+		this.participant = _participant;
+	}
+
+	/**
+	 * Getter of participantService.
+	 * 
+	 * @return the participantService.
+	 */
+	public ParticipantService getParticipantService() {
+		return this.participantService;
+	}
+
+	/**
+	 * Getter of loginService.
+	 *
+	 * @return the loginService.
+	 */
+	public LoginService getLoginService() {
+		return this.loginService ;
+	}
+
+	/**
+	 * Setter of loginService.
+	 *
+	 * @param _loginService The loginService to set.
+	 */
+	public void setLoginService(LoginService _loginService) {
+		this.loginService = _loginService ;
+	}
+
+	/**
+	 * Getter of passwordConfirmation.
+	 *
+	 * @return the passwordConfirmation.
+	 */
+	public String getPasswordConfirmation() {
+		return this.passwordConfirmation ;
+	}
+
+	/**
+	 * Setter of passwordConfirmation.
+	 *
+	 * @param _passwordConfirmation The passwordConfirmation to set.
+	 */
+	public void setPasswordConfirmation(String _passwordConfirmation) {
+		this.passwordConfirmation = _passwordConfirmation ;
+	}
+
+	/**
+	 * Setter of rolesList.
+	 *
+	 * @param _rolesList The rolesList to set.
+	 */
+	public void setRolesList(List<RoleDescriptor> _rolesList) {
+		this.rolesList = _rolesList ;
+	}
+
+	/**
+	 * Setter of participantService.
+	 * 
+	 * @param _participantService
+	 *            The participantService to set.
+	 */
+	public void setParticipantService(ParticipantService _participantService) {
+		this.participantService = _participantService;
+	}
+
+}
---
+++ wilos/trunk/src/wilos/presentation/web/wilosuser/ProcessManagerBean.java
@@ -0,0 +1,132 @@
+
+package wilos.presentation.web.wilosuser ;
+
+import javax.faces.application.FacesMessage ;
+import javax.faces.context.FacesContext ;
+
+import org.apache.commons.logging.Log ;
+import org.apache.commons.logging.LogFactory ;
+
+import wilos.business.services.wilosuser.LoginService ;
+import wilos.business.services.wilosuser.ProcessManagerService ;
+import wilos.model.misc.wilosuser.ProcessManager ;
+
+/**
+ * Managed-Bean link to processmanager_create.jsp
+ * 
+ * @author Marseyeah
+ */
+public class ProcessManagerBean {
+	private ProcessManagerService processManagerService ;
+
+	private ProcessManager processManager ;
+
+	private LoginService loginService ;
+
+	private String passwordConfirmation ;
+
+	protected final Log logger = LogFactory.getLog(this.getClass()) ;
+
+	/**
+	 * Constructor.
+	 * 
+	 */
+	public ProcessManagerBean() {
+		this.logger.debug("--- ProcessManagerBean --- == creating ..." + this) ;
+		this.processManager = new ProcessManager() ;
+	}
+
+	/**
+	 * Method for saving processManager data from form
+	 * 
+	 * @return
+	 */
+	public String saveProcessManagerAction() {
+		String url = "admin_main" ;
+		if(this.loginService.loginExist(this.processManager.getLogin())){
+			FacesMessage message = new FacesMessage() ;
+			message.setDetail("Ce Login existe deja") ;
+			message.setSeverity(FacesMessage.SEVERITY_ERROR) ;
+			FacesContext facesContext = FacesContext.getCurrentInstance() ;
+			facesContext.addMessage(null, message) ;
+			url = "processManager_create" ;
+		}
+		else{
+			this.processManagerService.saveProcessManager(this.processManager) ;
+			url = "admin_main" ;
+		}
+		return url ;
+	}
+
+	/**
+	 * Getter of processManager.
+	 * 
+	 * @return the processManager.
+	 */
+	public ProcessManager getProcessManager() {
+		return this.processManager ;
+	}
+
+	/**
+	 * Setter of processManager.
+	 * 
+	 * @param _processManager
+	 *            The processManager to set.
+	 */
+	public void setProcessManager(ProcessManager _processManager) {
+		this.processManager = _processManager ;
+	}
+
+	/**
+	 * Getter of processManagerService.
+	 * 
+	 * @return the processManagerService.
+	 */
+	public ProcessManagerService getProcessManagerService() {
+		return this.processManagerService ;
+	}
+
+	/**
+	 * Setter of processManagerService.
+	 * 
+	 * @param _processManagerService
+	 *            The processManagerService to set.
+	 */
+	public void setProcessManagerService(ProcessManagerService _processManagerService) {
+		this.processManagerService = _processManagerService ;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getPasswordConfirmation() {
+		return passwordConfirmation ;
+	}
+
+	/**
+	 * @param passwordConfirmation
+	 */
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation ;
+	}
+
+	/**
+	 * Getter of loginService.
+	 * 
+	 * @return the loginService.
+	 */
+	public LoginService getLoginService() {
+		return this.loginService ;
+	}
+
+	/**
+	 * Setter of loginService.
+	 * 
+	 * @param _loginService
+	 *            The loginService to set.
+	 */
+	public void setLoginService(LoginService _loginService) {
+		this.loginService = _loginService ;
+	}
+
+}
