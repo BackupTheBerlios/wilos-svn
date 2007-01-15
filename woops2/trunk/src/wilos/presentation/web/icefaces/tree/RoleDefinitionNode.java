6cd99a12d81b1cbe3e88af36c4e5680673f3f172
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-15 19:55:15 +0000

    [Woops2] recup des sources

---
+++ woops2/trunk/src/wilos/presentation/web/icefaces/tree/RoleDefinitionNode.java
@@ -0,0 +1,27 @@
+package wilos.presentation.web.icefaces.tree;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import com.icesoft.faces.component.tree.IceUserObject;
+
+import wilos.model.spem2.role.RoleDefinition;
+
+/**
+ * @author deder
+ *
+ */
+public class RoleDefinitionNode extends DefaultMutableTreeNode{
+
+	private static final long serialVersionUID = 2966157406617774982L ;
+
+	public RoleDefinitionNode(RoleDefinition _roleDefinition){
+		super();
+		IceUserObject iceUserObject = new IceUserObject(this) ;
+		this.setUserObject(iceUserObject) ;
+
+		iceUserObject.setText(_roleDefinition.getName()) ;
+		iceUserObject.setLeaf(true) ;
+		iceUserObject.setLeafIcon("images/icon_role.gif");
+	}
+	
+}
