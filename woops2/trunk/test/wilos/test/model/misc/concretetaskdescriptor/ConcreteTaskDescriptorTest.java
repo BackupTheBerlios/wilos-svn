2a1cf2e1d6a5c0616a835e3dd0de368b4a8addbb
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-17 22:33:14 +0000

    [Woops2] Rajout des tests sur ConcreteTaskDescriptor.

--- woops2/trunk/test/wilos/test/model/misc/concretetaskdescriptor/ConcreteTaskDescriptorTest.java
+++ woops2/trunk/test/wilos/test/model/misc/concretetaskdescriptor/ConcreteTaskDescriptorTest.java
@@ -1,19 +1,53 @@
 package wilos.test.model.misc.concretetaskdescriptor;
 
 import junit.framework.TestCase;
+import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
+import wilos.model.spem2.task.TaskDescriptor;
 
 public class ConcreteTaskDescriptorTest extends TestCase {
 
-	public void testClone() {
-		fail("Not yet implemented");
+	private ConcreteTaskDescriptor concreteTaskDescriptor ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp() ;
+		this.concreteTaskDescriptor = new ConcreteTaskDescriptor() ;
+		this.concreteTaskDescriptor.setConcreteName("Concrete Name");
+		this.concreteTaskDescriptor.setAccomplishedTime(15);
+		this.concreteTaskDescriptor.setPlannedTime(24);
 	}
 
-	public void testGetState() {
-		fail("Not yet implemented");
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+	}
+	
+	public void testAddTaskDescriptor() {
+		TaskDescriptor taskDescriptor = new TaskDescriptor();
+		this.concreteTaskDescriptor.addTaskDescriptor(taskDescriptor);
+		assertNotNull(this.concreteTaskDescriptor.getTaskDescriptor());
+		assertEquals(this.concreteTaskDescriptor.getTaskDescriptor(), taskDescriptor);
+		assertTrue(taskDescriptor.getConcreteTaskDescriptors().size() == 1);
 	}
 
-	public void testSetState() {
-		fail("Not yet implemented");
+	public void testRemoveTaskDescriptor() {
+		TaskDescriptor taskDescriptor = new TaskDescriptor();
+		
+		this.concreteTaskDescriptor.addTaskDescriptor(taskDescriptor);
+		assertNotNull(this.concreteTaskDescriptor.getTaskDescriptor());
+		assertEquals(this.concreteTaskDescriptor.getTaskDescriptor(), taskDescriptor);
+		
+		this.concreteTaskDescriptor.removeTaskDescriptor(taskDescriptor);
+		assertNull(this.concreteTaskDescriptor.getTaskDescriptor());
+		assertTrue(taskDescriptor.getConcreteTaskDescriptors().size() == 0);
 	}
 
 }
