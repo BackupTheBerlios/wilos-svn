53824c926792d59ead2247c419bf55da7b465465
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-17 20:35:31 +0000

    [Woops2] Rajout de la notion de ConcreteTaskDescriptor. De plus, quelques que correction dans les junits.

---
+++ woops2/trunk/test/wilos/test/hibernate/misc/concretetaskdescriptor/ConcreteTaskDescriptorDaoTest.java
@@ -0,0 +1,23 @@
+package wilos.test.hibernate.misc.concretetaskdescriptor;
+
+import junit.framework.TestCase;
+
+public class ConcreteTaskDescriptorDaoTest extends TestCase {
+
+	public void testSaveOrUpdateConcreteTaskDescriptor() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAllConcreteTaskDescriptors() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetConcreteTaskDescriptor() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteConcreteTaskDescriptor() {
+		fail("Not yet implemented");
+	}
+
+}
