6c86ae27c42cefb8de96b833d9c3def9f9d76603
faure <faure@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-17 16:39:56 +0000

    [SPELP] methode isEmpty ok getFIle en cours

--- spelp/trunk/import_xml/src/wilos/business/services/util/xml/parser/ZIPUtils.java
+++ spelp/trunk/import_xml/src/wilos/business/services/util/xml/parser/ZIPUtils.java
@@ -1,17 +1,73 @@
 package wilos.business.services.util.xml.parser;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 public class ZIPUtils
 {
-
-	public ZIPUtils(File pathEmpty) {
-		// TODO Auto-generated constructor stub
+	static final int BUFFER = 2048;
+	
+	private ZipFile aZipFile ;
+	private File file ;
+	
+	public ZIPUtils(File file) {
+		
+			System.out.println(file.exists());
+			try {
+				aZipFile = new ZipFile (file);
+				this.file = file ;
+			} catch (ZipException e) {
+				if (file.exists()){
+					aZipFile = null ;
+				}
+				else {
+					// TODO Exception
+					e.printStackTrace();
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+		
 	}
 
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return true;
+		return (aZipFile == null) ;
+	}
+
+	public File getXMLFile() {
+		
+		File fileReturned = null ;
+		boolean trouve = false ;
+		if (!isEmpty()){
+			Enumeration entries = aZipFile.entries();
+			ZipEntry e = null;
+			byte data[] = new byte[BUFFER];
+			/* Processing entries */
+			while(entries.hasMoreElements() && !trouve) {
+				e = (ZipEntry)entries.nextElement();
+				trouve = e.getName().substring(e.getName().lastIndexOf(".")+1).equals("xml");
+				
+		   }
+		   if (trouve){
+			   
+		   }
+		}
+		return fileReturned ;
+		
 	}
 
 }
--- spelp/trunk/import_xml/test/wilos/test/business/services/util/xml/parser/ZIPUtilsTest.java
+++ spelp/trunk/import_xml/test/wilos/test/business/services/util/xml/parser/ZIPUtilsTest.java
@@ -2,17 +2,37 @@ package wilos.test.business.services.util.xml.parser;
 
 import java.io.File;
 
-import wilos.business.services.util.xml.parser.ZIPUtils;
-
 import junit.framework.TestCase;
+import wilos.business.services.util.xml.parser.ZIPUtils;
 
 public class ZIPUtilsTest extends TestCase
 {
 	public static File pathEmpty = new File("test"+ File.separator +"wilos"+ File.separator +"test"+File.separator+"business"+ File.separator+ "services" +File.separator +  "util" +File.separator  +  "xml" +File.separator  + "resources" +File.separator  + "empty.zip");
 	public static File pathMonTest = new File("test"+ File.separator +"wilos"+ File.separator +"test"+File.separator+"business"+ File.separator+ "services" +File.separator +  "util" +File.separator  +  "xml" +File.separator  + "resources" +File.separator  + "monTest.zip");
+	public static File pathBizarre = new File("test"+ File.separator +"wilos"+ File.separator +"test"+File.separator+"business"+ File.separator+ "services" +File.separator +  "util" +File.separator  +  "xml" +File.separator  + "resources" +File.separator  + "bizarre.zip");
+	public static File pathTest2Niveau = new File("test"+ File.separator +"wilos"+ File.separator +"test"+File.separator+"business"+ File.separator+ "services" +File.separator +  "util" +File.separator  +  "xml" +File.separator  + "resources" +File.separator  + "test2Niveau.zip");
 	
 	public void testIsEmpty (){
 		ZIPUtils monZip = new ZIPUtils (pathEmpty);
 		assertTrue("TEST AVEC ARCHIVE VIDE", monZip.isEmpty());
+		
+		ZIPUtils monZip2 = new ZIPUtils (pathMonTest);
+		assertFalse("TEST AVEC ARCHIVE NON VIDE",monZip2.isEmpty());
+		
+		ZIPUtils monZip3 = new ZIPUtils (pathBizarre);
+		assertFalse("TEST AVEC ARCHIVE NON VIDE",monZip2.isEmpty());
 	}
+	
+	public void testGetXMLFile () {
+//		ZIPUtils monZip = new ZIPUtils(pathEmpty);
+//		assertTrue("TEST getXML FICHIER VIDE",monZip.getXMLFile() == null);
+//		
+//		ZIPUtils monZip2 = new ZIPUtils(pathMonTest);
+//		File file = monZip2.getXMLFile() ;
+//		assertTrue("TEST getXML FICHIER VIDE",file.getName().equals("monTest.xml"));
+		
+		
+	}
+	
+	
 }
