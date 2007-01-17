24d00ab12ac723e099761059e2d04c4a4e66aa36
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-17 20:25:58 +0000

    [PSI2] modification du fichier insertAdmin.sql en ajoutant un manager et un directeur dans la BD

--- psi2/trunk/ant/insertAdmin.sql
+++ psi2/trunk/ant/insertAdmin.sql
@@ -1,4 +1,8 @@
-INSERT INTO `administrator` (`administrator_id`) VALUES ('2c90c28e1031312e01103131ecff0001');
+INSERT INTO `wilosuser` (`wilosuser_id`, `login`, `name`, `firstname`, `emailAddress`, `password`) VALUES 
+('2c90a1b21031b5b1011031b85b7c0001', 'pm', 'pm', 'pm', NULL, 'd6bc0a30ec44a6557cd8051867929628'),
+('2c90a1b21031b5b1011031b8a0360002', 'pd', 'pd', 'pd', NULL, 'f424233fe38a41de39cc3542ca9b1818'),
+('2c90c28e1031312e01103131ecff0001', 'admin', 'admin', 'admin', 'admin@wilos.com', '85aac14e99386ee8f68c89372821b1ca');
 
-INSERT INTO `wilosuser` (`wilosuser_id`, `login`, `name`, `firstname`, `emailAddress`, `password`) 
-VALUES ('2c90c28e1031312e01103131ecff0001', 'admin', 'admin', 'admin', 'admin@wilos.com', '85aac14e99386ee8f68c89372821b1ca');
+INSERT INTO `administrator` (`administrator_id`) VALUES ('2c90c28e1031312e01103131ecff0001');
+INSERT INTO `projectdirector` (`projectdirector_id`) VALUES ('2c90a1b21031b5b1011031b8a0360002');
+INSERT INTO `processmanager` (`processmanager_id`) VALUES ('2c90a1b21031b5b1011031b85b7c0001');
