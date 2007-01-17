0b436a90664ca3e01b87f0b6cd09a23589e1caca
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-17 13:33:22 +0000

    [woops2]: mise en place de la relation avec PSI2

--- woops2/trunk/src/wilos/model/spem2/process/Process.java
+++ woops2/trunk/src/wilos/model/spem2/process/Process.java
@@ -1,18 +1,22 @@
+package wilos.model.spem2.process;
 
-package wilos.model.spem2.process ;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.apache.commons.lang.builder.EqualsBuilder ;
-import org.apache.commons.lang.builder.HashCodeBuilder ;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
+import wilos.model.misc.project.Project;
 import wilos.model.spem2.activity.Activity;
 
 /**
  * 
- * A Process is a special Activity that describes a structure for particular types of development
- * projects. To perform such a development project a Processes would be 'instantiated' and adapted
- * for the specific situation. Process is an abstract class and this meta-model defines different
- * special types of Processes for different process management applications and different situations
- * of process reuse.
+ * A Process is a special Activity that describes a structure for particular
+ * types of development projects. To perform such a development project a
+ * Processes would be 'instantiated' and adapted for the specific situation.
+ * Process is an abstract class and this meta-model defines different special
+ * types of Processes for different process management applications and
+ * different situations of process reuse.
  * 
  * @author soosuske
  * @author deder
@@ -22,13 +26,15 @@ import wilos.model.spem2.activity.Activity;
 public class Process extends Activity implements Cloneable {
 
 	// The Project of Process
-	//	private Project project;
+	private Set<Project> projects;
+
 	/**
 	 * Default constructor
 	 * 
 	 */
 	public Process() {
-		super() ;
+		super();
+		this.projects = new HashSet<Project>();
 	}
 
 	/*
@@ -36,11 +42,11 @@ public class Process extends Activity implements Cloneable {
 	 * 
 	 * @see java.lang.Object#clone()
 	 */
-	@ Override
+	@Override
 	public Process clone() throws CloneNotSupportedException {
-		Process process = new Process() ;
-		process.copy(this) ;
-		return process ;
+		Process process = new Process();
+		process.copy(this);
+		return process;
 	}
 
 	/**
@@ -50,7 +56,7 @@ public class Process extends Activity implements Cloneable {
 	 *            The process to copy.
 	 */
 	protected void copy(final Process _process) {
-		super.copy(_process) ;
+		super.copy(_process);
 	}
 
 	/*
@@ -59,14 +65,15 @@ public class Process extends Activity implements Cloneable {
 	 * @see woops2.model.activity.Activity#equals(java.lang.Object)
 	 */
 	public boolean equals(Object obj) {
-		if(obj instanceof Process == false){
-			return false ;
+		if (obj instanceof Process == false) {
+			return false;
 		}
-		if(this == obj){
-			return true ;
+		if (this == obj) {
+			return true;
 		}
-		Process process = (Process) obj ;
-		return new EqualsBuilder().appendSuper(super.equals(process)).isEquals() ;
+		Process process = (Process) obj;
+		return new EqualsBuilder().appendSuper(super.equals(process))
+				.isEquals();
 	}
 
 	/*
@@ -75,24 +82,58 @@ public class Process extends Activity implements Cloneable {
 	 * @see woops2.model.activity.Activity#hashCode()
 	 */
 	public int hashCode() {
-		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).toHashCode() ;
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.toHashCode();
 	}
-	
+
 	/**
-	 * methode to Add a process to a project
-	 * @param project
+	 * remove a project
+	 * 
+	 * @param _project
 	 */
-	/*public addProject(Project project){
-		   this.project = project;
-		   project.getProcesss().add(this);
-		}*/ 
+	public void removeProject(Project _project) {
+		_project.setProcess(null);
+		this.projects.remove(_project);
+	}
+
 	/**
-	 * methode to remove a process to a project
-	 * @param project
+	 * add a project
+	 * 
+	 * @param _project
 	 */
-	/*
-	public removeFromProject(Project project){
-		   this.project = null;
-		   project.getProcesss().remove(this);
-		}*/ 
+	public void addProject(Project _project) {
+		this.projects.add(_project);
+		_project.setProcess(this);
+	}
+
+	/**
+	 * remove all project
+	 * 
+	 */
+	public void removeAllProjects() {
+		for (Project project : this.projects) {
+			project.removeFromProcess(this);
+		}
+		this.projects.clear();
+	}
+
+	/**
+	 * Add a RoleDescriptor collection to the RoleDescriptor collection of an
+	 * RoleDefinition
+	 * 
+	 * @param _role
+	 */
+	public void addAllProjects(Set<Project> _project) {
+		for (Project _proj1 : _project) {
+			_proj1.addProcess(this);
+		}
+	}
+
+	public Set<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(Set<Project> project) {
+		this.projects = project;
+	}
 }
--- woops2/trunk/test/wilos/test/model/spem2/process/ProcessTest.java
+++ woops2/trunk/test/wilos/test/model/spem2/process/ProcessTest.java
@@ -1,20 +1,30 @@
+package wilos.test.model.spem2.process;
 
-package wilos.test.model.spem2.process ;
+import java.util.HashSet;
+import java.util.Set;
 
-import junit.framework.TestCase ;
+import junit.framework.TestCase;
+import wilos.model.misc.project.Project;
 import wilos.model.spem2.process.Process;
 
 /**
+ * @author soosuske
  * @author deder
  * 
  */
 public class ProcessTest extends TestCase {
 
-	private Process process ;
+	private Process process;
 
-	public static final String PREFIX = "prefix" ;
+	public static final String PREFIX = "prefix";
 
-	public static final Boolean IS_OPTIONAL = true ;
+	public static final Boolean IS_OPTIONAL = true;
+
+	public static final String NAME = "thisRoleDescriptor";
+
+	public static final String NAME2 = "name1";
+
+	public static final String DESCRIPTION = "roleDescriptor description";
 
 	/*
 	 * (non-Javadoc)
@@ -22,10 +32,10 @@ public class ProcessTest extends TestCase {
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
-		super.setUp() ;
-		this.process = new Process() ;
-		this.process.setPrefix(PREFIX) ;
-		this.process.setIsOptional(IS_OPTIONAL) ;
+		super.setUp();
+		this.process = new Process();
+		this.process.setPrefix(PREFIX);
+		this.process.setIsOptional(IS_OPTIONAL);
 	}
 
 	/*
@@ -34,7 +44,7 @@ public class ProcessTest extends TestCase {
 	 * @see junit.framework.TestCase#tearDown()
 	 */
 	protected void tearDown() throws Exception {
-		super.tearDown() ;
+		super.tearDown();
 	}
 
 	/**
@@ -43,41 +53,41 @@ public class ProcessTest extends TestCase {
 	public final void testHashCode() {
 		// Rk: the setUp method is called here.
 
-		Process proc = new Process() ;
-		proc.setPrefix(PREFIX) ;
-		proc.setIsOptional(IS_OPTIONAL) ;
+		Process proc = new Process();
+		proc.setPrefix(PREFIX);
+		proc.setIsOptional(IS_OPTIONAL);
 
-		assertNotNull(this.process.hashCode()) ;
-		assertNotNull(proc.hashCode()) ;
-		assertEquals(this.process.hashCode(), proc.hashCode()) ;
+		assertNotNull(this.process.hashCode());
+		assertNotNull(proc.hashCode());
+		assertEquals(this.process.hashCode(), proc.hashCode());
 
 		// Rk: the tearDown method is called here.
 	}
 
 	/**
-	 * Test method for {@link wilos.model.spem2.process.Process#equals(java.lang.Object)}.
+	 * Test method for
+	 * {@link wilos.model.spem2.process.Process#equals(java.lang.Object)}.
 	 */
 	public final void testEquals() {
 		// Rk: the setUp method is called here.
 
 		// Assert if it's equal by references.
-		assertTrue("By references", this.process.equals(this.process)) ;
+		assertTrue("By references", this.process.equals(this.process));
 
 		// Assert if it's equal field by field.
-		Process processTmp1 = null ;
-		try{
-			processTmp1 = this.process.clone() ;
+		Process processTmp1 = null;
+		try {
+			processTmp1 = this.process.clone();
+		} catch (CloneNotSupportedException e) {
+			fail("Error CloneNotSupportedException in the testEquals method");
 		}
-		catch(CloneNotSupportedException e){
-			fail("Error CloneNotSupportedException in the testEquals method") ;
-		}
-		assertTrue("Field by field", this.process.equals(processTmp1)) ;
+		assertTrue("Field by field", this.process.equals(processTmp1));
 
 		// Assert if it's not equal.
-		Process procTmp2 = new Process() ;
-		procTmp2.setPrefix("prefixFalse") ;
-		procTmp2.setIsOptional(true) ;
-		assertFalse("Not equals", this.process.equals(procTmp2)) ;
+		Process procTmp2 = new Process();
+		procTmp2.setPrefix("prefixFalse");
+		procTmp2.setIsOptional(true);
+		assertFalse("Not equals", this.process.equals(procTmp2));
 
 		// Rk: the tearDown method is called here.
 	}
@@ -88,13 +98,82 @@ public class ProcessTest extends TestCase {
 	public final void testClone() {
 		// Rk: the setUp method is called here.
 
-		try{
-			assertEquals(this.process.clone(), this.process) ;
-		}
-		catch(CloneNotSupportedException e){
-			fail("Error CloneNotSupportedException in the testClone method") ;
+		try {
+			assertEquals(this.process.clone(), this.process);
+		} catch (CloneNotSupportedException e) {
+			fail("Error CloneNotSupportedException in the testClone method");
 		}
 
 		// Rk: the tearDown method is called here.
+	}
+
+	public void testRemoveProjects() {
+		Project proj = new Project();
+		proj.setName(NAME);
+		proj.setDescription(DESCRIPTION);
+
+		this.process.addProject(proj);
+		assertFalse(this.process.getProjects().isEmpty());
+
+		this.process.removeProject(proj);
+		assertTrue(this.process.getProjects().isEmpty());
+
+	}
+
+	public void testAddProject() {
+		Project proj = new Project();
+		proj.setName(NAME);
+		proj.setDescription(DESCRIPTION);
+
+		this.process.addProject(proj);
+		assertFalse(this.process.getProjects().isEmpty());
+		assertTrue(this.process.getProjects().size() == 1);
+
+	}
+
+	/*public void testRemoveAllProject() {
+		Project proj = new Project();
+		proj.setName(NAME);
+		proj.setDescription(DESCRIPTION);
+
+		Project tmp = new Project();
+		tmp.setName(NAME2);
+		//tmp.setDescription(DESCRIPTION);
+
+		Set<Project> set = new HashSet<Project>();
+		set.add(proj);
+		set.add(tmp);
+
+		this.process.addAllProjects(set);
+		assertNotNull(proj.getProcess());
+		assertNotNull(tmp.getProcess());
+		assertTrue(this.process.getProjects().size() == set.size());
+
+		this.process.removeAllProjects();
+		assertNull(proj.getProcess());
+		assertNull(tmp.getProcess());
+		assertTrue(this.process.getProjects().isEmpty());
+	}*/
+
+	
+	public void testAddToAllProjects() {
+		Project proj = new Project();
+		proj.setName(NAME);
+		proj.setDescription(DESCRIPTION);
+
+		Project tmp = new Project();
+		tmp.setName(NAME2);
+		tmp.setDescription(DESCRIPTION);
+
+		Set<Project> set = new HashSet<Project>();
+		set.add(proj);
+		set.add(tmp);
+
+		this.process.addAllProjects(set);
+
+		assertFalse(this.process.getProjects().isEmpty());
+		assertTrue(this.process.getProjects().size() == 2);
+		assertNotNull(proj.getProcess());
+		assertNotNull(tmp.getProcess());
 	}
 }
