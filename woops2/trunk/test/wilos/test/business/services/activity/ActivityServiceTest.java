b9c0160c4c4abe54f65248930651f545fdf5a374
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-18 23:05:23 +0000

    [Woops2] Mise en place des tests unitaires pour le service sur les ConcreteTaskDescriptor. Ca bug donc je regarderai demain ...

--- woops2/trunk/test/wilos/test/business/services/activity/ActivityServiceTest.java
+++ woops2/trunk/test/wilos/test/business/services/activity/ActivityServiceTest.java
@@ -1,12 +1,11 @@
+package wilos.test.business.services.activity;
 
-package wilos.test.business.services.activity ;
+import java.util.List;
 
-import java.util.List ;
-
-import junit.framework.TestCase ;
+import junit.framework.TestCase;
 import wilos.business.services.activity.ActivityService;
 import wilos.model.spem2.activity.Activity;
-import wilos.test.TestConfiguration ;
+import wilos.test.TestConfiguration;
 
 /**
  * @author deder
@@ -14,28 +13,32 @@ import wilos.test.TestConfiguration ;
  */
 public class ActivityServiceTest extends TestCase {
 
-	private ActivityService activityService ;
+	private ActivityService activityService;
 
-	private Activity activity ;
+	private Activity activity;
 
-	public static final String PREFIX = "prefix" ;
+	public static final String PREFIX = "prefix";
 
-	public static final Boolean IS_OPTIONAL = true ;
+	public static final Boolean IS_OPTIONAL = true;
+
+	public ActivityServiceTest() {
+		// Get the ActivityService Singleton for managing Activity data
+		this.activityService = (ActivityService) TestConfiguration
+				.getInstance().getApplicationContext().getBean(
+						"ActivityService");
+
+		// Create empty Activity
+		this.activity = new Activity();
+	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see junit.framework.TestCase#setUp()
 	 */
-	@ Override
+	@Override
 	protected void setUp() throws Exception {
-		super.setUp() ;
-
-		// Get the ActivityService Singleton for managing Activity data
-		this.activityService = (ActivityService) TestConfiguration.getInstance().getApplicationContext().getBean("ActivityService") ;
-
-		// Create empty Activity
-		this.activity = new Activity() ;
+		super.setUp();
 	}
 
 	/*
@@ -43,17 +46,16 @@ public class ActivityServiceTest extends TestCase {
 	 * 
 	 * @see junit.framework.TestCase#tearDown()
 	 */
-	@ Override
+	@Override
 	protected void tearDown() throws Exception {
-		super.tearDown() ;
+		super.tearDown();
 
 		// Delete the tmp activity from the database.
-		this.activityService.getActivityDao().deleteActivity(this.activity) ;
+		this.activityService.getActivityDao().deleteActivity(this.activity);
 	}
 
 	/*
-	 * (non-Javadoc) 
-	 * Test method for
+	 * (non-Javadoc) Test method for
 	 * {@link woops2.business.activity.ActivityManager#getActivitiesList()}.
 	 * 
 	 */
@@ -61,18 +63,19 @@ public class ActivityServiceTest extends TestCase {
 		// Rk: the setUp method is called here.
 
 		// Save the activity.
-		this.activityService.saveActivity(this.activity) ;
+		this.activityService.saveActivity(this.activity);
 
-		// Look if this activity is also into the database and look if the size of the set is >= 1.
-		List<Activity> activities = this.activityService.getActivitiesList() ;
-		assertNotNull(activities) ;
-		assertTrue(activities.size() >= 1) ;
+		// Look if this activity is also into the database and look if the size
+		// of the set is >= 1.
+		List<Activity> activities = this.activityService.getActivitiesList();
+		assertNotNull(activities);
+		assertTrue(activities.size() >= 1);
 
 		// Rk: the tearDown method is called here.
 	}
 
-	/* (non-Javadoc)
-	 * Test method for
+	/*
+	 * (non-Javadoc) Test method for
 	 * {@link woops2.business.activity.ActivityManager#saveActivity(woops2.model.activity.Activity)}.
 	 * 
 	 */
@@ -80,16 +83,17 @@ public class ActivityServiceTest extends TestCase {
 		// Rk: the setUp method is called here.
 
 		// Save the activity to test the method saveActivity.
-		this.activity.setPrefix(PREFIX) ;
-		this.activity.setIsOptional(IS_OPTIONAL) ;
-		this.activityService.saveActivity(this.activity) ;
-		String id = this.activity.getId() ;
+		this.activity.setPrefix(PREFIX);
+		this.activity.setIsOptional(IS_OPTIONAL);
+		this.activityService.saveActivity(this.activity);
+		String id = this.activity.getId();
 
 		// Look if this activity is also into the database.
-		Activity activityTmp = (Activity) this.activityService.getActivityDao().getActivity(id) ;
-		assertNotNull(activityTmp) ;
-		assertEquals(activityTmp.getPrefix(), PREFIX) ;
-		assertEquals(activityTmp.getIsOptional(), IS_OPTIONAL) ;
+		Activity activityTmp = (Activity) this.activityService.getActivityDao()
+				.getActivity(id);
+		assertNotNull(activityTmp);
+		assertEquals(activityTmp.getPrefix(), PREFIX);
+		assertEquals(activityTmp.getIsOptional(), IS_OPTIONAL);
 
 		// Rk: the tearDown method is called here.
 	}
