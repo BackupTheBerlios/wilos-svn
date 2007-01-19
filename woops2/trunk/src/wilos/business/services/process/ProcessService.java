6a1e5f81e94195640c6c06e4f35701d0348b1b9a
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-19 14:41:07 +0000

    [woops2]: mise a jour du parsing

--- woops2/trunk/src/wilos/business/services/process/ProcessService.java
+++ woops2/trunk/src/wilos/business/services/process/ProcessService.java
@@ -20,6 +20,8 @@ import wilos.business.services.util.xml.parser.XMLParser ;
 import wilos.hibernate.spem2.activity.ActivityDao ;
 import wilos.hibernate.spem2.breakdownelement.BreakdownElementDao ;
 import wilos.hibernate.spem2.element.ElementDao ;
+import wilos.hibernate.spem2.iteration.IterationDao;
+import wilos.hibernate.spem2.phase.PhaseDao;
 import wilos.hibernate.spem2.process.ProcessDao ;
 import wilos.hibernate.spem2.role.RoleDefinitionDao ;
 import wilos.hibernate.spem2.role.RoleDescriptorDao ;
@@ -27,7 +29,10 @@ import wilos.hibernate.spem2.task.StepDao ;
 import wilos.hibernate.spem2.task.TaskDefinitionDao ;
 import wilos.hibernate.spem2.task.TaskDescriptorDao ;
 import wilos.hibernate.spem2.workbreakdownelement.WorkBreakdownElementDao ;
+import wilos.model.spem2.activity.Activity;
 import wilos.model.spem2.breakdownelement.BreakdownElement ;
+import wilos.model.spem2.iteration.Iteration;
+import wilos.model.spem2.phase.Phase;
 import wilos.model.spem2.process.Process ;
 import wilos.model.spem2.role.RoleDefinition ;
 import wilos.model.spem2.role.RoleDescriptor ;
@@ -50,6 +55,10 @@ public class ProcessService {
 	private StepService stepService ;
 
 	private ActivityDao activityDao ;
+	
+	private PhaseDao phaseDao ;
+	
+	private IterationDao iterationDao ;
 
 	private BreakdownElementDao breakdownElementDao ;
 
@@ -70,6 +79,22 @@ public class ProcessService {
 	private WorkBreakdownElementDao workBreakdownElementDao ;
 
 	protected final Log logger = LogFactory.getLog(this.getClass()) ;
+	
+	//declaration of list
+	List<Step> stepList = new ArrayList<Step>() ;
+	List<Step> stepListTmp = new ArrayList<Step>() ;
+	List<TaskDefinition> taskDefinitionList = new ArrayList<TaskDefinition>() ;
+	List<TaskDefinition> taskDefinitionListTmp = new ArrayList<TaskDefinition>() ;
+	List<TaskDescriptor> taskDescriptorList = new ArrayList<TaskDescriptor>() ;
+	List<TaskDescriptor> taskDescriptorListTmp = new ArrayList<TaskDescriptor>() ;
+	List<RoleDefinition> roleDefinitionList = new ArrayList<RoleDefinition>() ;
+	List<RoleDefinition> roleDefinitionListTmp = new ArrayList<RoleDefinition>() ;
+	List<Phase> phaseList = new ArrayList<Phase>() ;
+	List<Phase> phaseListTmp = new ArrayList<Phase>() ;
+	List<Iteration> iterationList = new ArrayList<Iteration>() ;
+	List<Iteration> iterationListTmp = new ArrayList<Iteration>() ;
+	List<RoleDescriptor> roleDescriptorList = new ArrayList<RoleDescriptor>() ;
+	List<RoleDescriptor> roleDescriptorListTmp = new ArrayList<RoleDescriptor>() ;
 
 	public Process spelpParsingXML(File _file) {
 		Process spelpProcess = null;
@@ -93,16 +118,7 @@ public class ProcessService {
 		List<BreakdownElement> bdes = new ArrayList<BreakdownElement>() ;
 		bdes.addAll(_process.getBreakDownElements()) ;
 
-		List<Step> stepList = new ArrayList<Step>() ;
-		List<Step> stepListTmp = new ArrayList<Step>() ;
-		List<TaskDefinition> taskDefinitionList = new ArrayList<TaskDefinition>() ;
-		List<TaskDefinition> taskDefinitionListTmp = new ArrayList<TaskDefinition>() ;
-		List<TaskDescriptor> taskDescriptorList = new ArrayList<TaskDescriptor>() ;
-		List<TaskDescriptor> taskDescriptorListTmp = new ArrayList<TaskDescriptor>() ;
-		List<RoleDefinition> roleDefinitionList = new ArrayList<RoleDefinition>() ;
-		List<RoleDefinition> roleDefinitionListTmp = new ArrayList<RoleDefinition>() ;
-		List<RoleDescriptor> roleDescriptorList = new ArrayList<RoleDescriptor>() ;
-		List<RoleDescriptor> roleDescriptorListTmp = new ArrayList<RoleDescriptor>() ;
+		
 		Process processTmp = new Process() ;
 
 		String id_process = null ;
@@ -111,75 +127,84 @@ public class ProcessService {
 		// add element of process in elements
 		for(BreakdownElement breakdownElement : bdes){
 			logger.debug("### TestSaveCollectionsProcess ### for bdes -> bde =" + breakdownElement) ;
-			if(breakdownElement instanceof TaskDescriptor){
-				logger.debug("### TestSaveCollectionsProcess ### bde instance of TaskDescriptor =" + breakdownElement) ;
-				TaskDescriptor taskDescriptor = (TaskDescriptor) breakdownElement ;
-				TaskDefinition taskDefinition = taskDescriptor.getTaskDefinition() ;
-
-				if(taskDefinition != null){
-					for(Step step : taskDefinition.getSteps()){
-						logger.debug("### SaveProcessService ### for steps ... =" + taskDefinition.getSteps()) ;
-						if(step != null)
-							stepList.add(step) ;
+			if(breakdownElement instanceof Activity){
+				Activity mon = (Activity) breakdownElement;
+				List<BreakdownElement> bdes1 = new ArrayList<BreakdownElement>() ;
+				bdes1.addAll(mon.getBreakDownElements());
+				this.miseAJListe(bdes1);
+			}
+			else
+			{
+				if(breakdownElement instanceof TaskDescriptor){
+					logger.debug("### TestSaveCollectionsProcess ### bde instance of TaskDescriptor =" + breakdownElement) ;
+					TaskDescriptor taskDescriptor = (TaskDescriptor) breakdownElement ;
+					TaskDefinition taskDefinition = taskDescriptor.getTaskDefinition() ;
+	
+					if(taskDefinition != null){
+						for(Step step : taskDefinition.getSteps()){
+							logger.debug("### SaveProcessService ### for steps ... =" + taskDefinition.getSteps()) ;
+							if(step != null)
+								stepList.add(step) ;
+							try{
+								stepListTmp.add(step.clone()) ;
+							}
+							catch(CloneNotSupportedException e){
+								e.printStackTrace() ;
+							}
+							logger.debug("TestSaveCollectionsProcess: stepList -> " + stepList) ;
+							logger.debug("TestSaveCollectionsProcess: stepList.size -> " + stepList.size()) ;
+						}
+	
+						taskDefinitionList.add(taskDefinition) ;
 						try{
-							stepListTmp.add(step.clone()) ;
+							taskDefinitionListTmp.add(taskDefinition.clone()) ;
 						}
 						catch(CloneNotSupportedException e){
 							e.printStackTrace() ;
 						}
-						logger.debug("TestSaveCollectionsProcess: stepList -> " + stepList) ;
-						logger.debug("TestSaveCollectionsProcess: stepList.size -> " + stepList.size()) ;
+						logger.debug("TestSaveCollectionsProcess: taskDefinitionList -> " + taskDefinitionList) ;
+						logger.debug("TestSaveCollectionsProcess: taskDefinitionList.size -> " + taskDefinitionList.size()) ;
 					}
-
-					taskDefinitionList.add(taskDefinition) ;
+					taskDescriptorList.add(taskDescriptor) ;
 					try{
-						taskDefinitionListTmp.add(taskDefinition.clone()) ;
+						taskDescriptorListTmp.add(taskDescriptor.clone()) ;
 					}
 					catch(CloneNotSupportedException e){
 						e.printStackTrace() ;
 					}
-					logger.debug("TestSaveCollectionsProcess: taskDefinitionList -> " + taskDefinitionList) ;
-					logger.debug("TestSaveCollectionsProcess: taskDefinitionList.size -> " + taskDefinitionList.size()) ;
+					nbTD++ ;
+					logger.debug("TestSaveCollectionsProcess: taskDescriptorList -> " + taskDescriptorList) ;
+					logger.debug("TestSaveCollectionsProcess: taskDescriptorList.size -> " + taskDescriptorList.size()) ;
+	
 				}
-				taskDescriptorList.add(taskDescriptor) ;
-				try{
-					taskDescriptorListTmp.add(taskDescriptor.clone()) ;
+				else if(breakdownElement instanceof RoleDescriptor){
+					logger.debug("### SaveProcessService ### breakdownElement instance of RoleDescriptor =" + breakdownElement) ;
+					RoleDescriptor roleDescriptor = (RoleDescriptor) breakdownElement ;
+					roleDescriptorList.add(roleDescriptor) ;
+					try{
+						roleDescriptorListTmp.add(roleDescriptor.clone()) ;
+					}
+					catch(CloneNotSupportedException e){
+						e.printStackTrace() ;
+					}
+	
+					logger.debug("TestSaveCollectionsProcess: roleDescriptorList -> " + roleDescriptorList) ;
+					logger.debug("TestSaveCollectionsProcess: roleDescriptorList.size -> " + roleDescriptorList.size()) ;
+	
+					RoleDefinition rd = roleDescriptor.getRoleDefinition() ;
+					roleDefinitionList.add(rd) ;
+					try{
+						roleDefinitionListTmp.add(rd.clone()) ;
+					}
+					catch(CloneNotSupportedException e){
+						e.printStackTrace() ;
+					}
+					logger.debug("TestSaveCollectionsProcess: roleDefinitionList -> " + roleDefinitionList) ;
+					logger.debug("TestSaveCollectionsProcess: roleDefinitionList.size -> " + roleDefinitionList.size()) ;
 				}
-				catch(CloneNotSupportedException e){
-					e.printStackTrace() ;
+				else{
+					logger.error("### SaveProcessService ### unkown type of object from process !") ;
 				}
-				nbTD++ ;
-				logger.debug("TestSaveCollectionsProcess: taskDescriptorList -> " + taskDescriptorList) ;
-				logger.debug("TestSaveCollectionsProcess: taskDescriptorList.size -> " + taskDescriptorList.size()) ;
-
-			}
-			else if(breakdownElement instanceof RoleDescriptor){
-				logger.debug("### SaveProcessService ### breakdownElement instance of RoleDescriptor =" + breakdownElement) ;
-				RoleDescriptor roleDescriptor = (RoleDescriptor) breakdownElement ;
-				roleDescriptorList.add(roleDescriptor) ;
-				try{
-					roleDescriptorListTmp.add(roleDescriptor.clone()) ;
-				}
-				catch(CloneNotSupportedException e){
-					e.printStackTrace() ;
-				}
-
-				logger.debug("TestSaveCollectionsProcess: roleDescriptorList -> " + roleDescriptorList) ;
-				logger.debug("TestSaveCollectionsProcess: roleDescriptorList.size -> " + roleDescriptorList.size()) ;
-
-				RoleDefinition rd = roleDescriptor.getRoleDefinition() ;
-				roleDefinitionList.add(rd) ;
-				try{
-					roleDefinitionListTmp.add(rd.clone()) ;
-				}
-				catch(CloneNotSupportedException e){
-					e.printStackTrace() ;
-				}
-				logger.debug("TestSaveCollectionsProcess: roleDefinitionList -> " + roleDefinitionList) ;
-				logger.debug("TestSaveCollectionsProcess: roleDefinitionList.size -> " + roleDefinitionList.size()) ;
-			}
-			else{
-				logger.error("### SaveProcessService ### unkown type of object from process !") ;
 			}
 		}
 		try{
@@ -193,7 +218,16 @@ public class ProcessService {
 		_process.getBreakDownElements().clear() ;
 		this.processDao.saveOrUpdateProcess(_process) ;
 		id_process = _process.getId() ;
-
+		for(Phase ph : phaseList)
+		{
+			ph.getSuperActivities().clear();
+			
+		}
+		for(Iteration it : iterationList)
+		{
+			it.getSuperActivities().clear();
+			
+		}
 		for(RoleDescriptor rd : roleDescriptorList){
 			rd.getAdditionalTasks().clear() ;
 			rd.getSuperActivities().clear() ;
@@ -275,13 +309,116 @@ public class ProcessService {
 		for(RoleDescriptor rd : roleDescriptorList){
 			this.roleDescriptorDao.saveOrUpdateRoleDescriptor(rd) ;
 		}
-
+		/*for(int i = 0; i < phaseListTmp.size(); i++ ){
+			phaseList.get(i).addAllSuperActivities(phaseListTmp.get(i).getSuperActivities()) ;
+			phaseList.get(i).addAllBreakdownElements(phaseListTmp.get(i).getBreakDownElements()) ;
+		}
+		for(Phase ph : phaseList)
+		{
+			this.phaseDao.saveOrUpdatePhase(ph) ;
+		}
+		
+		for(int i = 0; i < iterationListTmp.size(); i++ ){
+			iterationList.get(i).addAllSuperActivities(iterationListTmp.get(i).getSuperActivities()) ;
+			iterationList.get(i).addAllBreakdownElements(iterationListTmp.get(i).getBreakDownElements()) ;
+		}
+		for(Iteration it : iterationList)
+		{
+			this.iterationDao.saveOrUpdateIteration(it) ;
+		}
+		*/
 		_process.addAllBreakdownElements(processTmp.getBreakDownElements()) ;
 		this.processDao.saveOrUpdateProcess(_process) ;
 
 		System.out.println("TestProcessPersistence -> ca update ") ;
 
 		return id_process ;
+	}
+	
+	public void miseAJListe(List<BreakdownElement> bdes)
+	{
+		for(BreakdownElement breakdownElement : bdes){
+			logger.debug("### TestSaveCollectionsProcess ### for bdes -> bde =" + breakdownElement) ;
+			if(breakdownElement instanceof Activity){
+				Activity mon = (Activity) breakdownElement;
+				List<BreakdownElement> bdes1 = new ArrayList<BreakdownElement>() ;
+				bdes1.addAll(mon.getBreakDownElements());
+				this.miseAJListe(bdes1);
+			}
+			else
+			{
+				if(breakdownElement instanceof TaskDescriptor){
+					logger.debug("### TestSaveCollectionsProcess ### bde instance of TaskDescriptor =" + breakdownElement) ;
+					TaskDescriptor taskDescriptor = (TaskDescriptor) breakdownElement ;
+					TaskDefinition taskDefinition = taskDescriptor.getTaskDefinition() ;
+	
+					if(taskDefinition != null){
+						for(Step step : taskDefinition.getSteps()){
+							logger.debug("### SaveProcessService ### for steps ... =" + taskDefinition.getSteps()) ;
+							if(step != null)
+								stepList.add(step) ;
+							try{
+								stepListTmp.add(step.clone()) ;
+							}
+							catch(CloneNotSupportedException e){
+								e.printStackTrace() ;
+							}
+							logger.debug("TestSaveCollectionsProcess: stepList -> " + stepList) ;
+							logger.debug("TestSaveCollectionsProcess: stepList.size -> " + stepList.size()) ;
+						}
+	
+						taskDefinitionList.add(taskDefinition) ;
+						try{
+							taskDefinitionListTmp.add(taskDefinition.clone()) ;
+						}
+						catch(CloneNotSupportedException e){
+							e.printStackTrace() ;
+						}
+						logger.debug("TestSaveCollectionsProcess: taskDefinitionList -> " + taskDefinitionList) ;
+						logger.debug("TestSaveCollectionsProcess: taskDefinitionList.size -> " + taskDefinitionList.size()) ;
+					}
+					taskDescriptorList.add(taskDescriptor) ;
+					try{
+						taskDescriptorListTmp.add(taskDescriptor.clone()) ;
+					}
+					catch(CloneNotSupportedException e){
+						e.printStackTrace() ;
+					}
+					
+					logger.debug("TestSaveCollectionsProcess: taskDescriptorList -> " + taskDescriptorList) ;
+					logger.debug("TestSaveCollectionsProcess: taskDescriptorList.size -> " + taskDescriptorList.size()) ;
+	
+				}
+				else if(breakdownElement instanceof RoleDescriptor){
+					logger.debug("### SaveProcessService ### breakdownElement instance of RoleDescriptor =" + breakdownElement) ;
+					RoleDescriptor roleDescriptor = (RoleDescriptor) breakdownElement ;
+					roleDescriptorList.add(roleDescriptor) ;
+					try{
+						roleDescriptorListTmp.add(roleDescriptor.clone()) ;
+					}
+					catch(CloneNotSupportedException e){
+						e.printStackTrace() ;
+					}
+	
+					logger.debug("TestSaveCollectionsProcess: roleDescriptorList -> " + roleDescriptorList) ;
+					logger.debug("TestSaveCollectionsProcess: roleDescriptorList.size -> " + roleDescriptorList.size()) ;
+	
+					RoleDefinition rd = roleDescriptor.getRoleDefinition() ;
+					roleDefinitionList.add(rd) ;
+					try{
+						roleDefinitionListTmp.add(rd.clone()) ;
+					}
+					catch(CloneNotSupportedException e){
+						e.printStackTrace() ;
+					}
+					logger.debug("TestSaveCollectionsProcess: roleDefinitionList -> " + roleDefinitionList) ;
+					logger.debug("TestSaveCollectionsProcess: roleDefinitionList.size -> " + roleDefinitionList.size()) ;
+				}
+				else{
+					logger.error("### SaveProcessService ### unkown type of object from process !") ;
+				}
+			}
+		}
 	}
 
 	/**
@@ -294,30 +431,6 @@ public class ProcessService {
 		return this.processDao.getAllProcesses() ;
 	}
 
-	/**
-	 * @param _processId
-	 * @return
-	 */
-	@ Transactional (readOnly = true)
-	public Process getEntireProcess(String _processId) {
-		Process process = this.processDao.getProcess(_processId) ;
-		Set<BreakdownElement> bdes = new HashSet<BreakdownElement>() ;
-		bdes.addAll(this.breakdownElementService.getBreakdownElementsFromProcess(_processId)) ;
-		process.addAllBreakdownElements(bdes) ;
-		for(BreakdownElement bde : bdes){
-			if(bde instanceof TaskDescriptor){
-				TaskDescriptor tdor = (TaskDescriptor) bde ;
-				SortedSet<Step> steps = new TreeSet<Step>() ;
-				if(tdor.getTaskDefinition() != null){
-					steps.addAll(this.stepService.getStepsFromTask(tdor.getTaskDefinition().getId())) ;
-					tdor.getTaskDefinition().addAllSteps(steps) ;
-				}
-			}
-		}
-		return process ;
-	}
-	
-	@ Transactional (readOnly = true)
 	public Process getConcreteTaskDescriptorByProcess(String _processId) {
 		/*TODO Process process = this.processDao.getProcess(_processId) ;
 		Set<BreakdownElement> bdes = new HashSet<BreakdownElement>() ;
@@ -348,7 +461,30 @@ public class ProcessService {
 		return process ;*/
 		return null;
 	}
+	/**
+	 * @param _processId
+	 * @return
+	 */
+	@ Transactional (readOnly = true)
+	public Process getEntireProcess(String _processId) {
+		Process process = this.processDao.getProcess(_processId) ;
+		Set<BreakdownElement> bdes = new HashSet<BreakdownElement>() ;
+		bdes.addAll(this.breakdownElementService.getBreakdownElementsFromProcess(_processId)) ;
+		process.addAllBreakdownElements(bdes) ;
+		for(BreakdownElement bde : bdes){
+			if(bde instanceof TaskDescriptor){
+				TaskDescriptor tdor = (TaskDescriptor) bde ;
+				SortedSet<Step> steps = new TreeSet<Step>() ;
+				if(tdor.getTaskDefinition() != null){
+					steps.addAll(this.stepService.getStepsFromTask(tdor.getTaskDefinition().getId())) ;
+					tdor.getTaskDefinition().addAllSteps(steps) ;
+				}
+			}
+		}
+		return process ;
+	}
 	
+	// TODO PROBLEME : fonction peut etre inutile ?!
 	@ Transactional (readOnly = true)
 	public Process getProcessWithOnlyTaskDescriptors(String _processId) {
 		Process process = this.processDao.getProcess(_processId) ;
@@ -586,5 +722,21 @@ public class ProcessService {
 	 */
 	public void setStepService(StepService _stepService) {
 		this.stepService = _stepService ;
+	}
+
+	public IterationDao getIterationDao() {
+		return iterationDao;
+	}
+
+	public void setIterationDao(IterationDao iterationDao) {
+		this.iterationDao = iterationDao;
+	}
+
+	public PhaseDao getPhaseDao() {
+		return phaseDao;
+	}
+
+	public void setPhaseDao(PhaseDao phaseDao) {
+		this.phaseDao = phaseDao;
 	}
 }
