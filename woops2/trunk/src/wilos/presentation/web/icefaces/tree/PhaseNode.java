13446d1dc6850dbdf9c32f4c32852f5c61810810
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-22 17:56:16 +0000

    [Woops2] Debuggage de l'arbre. Ca fonctionne ! ;) Rk: On ne traite pas les activity ...

--- woops2/trunk/src/wilos/presentation/web/icefaces/tree/PhaseNode.java
+++ woops2/trunk/src/wilos/presentation/web/icefaces/tree/PhaseNode.java
@@ -2,7 +2,10 @@ package wilos.presentation.web.icefaces.tree;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import wilos.model.spem2.breakdownelement.BreakdownElement;
+import wilos.model.spem2.iteration.Iteration;
 import wilos.model.spem2.phase.Phase;
+import wilos.model.spem2.task.TaskDescriptor;
 
 public class PhaseNode extends DefaultMutableTreeNode {
 
@@ -23,13 +26,17 @@ public class PhaseNode extends DefaultMutableTreeNode {
 		iceUserObject.setBranchContractedIcon("images/tree/icon_phase.gif");
 		iceUserObject.setBranchExpandedIcon("images/tree/icon_phase.gif");
 		iceUserObject.setObjectId(_phase.getId());
-		/*for (BreakdownElement breakdownElement : this.phase
+		for (BreakdownElement breakdownElement : this.phase
 				.getBreakDownElements()) {
-			if (breakdownElement instanceof TaskDescriptor) {
+			if (breakdownElement instanceof Iteration) {
+				Iteration iteration = (Iteration) breakdownElement;
+				this.add(new IterationNode(iteration));
+			}
+			else if (breakdownElement instanceof TaskDescriptor) {
 				TaskDescriptor taskDescriptor = (TaskDescriptor) breakdownElement;
 				this.add(new TaskDescriptorNode(taskDescriptor));
 			}
-		}*/
+		}
 	}
 
 }
