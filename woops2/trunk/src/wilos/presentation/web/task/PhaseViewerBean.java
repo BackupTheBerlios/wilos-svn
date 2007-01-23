d4ca47d72f84a2dd073c235d9f6d25f26c3319d2
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-23 21:19:03 +0000

    [Woops2] grommit Phase

---
+++ woops2/trunk/src/wilos/presentation/web/task/PhaseViewerBean.java
@@ -0,0 +1,45 @@
+package wilos.presentation.web.task;
+
+import wilos.business.services.spem2.phase.PhaseService;
+import wilos.model.spem2.phase.Phase;
+
+public class PhaseViewerBean {
+
+	private Phase phase;
+
+	private PhaseService phaseService;
+
+	private String phaseId = "";
+
+	public void buildPhaseModel() {
+		this.phase = new Phase();
+		if (!(phaseId.equals("")) || phaseId != null) {
+			this.phase = this.phaseService.getPhaseDao().getPhase(this.phaseId);
+		}
+	}
+
+	public Phase getPhase() {
+		return phase;
+	}
+
+	public void setPhase(Phase phase) {
+		this.phase = phase;
+	}
+
+	public String getPhaseId() {
+		return phaseId;
+	}
+
+	public void setPhaseId(String phaseId) {
+		this.phaseId = phaseId;
+	}
+
+	public PhaseService getPhaseService() {
+		return phaseService;
+	}
+
+	public void setPhaseService(PhaseService phaseService) {
+		this.phaseService = phaseService;
+	}
+
+}
