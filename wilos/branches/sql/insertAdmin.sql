6599dd3650233a8aec23b3ab5b7480935b9af8c4
mikamikaze <mikamikaze@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-24 17:36:02 +0000

    [Wilos] Integrations modifs dues a l'affectation process/project

--- wilos/branches/sql/insertAdmin.sql
+++ wilos/branches/sql/insertAdmin.sql
@@ -4,7 +4,16 @@ INSERT INTO `wilosuser` (`wilosuser_id`, `login`, `name`, `firstname`, `emailAdd
 ('2c90a1b21031d7ef011031de53650001', 'parti', 'parti', 'parti', 'marti@parti.com', '4a928ea62e001b6cb3780d2137b8eb00'),
 ('2c90c28e1031312e01103131ecff0001', 'admin', 'admin', 'admin', 'admin@wilos.com', '85aac14e99386ee8f68c89372821b1ca');
 
+-- admin/mdpadmin
 INSERT INTO `administrator` (`administrator_id`) VALUES ('2c90c28e1031312e01103131ecff0001');
+-- pd/pdpdpd
 INSERT INTO `projectdirector` (`projectdirector_id`) VALUES ('2c90a1b21031b5b1011031b8a0360002');
+-- pm/pmpmpm
 INSERT INTO `processmanager` (`processmanager_id`) VALUES ('2c90a1b21031b5b1011031b85b7c0001');
+-- parti/mdpparti
 INSERT INTO `participant` (`participant_id`) VALUES ('2c90a1b21031d7ef011031de53650001');
+
+-- Creation de deux projets
+INSERT INTO `project` (`project_id`, `name`, `description`, `creationDate`, `launchingDate`, `isFinished`, `process_id`, `participant_id`) VALUES 
+('2c90a1b2104ad70601104ad906f90001', 'Shmurf', 'shhhhmuuuuuurf', '2007-01-22 18:27:05', '2007-01-22 00:00:00', '\0', NULL, NULL),
+('2c90a1b2104ad70601104ad9d72a0002', 'Poulouuuuu', 'pooooouuuuuuuuuulouuuuuuuuuuuuuuuuuu', '2007-01-22 18:27:21', '2008-01-22 00:00:00', '\0', NULL, NULL);
