968371a72537e1db32d20ee64a11150665cebefe
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-25 23:56:09 +0000

    [Woops2] Prise en compte dans l'arbre sur le fait qu'un projet peut avoir un process a null.

--- woops2/trunk/src/wilos/presentation/web/tree/ProjectNode.java
+++ woops2/trunk/src/wilos/presentation/web/tree/ProjectNode.java
@@ -27,8 +27,7 @@ public class ProjectNode extends DefaultMutableTreeNode {
 		this.setUserObject(iceUserObject);
 
 		iceUserObject.setExpanded(true);
-		iceUserObject.setText(this.project.getName() + " ("
-				+ this.project.getProcess().getName() + ")");
+		iceUserObject.setText(this.project.getName());
 		iceUserObject.setLeaf(false);
 		iceUserObject.setBranchContractedIcon("images/tree/icon_process.gif");
 		iceUserObject.setBranchExpandedIcon("images/tree/icon_process.gif");
@@ -36,24 +35,32 @@ public class ProjectNode extends DefaultMutableTreeNode {
 		iceUserObject.setId(this.project.getProject_id());
 		iceUserObject.setPageId(WilosObjectNode.PROJECTNODE);
 
-		for (BreakdownElement breakdownElement : this.project.getProcess()
-				.getBreakDownElements()) {
-			if (breakdownElement instanceof Phase) {
-				this.add(new PhaseNode((Phase) breakdownElement,
-						_roleDescriptors));
-			} else if (breakdownElement instanceof Iteration) {
-				this.add(new IterationNode((Iteration) breakdownElement,
-						_roleDescriptors));
-			} else if (breakdownElement instanceof Activity) {
-				this.add(new ActivityNode((Activity) breakdownElement,
-						_roleDescriptors));
-			} else if (breakdownElement instanceof TaskDescriptor) {
-				TaskDescriptor td = (TaskDescriptor) breakdownElement;
-				if ((_roleDescriptors == null)||(_roleDescriptors.contains(td.getMainRole())))
-					for (ConcreteTaskDescriptor ctd : td
-							.getConcreteTaskDescriptors())
-						this.add(new ConcreteTaskDescriptorNode(ctd,
-								_roleDescriptors));
+		if (this.project.getProcess() != null) {
+			//Project node name.
+			iceUserObject.setText(this.project.getName() + " ("
+					+ this.project.getProcess().getName() + ")");
+			
+			//Nested nodes.
+			for (BreakdownElement breakdownElement : this.project.getProcess()
+					.getBreakDownElements()) {
+				if (breakdownElement instanceof Phase) {
+					this.add(new PhaseNode((Phase) breakdownElement,
+							_roleDescriptors));
+				} else if (breakdownElement instanceof Iteration) {
+					this.add(new IterationNode((Iteration) breakdownElement,
+							_roleDescriptors));
+				} else if (breakdownElement instanceof Activity) {
+					this.add(new ActivityNode((Activity) breakdownElement,
+							_roleDescriptors));
+				} else if (breakdownElement instanceof TaskDescriptor) {
+					TaskDescriptor td = (TaskDescriptor) breakdownElement;
+					if ((_roleDescriptors == null)
+							|| (_roleDescriptors.contains(td.getMainRole())))
+						for (ConcreteTaskDescriptor ctd : td
+								.getConcreteTaskDescriptors())
+							this.add(new ConcreteTaskDescriptorNode(ctd,
+									_roleDescriptors));
+				}
 			}
 		}
 	}
