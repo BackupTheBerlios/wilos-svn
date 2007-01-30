6b2fc42d9736b4d1f024f8f10ebcecd4353cef12
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-30 21:55:33 +0000

    [Woops2] Commencement de la prise en compte de la notion de concreteroledescriptor. Il y a encore des bugs que je tente de fixer.

--- woops2/trunk/test/wilos/test/model/misc/concreteroledescriptor/ConcreteRoleDescriptorTest.java
+++ woops2/trunk/test/wilos/test/model/misc/concreteroledescriptor/ConcreteRoleDescriptorTest.java
@@ -3,6 +3,7 @@ package wilos.test.model.misc.concreteroledescriptor;
 
 import junit.framework.TestCase;
 import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
+import wilos.model.misc.project.Project;
 import wilos.model.spem2.role.RoleDescriptor;
 
 public class ConcreteRoleDescriptorTest extends TestCase {
@@ -11,12 +12,12 @@ public class ConcreteRoleDescriptorTest extends TestCase {
 
 	/*
 	public ConcreteRoleDescriptorTest(){
-		
+
 	}*/
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
@@ -24,42 +25,42 @@ public class ConcreteRoleDescriptorTest extends TestCase {
 		this.concreteRoleDescriptor = new ConcreteRoleDescriptor() ;
 		this.concreteRoleDescriptor.setConcreteName("Concrete Name");
 		this.concreteRoleDescriptor.setId("465");
-		this.concreteRoleDescriptor.setProjectId("8979");
+		this.concreteRoleDescriptor.setProject(new Project());
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#tearDown()
 	 */
 	protected void tearDown() throws Exception {
 		super.tearDown() ;
 	}
-	
+
 	public void testAddRoleDescriptor() {
 		// Rk: the setUp method is called here.
-		
+
 		RoleDescriptor roleDescriptor = new RoleDescriptor();
 		this.concreteRoleDescriptor.addRoleDescriptor(roleDescriptor);
 		assertNotNull(this.concreteRoleDescriptor.getRoleDescriptor());
 		assertEquals(this.concreteRoleDescriptor.getRoleDescriptor(), roleDescriptor) ;
-		
+
 		// Rk: the tearDown method is called here.
 	}
 
 	public void testRemoveRoleDescriptor() {
 		// Rk: the setUp method is called here.
-		
+
 		RoleDescriptor roleDescriptor = new RoleDescriptor();
-		
+
 		this.concreteRoleDescriptor.addRoleDescriptor(roleDescriptor);
 		assertNotNull(this.concreteRoleDescriptor.getRoleDescriptor());
 		assertEquals(this.concreteRoleDescriptor.getRoleDescriptor(), roleDescriptor);
-		
+
 		this.concreteRoleDescriptor.removeRoleDescriptor(roleDescriptor);
 		assertNull(this.concreteRoleDescriptor.getRoleDescriptor());
 		assertTrue(roleDescriptor.getConcreteRoleDescriptors().size() == 0);
-		
+
 		this.concreteRoleDescriptor.removeRoleDescriptor(roleDescriptor) ;
 		assertNull(this.concreteRoleDescriptor.getRoleDescriptor());
 
