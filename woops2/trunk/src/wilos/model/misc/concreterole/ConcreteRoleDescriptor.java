7a8042591fb839f569f4704a9841029f4dba4166
aollier <aollier@07d201f1-7922-0410-a939-aa91563164ab>
2007-01-31 14:55:42 +0000

    [Woops2] Correction de ConcreteRoleDescriptor

--- woops2/trunk/src/wilos/model/misc/concreterole/ConcreteRoleDescriptor.java
+++ woops2/trunk/src/wilos/model/misc/concreterole/ConcreteRoleDescriptor.java
@@ -1,8 +1,10 @@
 package wilos.model.misc.concreterole;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
+import wilos.model.misc.wilosuser.Participant;
 import wilos.model.spem2.role.RoleDescriptor;
 
 public class ConcreteRoleDescriptor {
@@ -16,7 +18,31 @@ public class ConcreteRoleDescriptor {
 	private RoleDescriptor roleDescriptor;
 
 	private Set<ConcreteTaskDescriptor> concreteTaskDescriptors;
+	
+	private Participant participant;
 
+	public ConcreteRoleDescriptor() {
+		this.concreteTaskDescriptors = new HashSet<ConcreteTaskDescriptor>();
+	}
+	
+	/*
+	 * relation between ConcreteRoleDescriptor and Participant.
+	 */
+
+	public void addParticipant(Participant _participant) {
+		this.participant = _participant;
+		//_participant.getConcreteRoleDescriptors().add(this);
+	}
+
+	public void removeParticipant(Participant _participant) {
+		//_participant.getConcreteRoleDescriptors().remove(this);
+		this.participant = null;
+	}
+	
+	/*
+	 * Relation between ConcreteRoleDescriptor and RoleDescriptor.
+	 *
+	 */
 	public void addRoleDescriptor(RoleDescriptor _roleDescriptor) {
 		this.roleDescriptor = _roleDescriptor ;
 		_roleDescriptor.getConcreteRoleDescriptors().add(this) ;
@@ -27,36 +53,73 @@ public class ConcreteRoleDescriptor {
 		this.roleDescriptor = null ;
 	}
 
+	/*
+	 * Relation between ConcreteRoleDescriptor and ConcreteTaskDescriptor.
+	 *
+	 */
+
+	public void addConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor) {
+		this.concreteTaskDescriptors.add(_concreteTaskDescriptor);
+		_concreteTaskDescriptor.setConcreteRoleDescriptor(this);
+	}
+
+	public void removeConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor) {
+		this.concreteTaskDescriptors.remove(_concreteTaskDescriptor);
+		_concreteTaskDescriptor.setConcreteRoleDescriptor(null);
+	}
+
+	public void addAllConcreteTaskDescriptors(
+			Set<ConcreteTaskDescriptor> _concreteTaskDescriptors) {
+		this.concreteTaskDescriptors.addAll(_concreteTaskDescriptors) ;
+		
+		for (ConcreteTaskDescriptor ctd : _concreteTaskDescriptors) {
+			ctd.addConcreteRoleDescriptor(this);
+		}
+	}
+
+	public void removeAllConcreteTaskDescriptors() {
+		for (ConcreteTaskDescriptor ctd : this.getConcreteTaskDescriptors())
+			ctd.setConcreteRoleDescriptor(null);
+		this.getConcreteTaskDescriptors().clear();
+	}
+	
+	/*
+	 * Getter and Setter.
+	 *
+	 */
+	
 	public String getConcreteName() {
 		return concreteName;
 	}
 
-	public void setConcreteName(String concreteName) {
-		this.concreteName = concreteName;
+	public void setConcreteName(String _concreteName) {
+		this.concreteName = _concreteName;
 	}
 
 	public String getId() {
 		return id;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setId(String _id) {
+		this.id = _id;
 	}
 
 	public String getProjectId() {
 		return projectId;
 	}
 
-	public void setProjectId(String projectId) {
-		this.projectId = projectId;
+	public void setProjectId(String _projectId) {
+		this.projectId = _projectId;
 	}
 
 	public RoleDescriptor getRoleDescriptor() {
 		return roleDescriptor;
 	}
 
-	public void setRoleDescriptor(RoleDescriptor roleDescriptor) {
-		this.roleDescriptor = roleDescriptor;
+	public void setRoleDescriptor(RoleDescriptor _roleDescriptor) {
+		this.roleDescriptor = _roleDescriptor;
 	}
 
 	public Set<ConcreteTaskDescriptor> getConcreteTaskDescriptors() {
@@ -66,5 +129,13 @@ public class ConcreteRoleDescriptor {
 	public void setConcreteTaskDescriptors(
 			Set<ConcreteTaskDescriptor> concreteTaskDescriptors) {
 		this.concreteTaskDescriptors = concreteTaskDescriptors;
+	}
+	
+	public Participant getParticipant() {
+		return participant;
+	}
+
+	public void setParticipant(Participant _participant) {
+		this.participant = _participant;
 	}
 }
