2643a343cc2598317995b9b58d30d8e096bdcdc8
sylv <sylv@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-02 16:04:48 +0000

    ConcreteWorkBreakDownElementDao - Sylv

---
+++ woops2/trunk/src/wilos/hibernate/misc/concreteworkbreakdownelement/ConcreteWorkBreakDownElementDao.java
@@ -0,0 +1,81 @@
+package wilos.hibernate.misc.concreteworkbreakdownelement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import wilos.model.misc.concreteworkbreakdownelement.ConcreteWorkBreakdownElement;
+import wilos.utils.ExceptionManager;
+
+/**
+ * @author Sylvain
+ * 
+ * ConcreteWorkBreakdownElementDao manage requests from the system to store wbdes to the database
+ * 
+ */
+
+public class ConcreteWorkBreakDownElementDao extends HibernateDaoSupport {
+	/**
+	 * Save or update an activity
+	 * 
+	 * @param _activity
+	 */
+	public void saveOrUpdateConcreteWorkBreakdownElement(ConcreteWorkBreakdownElement _concreteworkBreakdownElement) {
+		try{
+			this.getHibernateTemplate().saveOrUpdate(_concreteworkBreakdownElement) ;
+			// this.getHibernateTemplate().flush() ;
+
+		}
+		catch(DataIntegrityViolationException _e){
+			ExceptionManager.getInstance().manageDataIntegrityViolationException(this.getClass().getName(), "saveOrUpdateConcreteWorkBreakdownElement", _e);
+		}
+		catch(ConstraintViolationException _ex){
+			ExceptionManager.getInstance().manageConstraintViolationException(this.getClass().getName(), "saveOrUpdateConcreteWorkBreakdownElement", _ex);
+		}
+	}
+
+	/**
+	 * Return a list of activities
+	 * 
+	 * @return
+	 */
+	@ SuppressWarnings ("unchecked")
+	public List<ConcreteWorkBreakdownElement> getAllConcreteWorkBreakdownElements() {
+		List<ConcreteWorkBreakdownElement> loadAll = new ArrayList<ConcreteWorkBreakdownElement>() ;
+		try{
+			loadAll.addAll(this.getHibernateTemplate().loadAll(ConcreteWorkBreakdownElement.class)) ;
+		}
+		catch(DataAccessException _e){
+			ExceptionManager.getInstance().manageDataAccessException(this.getClass().getName(), "getAllConcreteWorkBreakdownElements", _e);
+		}
+		return loadAll ;
+	}
+
+	/**
+	 * Return the activity which has the id _id
+	 * 
+	 * @param _id
+	 * @return
+	 */
+	public ConcreteWorkBreakdownElement getConcreteWorkBreakdownElement(String _id) {
+		return (ConcreteWorkBreakdownElement) this.getHibernateTemplate().get(ConcreteWorkBreakdownElement.class, _id) ;
+	}
+
+	/**
+	 * Delete the activity
+	 * 
+	 * @param _activity
+	 */
+	public void deleteConcreteWorkBreakdownElement(ConcreteWorkBreakdownElement _concreteworkBreakdownElement) {
+		try{
+			this.getHibernateTemplate().delete(_concreteworkBreakdownElement) ;
+		}
+		catch(DataAccessException _e){
+			ExceptionManager.getInstance().manageDataAccessException(this.getClass().getName(), "deleteConcreteWorkBreakdownElement", _e);
+		}
+	}
+}
