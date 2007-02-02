c785bc3832f0ac51026ea9106af254c6a366378f
mastasushi <mastasushi@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-02 16:29:26 +0000

    [SPELP] Mise a jour de ServersListParser avec les tests Junits des 2 grandes methodes --> OK

--- spelp/trunk/wizard/src/wilos/presentation/assistant/control/ServersListParser.java
+++ spelp/trunk/wizard/src/wilos/presentation/assistant/control/ServersListParser.java
@@ -30,14 +30,24 @@ public class ServersListParser {
 	public ServersListParser(String f) {
 		XML_File = new File (f);
 		XMLUtils.setDocument(XML_File);
+		if (!XML_File.exists()) {
+			try {
+				XML_File.createNewFile();
+			} catch (Exception e) {}
+		}
 	}
 	
 	public ServersListParser() {
 		XML_File = new File (ServersListParser.fic);
 		XMLUtils.setDocument(XML_File);
+		if (!XML_File.exists()) {
+			try {
+				XML_File.createNewFile();
+			} catch (Exception e) {}
+		}
 	}
 
-	public List<WizardServer> getServersList() {
+	public ArrayList<WizardServer> getServersList() {
 		if (serversList == null) {
 			
 			if (XML_File.exists() && XML_File.length() > 2) {
@@ -66,14 +76,13 @@ public class ServersListParser {
 							if (attributesNode.item(j).getNodeName().equals("Address")) {
 								add = attributesNode.item(j).getTextContent();
 							}
-							/*
 							if (attributesNode.item(j).getNodeName().equals("Id")) {
-								id = attributesNode.item(j).getTextContent();
-							}*/
+								id = Integer.parseInt(attributesNode.item(j).getTextContent());
+							}
 						}
 						
 						// Don't add the server if it's not valid
-						if (!al.equals("") && !add.equals("")) {		//TODO condition sur id
+						if (!al.equals("") && !add.equals("") && id != -1) {
 							WizardServer ws = new WizardServer (al, add,id);
 							serversList.add(ws);
 						}
@@ -90,11 +99,8 @@ public class ServersListParser {
 		Element tmp;
 		Element server;
 		Document myDocument = new  Document(data);
-//		XStream xstream = new XStream();
-//		xstream.alias("Server", WizardServer.class);
 		
 		WizardServer ws;
-		//String toadd;
 		
 		Iterator it = wsl.iterator();		
 		while(it.hasNext())
@@ -110,11 +116,11 @@ public class ServersListParser {
 			tmp = new Element("Address");
 			tmp.addContent(ws.getAddress());
 			server.addContent(tmp);
-			/*
+			
 			tmp = new Element("Id");
-			tmp.addContent(String.valueOf(allContacts.get(i).getId()));
-			contact.addContent(tmp);
-			*/
+			tmp.addContent(""+ws.getId());
+			server.addContent(tmp);
+			
 			data.addContent(server);
 		}
 
@@ -130,251 +136,8 @@ public class ServersListParser {
 			e.printStackTrace();
 		}
 	}
-}
-/**
-package model;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.xpath.XPathConstants;
-
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import utils.XMLUtils;
-
-import com.thoughtworks.xstream.XStream;
-
-import control.CContact;
-
-public class Contact {
-	private static final String xpath_contact = "//Contact";
-	private static final String xpath_data = "//Data";
 	
-	private String name;
-	private String number;
-	private int id;
-	private static File XMLFile;
-	private static List<Contact> allContacts = null;
-	
-	public static void setXMLFile(File anXMLFile) {
-		XMLFile = anXMLFile;
-		XMLUtils.setDocument(XMLFile);
-	}
-	
-	public Contact(String name, String number, int id) {
-		this.name = name;
-		this.number = number;
-		this.id = id;
+	public void lastUsedServer (int no) {
 		
-		allContacts.add(this);
-	}
-	
-	public Contact(String name, String number) {
-		this.name = name;
-		this.number = number;
-		this.id = 0;
-		
-		allContacts.add(this);
-	}
-
-	public static List<Contact> getAllContacts() {
-		if (allContacts == null) {
-			if (XMLFile.exists() && XMLFile.length() > 2) {
-				allContacts = new ArrayList<Contact>();
-				
-				NodeList nodeReturned = (NodeList)XMLUtils.evaluate(xpath_contact, XPathConstants.NODESET);
-				
-				if (nodeReturned != null) {
-					Node aNode;
-					String name = "";
-					String number = "";
-					int id = 0;
-					for(int i = 0; i < nodeReturned.getLength(); i++) {
-						aNode = nodeReturned.item(i);
-						
-						NodeList attributesNode = aNode.getChildNodes();
-						for (int j = 0 ; j < attributesNode.getLength() ; j++) {
-							//System.out.println(attributesNode.item(j).getName);
-							if (attributesNode.item(j).getNodeName().equals("Name")) {
-								name = attributesNode.item(j).getTextContent();
-							}
-							if (attributesNode.item(j).getNodeName().equals("Number")) {
-								number = attributesNode.item(j).getTextContent();
-							}
-							if (attributesNode.item(j).getNodeName().equals("Id")) {
-								Integer tmpId = new Integer(attributesNode.item(j).getTextContent());
-								id = tmpId.intValue();
-							}
-						}
-						
-						Contact contact = new Contact(name, number, id);
-						
-						allContacts.add(contact);			
-					}	
-				}
-			}
-			else allContacts = new ArrayList<Contact>();
-		}
-		return allContacts;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public String getNumber() {
-		return this.number;
-	}
-
-	public int getId() {
-		return this.id;
-	}
-
-	public static void save() {
-		if (this.id == 0) {			
-			this.add();
-		}
-		else {
-			this.update();
-		}
-		Element data = new Element("Data");
-		Element tmp;
-		Element contact;
-		Document myDocument = new  Document(data);
-		XStream xstream = new XStream();
-		xstream.alias("Contact", Contact.class);
-		String toadd;
-		for(int i = 0; i < allContacts.size(); i++) {
-//			toadd = xstream.toXML(allContacts.get(i));
-//			tmp =  new Element(toadd);
-//			data.addContent(tmp);
-			contact = new Element("Contact");
-			
-			tmp = new Element("Name");
-			tmp.addContent(allContacts.get(i).getName());
-			contact.addContent(tmp);
-			
-			tmp = new Element("Number");
-			tmp.addContent(allContacts.get(i).getNumber());
-			contact.addContent(tmp);
-			
-			tmp = new Element("Id");
-			tmp.addContent(String.valueOf(allContacts.get(i).getId()));
-			contact.addContent(tmp);
-			
-			data.addContent(contact);
-		}
-
-		XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-		try {
-			
-			FileWriter writer = new FileWriter(XMLFile);
-			outputter.output(myDocument, new FileOutputStream(XMLFile));
-			writer.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void add() {
-		// Creation d'un Noeud Contact, et des Noeuds Name, Number et Id
-		NodeList nodeReturned = (NodeList)XMLUtils.evaluate(xpath_data, XPathConstants.NODESET);
-		System.out.println("ici");
-		if (nodeReturned != null) {
-//			Node aNode;
-//			Node newContact;
-//			for(int i = 0; i < nodeReturned.getLength(); i++) {
-//				aNode = nodeReturned.item(i);
-//				newContact = XMLUtils.addElement("Contact");
-////				Element 
-////				aNode.appendChild(arg0);		
-//			}
-			
-			String toadd;
-			XStream xstream = new XStream();
-			
-			xstream.alias("Contact", Contact.class);
-			
-			toadd = xstream.toXML(this);
-			
-			//nodeReturned.item(0).
-			
-			//System.out.println(toadd);
-		}
-	}
-	public void delete() {
-		// Suppression du Noeud Contact et de ses sous noeuds, correspondant au Contact supprime
-	}
-	
-	public static Contact getContactById(int id) {
-		Contact contact = null;
-		
-		if (XMLFile.exists() && XMLFile.length() > 2) {			
-			NodeList nodeReturned = (NodeList)XMLUtils.evaluate(xpath_contact, XPathConstants.NODESET);
-			
-			if (nodeReturned != null) {
-				Node aNode;
-				String name = "";
-				String number = "";
-				int nodeId = 0;
-				for(int i = 0; i < nodeReturned.getLength(); i++) {
-					aNode = nodeReturned.item(i);
-					
-					NodeList attributesNode = aNode.getChildNodes();
-
-					for (int j = 0 ; j < attributesNode.getLength() ; j++) {
-						if (attributesNode.item(j).getNodeName().equals("Name")) {
-							name = attributesNode.item(j).getTextContent();
-						}
-						if (attributesNode.item(j).getNodeName().equals("Number")) {
-							number = attributesNode.item(j).getTextContent();
-						}
-						if (attributesNode.item(j).getNodeName().equals("Id")) {
-							Integer tmpId = new Integer(attributesNode.item(j).getTextContent());
-							nodeId = tmpId.intValue();
-						}
-					}
-					
-					if (nodeId == id) {
-						contact = new Contact(name, number, id);
-					}
-					
-					
-				}	
-			}
-		}
-		return contact;
 	}
 }
-
-//////////////////////////////////////////
-<?xml version="1.0" encoding="UTF-8"?>
-<Data lastModification=""> 
-	<Contact>
-		<Name>Mon Premier Contact</Name>
-		<Number>0123456789</Number>
-		<Id>1</Id>
-	</Contact>
-	<Contact>
-		<Name>Mon Second Contact</Name>
-		<Number>9876543210</Number>
-		<Id>2</Id>
-	</Contact>
-</Data>
-///////////////////////////////////////////
-
-
-
-*/
--- spelp/trunk/wizard/test/wilos/test/presentation/assistant/control/ServersListParserTest.java
+++ spelp/trunk/wizard/test/wilos/test/presentation/assistant/control/ServersListParserTest.java
@@ -1,5 +1,6 @@
 package wilos.test.presentation.assistant.control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -33,10 +34,27 @@ public class ServersListParserTest extends TestCase {
 	}
 
 	public void testSaveServersList() {
-		/*
 		WizardServer ws1 = new WizardServer("lala", "http://marine.edu.ups-tlse.fr:9014/wi",1);
+		WizardServer ws2 = new WizardServer("hello", "http://www.youpi.com",1);
+		WizardServer ws3 = new WizardServer("mon_serveur", "http://blablabla.fr",1);
 		
-*/
+		ServersListParser slp = new ServersListParser("test/wilos/test/presentation/assistant/control/servers_test_save.xml");
+		ArrayList<WizardServer> wsl1 = new ArrayList<WizardServer>();
+		wsl1.add(ws1);
+		wsl1.add(ws2);
+		wsl1.add(ws3);
+		slp.saveServersList(wsl1);
+		
+		ArrayList<WizardServer> wsl2 = slp.getServersList();
+		assertTrue(wsl2.get(0).getAlias().equals(ws1.getAlias()));
+		assertTrue(wsl2.get(0).getAddress().equals(ws1.getAddress()));
+		assertEquals(wsl2.get(0).getId(),ws1.getId());
+		assertTrue(wsl2.get(1).getAlias().equals(ws2.getAlias()));
+		assertTrue(wsl2.get(1).getAddress().equals(ws2.getAddress()));
+		assertEquals(wsl2.get(1).getId(),ws2.getId());
+		assertTrue(wsl2.get(2).getAlias().equals(ws3.getAlias()));
+		assertTrue(wsl2.get(2).getAddress().equals(ws3.getAddress()));
+		assertEquals(wsl2.get(2).getId(),ws3.getId());
 	}
 
 }
