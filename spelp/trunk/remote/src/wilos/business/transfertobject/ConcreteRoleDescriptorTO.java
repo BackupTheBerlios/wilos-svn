fa3f7370244bfe1ad73215a0d521e18db6bb0437
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-04 15:39:34 +0000

    [SPELP] Ajout du ConcreteRoleDescriptorTO

---
+++ spelp/trunk/remote/src/wilos/business/transfertobject/ConcreteRoleDescriptorTO.java
@@ -0,0 +1,27 @@
+package wilos.business.transfertobject;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
+import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
+import wilos.model.spem2.role.RoleDescriptor;
+
+public class ConcreteRoleDescriptorTO extends ConcreteRoleDescriptor {
+	
+	/** Creates a new instance of ParticipantTO */
+    public ConcreteRoleDescriptorTO() {
+
+    }    
+  
+    public ConcreteRoleDescriptorTO(ConcreteRoleDescriptor myConcreteRoleDescriptor) {
+    	this.setId(myConcreteRoleDescriptor.getId());
+    	this.setConcreteName(myConcreteRoleDescriptor.getConcreteName());
+    	this.setRoleDescriptor(new RoleDescriptorTO(myConcreteRoleDescriptor.getRoleDescriptor()));
+    	
+        for (ConcreteTaskDescriptor ctd : myConcreteRoleDescriptor.getConcreteTaskDescriptors()) {
+        	this.addConcreteTaskDescriptor(new ConcreteTaskDescriptorTO(ctd));
+        }
+
+    }
+}
