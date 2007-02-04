a6220caa729bb2a81cdb84cc55f7b7bb6998f46f
themorpheus <themorpheus@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-04 15:54:39 +0000

    [Woops2] creation des test sur concreteiteration package

--- woops2/trunk/test/wilos/test/model/misc/AllTests.java
+++ woops2/trunk/test/wilos/test/model/misc/AllTests.java
@@ -13,8 +13,12 @@ public class AllTests {
 	public static Test suite () {
 		TestSuite suite = new TestSuite("Test for wilos.test.model.misc") ;
 		// $JUnit-BEGIN$
+		suite.addTest(wilos.test.model.misc.concreteactivity.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.misc.concretebreakdownelement.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.misc.concreteiteration.AllTests.suite()) ;
 		suite.addTest(wilos.test.model.misc.concretetaskdescriptor.AllTests.suite()) ;
 		suite.addTest(wilos.test.model.misc.concreteroledescriptor.AllTests.suite()) ;
+		suite.addTest(wilos.test.model.misc.concreteworkbreakdownelement.AllTests.suite()) ;
 		// $JUnit-END$
 		return suite ;
 	}
---
+++ woops2/trunk/test/wilos/test/model/misc/concreteiteration/ConcreteIterationTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package wilos.test.model.misc.concreteiteration;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Sebastien
+ *
+ */
+public class ConcreteIterationTest extends TestCase {
+
+	/**
+	 * Test method for {@link wilos.model.misc.concreteiteration.ConcreteIteration#hashCode()}.
+	 */
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wilos.model.misc.concreteiteration.ConcreteIteration#equals(java.lang.Object)}.
+	 */
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wilos.model.misc.concreteiteration.ConcreteIteration#clone()}.
+	 */
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wilos.model.misc.concreteiteration.ConcreteIteration#addIteration(wilos.model.spem2.iteration.Iteration)}.
+	 */
+	public void testAddIteration() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wilos.model.misc.concreteiteration.ConcreteIteration#removeIteration(wilos.model.spem2.iteration.Iteration)}.
+	 */
+	public void testRemoveIteration() {
+		fail("Not yet implemented");
+	}
+
+}
