7cdbb4c7f01afda95cef312813cf38f8bbe068d6
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-04 17:22:47 +0000

    [woops2]: Début de mise en place des Junit dans concretephase

---
+++ woops2/trunk/test/wilos/test/model/misc/concretephase/ConcretePhaseTest.java
@@ -0,0 +1,96 @@
+package wilos.test.model.misc.concretephase;
+
+import junit.framework.TestCase;
+import wilos.model.misc.concretephase.ConcretePhase;
+import wilos.model.spem2.phase.Phase;
+
+/**
+ * 
+ * @author Soosuske
+ *
+ */
+public class ConcretePhaseTest extends TestCase{
+	private ConcretePhase concretephase ;
+
+	public static final String CONCRETENAME = "ConcreteName" ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp() ;
+		this.concretephase = new ConcretePhase() ;
+		this.concretephase.setConcreteName(CONCRETENAME) ;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown() ;
+	}
+
+	/**
+	 * Test method for {@link wilos.model.misc.concretephase.ConcretePhaseTest#hashCode()}.
+	 */
+	public final void testHashCode() {
+		// Rk: the setUp method is called here.
+
+		ConcretePhase concretephas = new ConcretePhase() ;
+		concretephas.setConcreteName(CONCRETENAME) ;
+		assertNotNull(this.concretephase.hashCode()) ;
+		assertNotNull(concretephas.hashCode()) ;
+		assertEquals(this.concretephase.hashCode(), concretephas.hashCode()) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link wilos.model.spem2.phase.Phase#equals(java.lang.Object)}.
+	 */
+	public final void testEquals() {
+		// Rk: the setUp method is called here.
+
+		// Assert if it's equal by references.
+		assertTrue("By references", this.concretephase.equals(this.concretephase)) ;
+
+		// Assert if it's equal field by field.
+		ConcretePhase concretephaseTmp1 = null ;
+		try{
+			concretephaseTmp1 = this.concretephase.clone() ;
+		}
+		catch(CloneNotSupportedException e){
+			fail("Error CloneNotSupportedException in the testEquals method") ;
+		}
+		assertTrue("Field by field", this.concretephase.equals(concretephaseTmp1)) ;
+
+		// Assert if it's not equal.
+		ConcretePhase concretephasTmp2 = new ConcretePhase() ;
+		concretephasTmp2.setConcreteName("concreteFalse") ;
+		assertFalse("Not equals", this.concretephase.equals(concretephasTmp2)) ;
+
+		// Rk: the tearDown method is called here.
+	}
+
+	/**
+	 * Test method for {@link wilos.model.spem2.phase.Phase#clone()}.
+	 */
+	public final void testClone() {
+		// Rk: the setUp method is called here.
+
+		try{
+			assertEquals(this.concretephase.clone(), this.concretephase) ;
+		}
+		catch(CloneNotSupportedException e){
+			fail("Error CloneNotSupportedException in the testClone method") ;
+		}
+
+		// Rk: the tearDown method is called here.
+	}
+	
+	
+}
