404622be4d573bc543cbfcaff3f5ade87bdef918
eperico <eperico@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-05 16:01:47 +0000

    [woops2]: ajout de Junit sur CBDE

--- woops2/trunk/test/wilos/test/model/misc/concretebreakdownelement/ConcreteBreakdownElementTest.java
+++ woops2/trunk/test/wilos/test/model/misc/concretebreakdownelement/ConcreteBreakdownElementTest.java
@@ -1,6 +1,9 @@
 package wilos.test.model.misc.concretebreakdownelement;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
 import wilos.model.misc.concreteactivity.ConcreteActivity;
 import wilos.model.misc.concretebreakdownelement.ConcreteBreakdownElement;
@@ -25,6 +28,7 @@ public class ConcreteBreakdownElementTest extends TestCase {
 		super.setUp() ;
 		this.concreteActivity = new ConcreteActivity() ;
 		this.concreteActivity.setConcreteName("concreteactivityname");
+		
 		this.breakdownElement = new BreakdownElement();
 		this.breakdownElement.setName("name");
 
@@ -95,14 +99,15 @@ public class ConcreteBreakdownElementTest extends TestCase {
 	 */
 	public void testAddSuperConcreteActivity() {
 		// Rk: the setUp method is called here.
-
+		
 		assertNotNull(this.concreteBreakdownElement.getSuperConcreteActivities());
 		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 1);
 		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().contains(this.concreteActivity));
 
+		assertNotNull(this.concreteActivity.getConcreteBreakdownElements());
 		assertTrue(this.concreteActivity.getConcreteBreakdownElements().size() == 1);
 		assertTrue(this.concreteActivity.getConcreteBreakdownElements().contains(this.concreteBreakdownElement));
-
+		
 		// Rk: the tearDown method is called here.
 	}
 
@@ -112,19 +117,88 @@ public class ConcreteBreakdownElementTest extends TestCase {
 	public void testRemoveSuperConcreteActivity() {
 		// Rk: the setUp method is called here.
 
-		ConcreteActivity concreteSuperActivity = new ConcreteActivity();
-
-		assertNotNull(this.concreteBreakdownElement.getSuperConcreteActivities());
-		assertNotNull(concreteSuperActivity.getConcreteBreakdownElements());
-
-		this.concreteBreakdownElement.removeSuperConcreteActivity(concreteSuperActivity);
-		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 0);
-
-		assertNotNull(concreteSuperActivity.getConcreteBreakdownElements());
-
 		this.concreteBreakdownElement.removeSuperConcreteActivity(this.concreteActivity);
 		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 0);
+		
+		ConcreteActivity superConcreteActivity = new ConcreteActivity();
+		superConcreteActivity.setConcreteName("Name");
+		
+		assertNotNull(this.concreteBreakdownElement.getSuperConcreteActivities());
+		assertNotNull(superConcreteActivity.getConcreteBreakdownElements());
 
+		this.concreteBreakdownElement.removeSuperConcreteActivity(superConcreteActivity);
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 0);
+
+		assertNotNull(superConcreteActivity.getConcreteBreakdownElements());
+
+		// Rk: the tearDown method is called here.
+	}
+	
+	public void testAddAllSuperActivities() {
+		
+		// Rk: the setUp method is called here.
+		
+		this.concreteBreakdownElement.removeSuperConcreteActivity(this.concreteActivity);
+		assertEquals(0, this.concreteBreakdownElement.getSuperConcreteActivities().size());
+		
+		ConcreteActivity cAct = new ConcreteActivity() ;
+		cAct.setConcreteName("a name");
+		
+		ConcreteActivity cAct2 = new ConcreteActivity() ;
+		cAct2.setConcreteName("another name");
+		
+		Set<ConcreteActivity> list = new HashSet<ConcreteActivity>();
+		list.add(cAct);
+		list.add(cAct2);
+		this.concreteBreakdownElement.addAllSuperActivities(list);
+		
+		this.concreteBreakdownElement.addBreakdownElement(this.breakdownElement);
+		
+		assertNotNull(this.concreteBreakdownElement.getSuperConcreteActivities());
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 2);
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct));
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct2));
+		
+		assertTrue(this.concreteActivity.getConcreteBreakdownElements().size() == 1);
+		assertTrue(cAct.getConcreteBreakdownElements().contains(this.concreteBreakdownElement));
+		assertTrue(cAct2.getConcreteBreakdownElements().contains(this.concreteBreakdownElement));
+		
+		// Rk: the tearDown method is called here.
+	}
+
+	public void testRemoveAllSuperConcreteActivities() {
+		// Rk: the setUp method is called here.
+
+		this.concreteBreakdownElement.removeSuperConcreteActivity(this.concreteActivity);
+		assertEquals(0, this.concreteBreakdownElement.getSuperConcreteActivities().size());
+		
+		ConcreteActivity cAct = new ConcreteActivity() ;
+		cAct.setConcreteName("a name");
+		
+		ConcreteActivity cAct2 = new ConcreteActivity() ;
+		cAct2.setConcreteName("another name");
+		
+		Set<ConcreteActivity> list = new HashSet<ConcreteActivity>();
+		list.add(cAct);
+		list.add(cAct2);
+		this.concreteBreakdownElement.addAllSuperActivities(list);
+		
+		this.concreteBreakdownElement.addBreakdownElement(this.breakdownElement);
+		
+		assertNotNull(this.concreteBreakdownElement.getSuperConcreteActivities());
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 2);
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct));
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct2));
+		
+		assertTrue(this.concreteActivity.getConcreteBreakdownElements().size() == 1);
+		assertTrue(cAct.getConcreteBreakdownElements().contains(this.concreteBreakdownElement));
+		assertTrue(cAct2.getConcreteBreakdownElements().contains(this.concreteBreakdownElement));
+		
+		this.concreteBreakdownElement.removeAllSuperConcreteActivities();
+		assertTrue(this.concreteBreakdownElement.getSuperConcreteActivities().size() == 0);
+		assertFalse(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct));
+		assertFalse(this.concreteBreakdownElement.getSuperConcreteActivities().contains(cAct2));
+		
 		// Rk: the tearDown method is called here.
 	}
 }
