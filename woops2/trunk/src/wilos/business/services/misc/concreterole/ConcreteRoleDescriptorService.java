58b54e4f9010ec3762c3520cc8fc1de69a9d4bc2
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-06 18:19:58 +0000

    [Woops2] Clean des services.

--- woops2/trunk/src/wilos/business/services/misc/concreterole/ConcreteRoleDescriptorService.java
+++ woops2/trunk/src/wilos/business/services/misc/concreterole/ConcreteRoleDescriptorService.java
@@ -3,8 +3,6 @@ package wilos.business.services.misc.concreterole;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,8 +19,6 @@ public class ConcreteRoleDescriptorService {
 
 		private ConcreteRoleDescriptorDao concreteRoleDescriptorDao;
 
-		protected final Log logger = LogFactory.getLog(this.getClass()) ;
-
 		/**
 		 * Return concreteRoleDescriptor for a project list
 		 *
@@ -30,35 +26,9 @@ public class ConcreteRoleDescriptorService {
 		 */
 		@Transactional(readOnly = true)
 		public List<ConcreteRoleDescriptor> getConcreteRoleDescriptorsForProject(String _projectId) {
-			List<ConcreteRoleDescriptor> roleDescriptorsList = this.concreteRoleDescriptorDao.getAllConcreteRoleDescriptors();
-			List<ConcreteRoleDescriptor> returnedList = new ArrayList<ConcreteRoleDescriptor>();
-
-			for (ConcreteRoleDescriptor concreteRoleDescriptor : roleDescriptorsList) {
-				//FIXME Pb Service : getConcreteRoleDescriptorsForProject // manque methode
-				/*if (concreteRoleDescriptor.getProject().getProject_id().equals(_projectId)) {
-					returnedList.add(concreteRoleDescriptor);
-				}
-				*/
-			}
-
-			return returnedList;
+			//FIXME !!! retrieve all the crds of this project.
+			return new ArrayList<ConcreteRoleDescriptor>();
 		}
-
-		/**
-		 * TODO Method description
-		 *
-		 * @param _td
-		 */
-		/*public void roleDescriptorInstanciation (String _projectId, RoleDescriptor rd) {
-
-			ConcreteRoleDescriptor crd = new ConcreteRoleDescriptor();
-
-			crd.setConcreteName(rd.getPresentationName());
-			crd.setProject(_projectId);
-			crd.addRoleDescriptor(rd);
-
-			this.concreteTaskDescriptorDao.saveOrUpdateConcreteTaskDescriptor(ctd);
-		}*/
 
 		public ConcreteRoleDescriptorDao getConcreteRoleDescriptorDao() {
 			return concreteRoleDescriptorDao;
