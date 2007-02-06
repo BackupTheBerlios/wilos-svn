e1511c118cf0fe0dc920ce3d9cfc9211f8c70177
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-06 18:11:49 +0000

    [Woops2] Clean des services.

--- woops2/trunk/test/wilos/test/business/services/spem2/role/RoleDescriptorServiceTest.java
+++ woops2/trunk/test/wilos/test/business/services/spem2/role/RoleDescriptorServiceTest.java
@@ -6,35 +6,23 @@ import junit.framework.TestCase;
 public class RoleDescriptorServiceTest extends TestCase {
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp() ;
 	}
 
+	public void testRoleDescriptorInstanciation(){
+		//TODO !!!
+	}
+
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#tearDown()
 	 */
 	protected void tearDown() throws Exception {
 		super.tearDown() ;
-	}
-
-	/**
-	 * Test method for
-	 * {@link wilos.business.services.spem2.role.RoleDescriptorService#getRoleDescriptorsFromProcess(java.lang.String)}.
-	 */
-	public void testGetRoleDescriptorsFromProcess() {
-		fail("Not yet implemented") ;
-	}
-
-	/**
-	 * Test method for
-	 * {@link wilos.business.services.spem2.role.RoleDescriptorService#getRoleDefinitionFromRoleDescriptor(java.lang.String)}.
-	 */
-	public void testGetRoleDefinitionFromRoleDescriptor() {
-		fail("Not yet implemented") ;
 	}
 }
