36cd65323bbeec57654e78a84f0d85cda36d5a32
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-06 18:17:09 +0000

    [Woops2] Clean des services.

--- woops2/trunk/src/wilos/business/services/misc/concretetask/ConcreteTaskDescriptorService.java
+++ woops2/trunk/src/wilos/business/services/misc/concretetask/ConcreteTaskDescriptorService.java
@@ -1,20 +1,13 @@
 package wilos.business.services.misc.concretetask;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import wilos.business.services.spem2.role.RoleDescriptorService;
-import wilos.business.services.spem2.task.TaskDescriptorService;
 import wilos.hibernate.misc.concretetask.ConcreteTaskDescriptorDao;
-import wilos.hibernate.spem2.role.RoleDescriptorDao;
-import wilos.hibernate.spem2.task.TaskDescriptorDao;
 import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
 import wilos.model.misc.wilosuser.Participant;
 import wilos.utils.Constantes;
@@ -30,34 +23,20 @@ public class ConcreteTaskDescriptorService {
 
 	private ConcreteTaskDescriptorDao concreteTaskDescriptorDao;
 
-	private RoleDescriptorDao roleDescriptorDao;
-
-	private TaskDescriptorDao taskDescriptorDao;
-
-	private TaskDescriptorService taskDescriptorService;
-
-	private RoleDescriptorService roleDescriptorService;
-
 	/**
 	 * Return concreteTaskDescriptor for a project list
 	 *
 	 * @return
 	 */
 	@Transactional(readOnly = true)
-	public List<ConcreteTaskDescriptor> getConcreteTaskDescriptorsForProject(
+	public Set<ConcreteTaskDescriptor> getConcreteTaskDescriptorsForProject(
 			String _projectId) {
-		List<ConcreteTaskDescriptor> tempList = this.concreteTaskDescriptorDao
-				.getAllConcreteTaskDescriptors();
-		List<ConcreteTaskDescriptor> returnedList = new ArrayList<ConcreteTaskDescriptor>();
+		//FIXME !!! retrieve all ctds of this project.
+		return null;
+	}
 
-		/*
-		 * FIXME Urgent (project) !!! for (ConcreteTaskDescriptor
-		 * concreteTaskDescriptor : tempList) { if
-		 * (concreteTaskDescriptor.getProject().getProject_id().equals(_projectId)) {
-		 * returnedList.add(concreteTaskDescriptor); } }
-		 */
-
-		return returnedList;
+	public ConcreteTaskDescriptor getConcreteTaskDescriptor(String _concreteTaskDescriptorId) {
+		return this.getConcreteTaskDescriptorDao().getConcreteTaskDescriptor(_concreteTaskDescriptorId);
 	}
 
 	/**
@@ -145,21 +124,6 @@ public class ConcreteTaskDescriptorService {
 	}
 
 	/**
-	 *
-	 * TODO Method description
-	 *
-	 * @param _processId
-	 * @return
-	 */
-	public Set<ConcreteTaskDescriptor> getConcreteTaskDescriptorFromProject(
-			String _projectId) {
-		Set<ConcreteTaskDescriptor> ctds = new HashSet<ConcreteTaskDescriptor>();
-		ctds.addAll(this.concreteTaskDescriptorDao
-				.getConcreteTaskDescriptorFromProject(_projectId));
-		return ctds;
-	}
-
-	/**
 	 * Visible ob affected buton
 	 */
 	public boolean affectedVisible(
@@ -182,7 +146,6 @@ public class ConcreteTaskDescriptorService {
 		 */
 
 		return visi;
-
 	}
 
 	/**
@@ -228,39 +191,5 @@ public class ConcreteTaskDescriptorService {
 	public void setConcreteTaskDescriptorDao(
 			ConcreteTaskDescriptorDao _concreteTaskDescriptorDao) {
 		this.concreteTaskDescriptorDao = _concreteTaskDescriptorDao;
-	}
-
-	public RoleDescriptorDao getRoleDescriptorDao() {
-		return roleDescriptorDao;
-	}
-
-	public void setRoleDescriptorDao(RoleDescriptorDao _roleDescriptorDao) {
-		this.roleDescriptorDao = _roleDescriptorDao;
-	}
-
-	public TaskDescriptorDao getTaskDescriptorDao() {
-		return taskDescriptorDao;
-	}
-
-	public void setTaskDescriptorDao(TaskDescriptorDao _taskDescriptorDao) {
-		taskDescriptorDao = _taskDescriptorDao;
-	}
-
-	public TaskDescriptorService getTaskDescriptorService() {
-		return this.taskDescriptorService;
-	}
-
-	public void setTaskDescriptorService(
-			TaskDescriptorService _taskDescriptorService) {
-		this.taskDescriptorService = _taskDescriptorService;
-	}
-
-	public RoleDescriptorService getRoleDescriptorService() {
-		return roleDescriptorService;
-	}
-
-	public void setRoleDescriptorService(
-			RoleDescriptorService roleDescriptorService) {
-		this.roleDescriptorService = roleDescriptorService;
 	}
 }
