fceb57e24ae72cc23b1994b1f7576af18d902394
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-07 17:27:54 +0000

    [SPELP] Ajout d'une application de test permettant d'initialiser la bd

---
+++ spelp/trunk/remote/applitest/wilos/application/console/ProcessImportTest.java
@@ -0,0 +1,23 @@
+package wilos.application.console;
+
+import java.io.File;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import wilos.business.services.spem2.process.ProcessService;
+import wilos.model.spem2.process.Process;
+
+public class ProcessImportTest {
+	public static void main(String[] args) {
+		// Getback the application context from the spring configuration file
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		ProcessService am = (ProcessService) ctx.getBean("ProcessService");
+		/*Process scrum = am.spelpParsingXML(new File("applitest/wilos/application/console/scrum.xml"));
+		am.saveProcess(scrum);*/
+		
+		Process openup = am.spelpParsingXML(new File("applitest/wilos/application/console/openUP.xml"));
+		am.saveProcess(openup);
+		}
+}
