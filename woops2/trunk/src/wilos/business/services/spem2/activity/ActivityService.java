ba1571329a7c1e5600cd779062d4bd970ea9ef20
aollier <aollier@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-07 16:24:22 +0000

    [Woops2] Modif instanciation ActivityService

--- woops2/trunk/src/wilos/business/services/spem2/activity/ActivityService.java
+++ woops2/trunk/src/wilos/business/services/spem2/activity/ActivityService.java
@@ -1,16 +1,17 @@
-
-package wilos.business.services.spem2.activity ;
+package wilos.business.services.spem2.activity;
 
 import java.util.List;
 
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import wilos.business.services.spem2.breakdownelement.BreakdownElementService;
 import wilos.hibernate.misc.concreteactivity.ConcreteActivityDao;
 import wilos.hibernate.spem2.activity.ActivityDao;
 import wilos.model.misc.concreteactivity.ConcreteActivity;
 import wilos.model.misc.project.Project;
 import wilos.model.spem2.activity.Activity;
+import wilos.model.spem2.breakdownelement.BreakdownElement;
 
 /**
  * ActivityManager is a transactional class, that manages operations about activity, requested by web pages (activity.jsp &
@@ -22,65 +23,77 @@ import wilos.model.spem2.activity.Activity;
 @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 public class ActivityService {
 
-	private ActivityDao activityDao ;
+	private ActivityDao activityDao;
 
 	private ConcreteActivityDao concreteActivityDao;
-	
+
+	private BreakdownElementService breakdownElementService = new BreakdownElementService();
+
 	/**
-	 * Instanciates an acticity for a project
+	 * Instanciates an activity for a project
 	 * @param _project project for which the activity shall be instanciated
-	 * @param _phase activity to instanciates
+	 * @param _phase activity to instanciate
 	 */
-	public void activityInstanciation (Project _project, Activity _activity) {
+	public void activityInstanciation(Project _project, Activity _activity) {
 
 		ConcreteActivity cact = new ConcreteActivity();
 
 		if (_activity.getPresentationName() == null)
-			cact.setConcreteName(_activity.getName()) ;
+			cact.setConcreteName(_activity.getName());
 		else
 			cact.setConcreteName(_activity.getPresentationName());
-		
+
 		cact.addActivity(_activity);
+
+		/* TODO verifier code par un M1 :) */
+		/* instanciating and adding all the ConcreteBreakdownElements included in the activity */
+		for (BreakdownElement bde : _activity.getBreakdownElements()) {
+			if (bde.getConcreteBreakdownElements().size() == 0) {
+				this.breakdownElementService.breakdownElementInstanciation(
+						_project, bde);
+			}
+			cact.addAllConcreteBreakdownElements(bde.getConcreteBreakdownElements());
+		}
 
 		this.concreteActivityDao.saveOrUpdateConcreteActivity(cact);
 	}
 
 	/**
-	 * Returns activities list
-	 *
+	 * Return activities list
+	 * 
 	 * @return List of activities
 	 */
 	@Transactional(readOnly = true)
 	public List<Activity> getActivitiesList() {
-		return this.activityDao.getAllActivities() ;
+		return this.activityDao.getAllActivities();
 	}
 
 	/**
-	 * Saves activity
-	 *
+	 * Save activity
+	 * 
 	 * @param _activity the activity to save
 	 */
 	public void saveActivity(Activity _activity) {
-		activityDao.saveOrUpdateActivity(_activity) ;
+		activityDao.saveOrUpdateActivity(_activity);
 	}
 
 	/**
 	 * Getter of activityDao.
-	 *
+	 * 
 	 * @return the activityDao.
 	 */
 	public ActivityDao getActivityDao() {
-		return this.activityDao ;
+		return this.activityDao;
 	}
 
 	/**
 	 * Setter of activityDao.
-	 *
+	 * 
 	 * @param _activityDao
 	 *            The activityDao to set.
 	 */
 	public void setActivityDao(ActivityDao _activityDao) {
-		this.activityDao = _activityDao ;
+		this.activityDao = _activityDao;
 	}
 
 	/**
@@ -95,7 +108,8 @@ public class ActivityService {
 	/**
 	 * Setter of concreteActivityDao
 	 * 
-	 * @param concreteActivityDao the concreteActivityDao to set
+	 * @param concreteActivityDao
+	 *            the concreteActivityDao to set
 	 */
 	public void setConcreteActivityDao(ConcreteActivityDao concreteActivityDao) {
 		this.concreteActivityDao = concreteActivityDao;
