365d1a85072c1c2ea2c92699811d2b9f45a15ddc
aollier <aollier@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-07 16:26:14 +0000

    [Woops2] Modif instanciation BreakdownElementService

--- woops2/trunk/src/wilos/business/services/spem2/breakdownelement/BreakdownElementService.java
+++ woops2/trunk/src/wilos/business/services/spem2/breakdownelement/BreakdownElementService.java
@@ -4,9 +4,11 @@ package wilos.business.services.spem2.breakdownelement ;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import wilos.business.services.spem2.activity.ActivityService;
 import wilos.hibernate.misc.concretebreakdownelement.ConcreteBreakdownElementDao;
 import wilos.model.misc.concretebreakdownelement.ConcreteBreakdownElement;
 import wilos.model.misc.project.Project;
+import wilos.model.spem2.activity.Activity;
 import wilos.model.spem2.breakdownelement.BreakdownElement;
 
 /**
@@ -19,12 +21,13 @@ public class BreakdownElementService {
 
 	private ConcreteBreakdownElementDao concreteBreakdownElementDao ;
 	
+	private ActivityService activityService = new ActivityService() ;
+
 	/**
 	 * Instanciates a BreakdownElement
 	 * @param _project project for which the BreakdownElement shall be instanciated
 	 * @param _bde BreakdownElement to instanciate
 	 */
-
 	public void breakdownElementInstanciation (Project _project, BreakdownElement _bde) {
 
 		ConcreteBreakdownElement cbe = new ConcreteBreakdownElement();
@@ -35,6 +38,15 @@ public class BreakdownElementService {
 			cbe.setConcreteName(_bde.getPresentationName());
 		
 		cbe.addBreakdownElement(_bde);
+		
+		/* TODO verifier code par un M1 :) */
+		/* instanciating and adding all the ConcreteActivities included in the breakdownelement */
+		for (Activity act : _bde.getSuperActivities()) {
+			if (act.getBreakdownElements().size() == 0) {
+				this.activityService.activityInstanciation(_project, act);
+			}
+			_bde.addAllConcreteBreakdownElements(act.getConcreteBreakdownElements());
+		}
 
 		this.concreteBreakdownElementDao.saveOrUpdateConcreteBreakdownElement(cbe);
 	}
