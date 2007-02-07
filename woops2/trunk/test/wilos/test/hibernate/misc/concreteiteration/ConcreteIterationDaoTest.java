e0fcfe284d720c506df5c406a9c69c08bd5d1e92
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-07 19:17:52 +0000

    [Woops2] Revisitation des *ServiceTest et *DaoTest pour l'optimisation de l'execution des tests.

--- woops2/trunk/test/wilos/test/hibernate/misc/concreteiteration/ConcreteIterationDaoTest.java
+++ woops2/trunk/test/wilos/test/hibernate/misc/concreteiteration/ConcreteIterationDaoTest.java
@@ -8,128 +8,156 @@ import wilos.model.misc.concreteiteration.ConcreteIteration;
 import wilos.test.TestConfiguration;
 
 /**
- * 
+ *
  * @author Almiriad
  *
  */
 public class ConcreteIterationDaoTest extends TestCase {
-	private ConcreteIterationDao concreteIterationDao = (ConcreteIterationDao) TestConfiguration.getInstance().getApplicationContext().getBean("ConcreteIterationDao") ;
+
+	private ConcreteIterationDao concreteIterationDao = null;
 
 	private ConcreteIteration concreteIteration;
-	
-	public static final String CONCRETE_NAME = "thisConcreteIteration" ;
+
+	public static final String CONCRETE_NAME = "thisConcreteIteration";
+
+	public ConcreteIterationDaoTest() {
+		concreteIterationDao = (ConcreteIterationDao) TestConfiguration
+				.getInstance().getApplicationContext().getBean(
+						"ConcreteIterationDao");
+	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#setUp()
 	 */
-	@ Override
+	@Override
 	protected void setUp() throws Exception {
-		super.setUp() ;
-
-		// Get the ConcreteIterationDao Singleton for managing ConcreteIteration data
-		this.concreteIterationDao = (ConcreteIterationDao) TestConfiguration.getInstance().getApplicationContext().getBean("ConcreteIterationDao") ;
+		super.setUp();
 
 		// Create empty ConcreteIteration
-		this.concreteIteration = new ConcreteIteration() ;
+		this.concreteIteration = new ConcreteIteration();
 
 		// Save the ConcreteIteration with the method to test.
-		this.concreteIterationDao.saveOrUpdateConcreteIteration(this.concreteIteration) ;
+		this.concreteIterationDao
+				.saveOrUpdateConcreteIteration(this.concreteIteration);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#tearDown()
 	 */
-	@ Override
+	@Override
 	protected void tearDown() throws Exception {
-		super.tearDown() ;
+		super.tearDown();
 
-		this.concreteIterationDao.deleteConcreteIteration(this.concreteIteration) ;
+		this.concreteIterationDao
+				.deleteConcreteIteration(this.concreteIteration);
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.ConcreteIteration.ConcreteIterationDao#saveOrUpdateProcess(woops2.model.process.Process)}.
+	 * Test method for
+	 * {@link woops2.hibernate.ConcreteIteration.ConcreteIterationDao#saveOrUpdateProcess(woops2.model.process.Process)}.
 	 */
 	public final void testSaveOrUpdateConcreteIteration() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
-		//Save the ConcreteIteration with the method to test.
-		this.concreteIterationDao.saveOrUpdateConcreteIteration(this.concreteIteration) ;
-		
+		// Save the ConcreteIteration with the method to test.
+		this.concreteIterationDao
+				.saveOrUpdateConcreteIteration(this.concreteIteration);
+
 		// Check the saving.
-		String id = this.concreteIteration.getId() ;
-		ConcreteIteration ConcreteIterationTmp = (ConcreteIteration) this.concreteIterationDao.getConcreteIteration(id) ;
-		assertNotNull(ConcreteIterationTmp) ;
-		
+		String id = this.concreteIteration.getId();
+		ConcreteIteration ConcreteIterationTmp = (ConcreteIteration) this.concreteIterationDao
+				.getConcreteIteration(id);
+		assertNotNull(ConcreteIterationTmp);
+
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#getAllProcesses()}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#getAllProcesses()}.
 	 */
 	public final void testGetAllConcreteIterations() {
-		//Rk: the setUp method is called here.
-		
-		//Save the ConcreteIteration with the method to test.
-		this.concreteIterationDao.saveOrUpdateConcreteIteration(this.concreteIteration) ;
-		
-		// Look if this ConcreteIteration is also into the database and look if the size of the set is >= 1.
-		List<ConcreteIteration> ConcreteIterations = this.concreteIterationDao.getAllConcreteIterations() ;
-		assertNotNull(ConcreteIterations) ;
-		assertTrue(ConcreteIterations.size() >= 1) ;
+		// Rk: the setUp method is called here.
+
+		// Save the ConcreteIteration with the method to test.
+		this.concreteIterationDao
+				.saveOrUpdateConcreteIteration(this.concreteIteration);
+
+		// Look if this ConcreteIteration is also into the database and look if
+		// the size of the set is >= 1.
+		List<ConcreteIteration> ConcreteIterations = this.concreteIterationDao
+				.getAllConcreteIterations();
+		assertNotNull(ConcreteIterations);
+		assertTrue(ConcreteIterations.size() >= 1);
 
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#getProcess(java.lang.String)}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#getProcess(java.lang.String)}.
 	 */
 	public final void testGetConcreteIteration() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
 		// Add properties to the ConcreteIteration.
-		this.concreteIteration.setConcreteName(CONCRETE_NAME) ;
-		
-		// Save the ConcreteIteration into the database.
-		this.concreteIterationDao.saveOrUpdateConcreteIteration(this.concreteIteration) ;
-		String id = this.concreteIteration.getId() ;
+		this.concreteIteration.setConcreteName(CONCRETE_NAME);
 
-		// Test the method getConcreteIteration with an existing ConcreteIteration.
-		ConcreteIteration concreteIterationTmp = this.concreteIterationDao.getConcreteIteration(id) ;
-		assertNotNull(concreteIterationTmp) ;
-		assertEquals("Name", concreteIterationTmp.getConcreteName(), CONCRETE_NAME) ;
-		
-		// Test the method getConcreteIteration with an unexisting ConcreteIteration.
-		this.concreteIterationDao.deleteConcreteIteration(this.concreteIteration) ;
-		concreteIterationTmp = this.concreteIterationDao.getConcreteIteration(id) ;
-		assertNull(concreteIterationTmp) ;
+		// Save the ConcreteIteration into the database.
+		this.concreteIterationDao
+				.saveOrUpdateConcreteIteration(this.concreteIteration);
+		String id = this.concreteIteration.getId();
+
+		// Test the method getConcreteIteration with an existing
+		// ConcreteIteration.
+		ConcreteIteration concreteIterationTmp = this.concreteIterationDao
+				.getConcreteIteration(id);
+		assertNotNull(concreteIterationTmp);
+		assertEquals("Name", concreteIterationTmp.getConcreteName(),
+				CONCRETE_NAME);
+
+		// Test the method getConcreteIteration with an unexisting
+		// ConcreteIteration.
+		this.concreteIterationDao
+				.deleteConcreteIteration(this.concreteIteration);
+		concreteIterationTmp = this.concreteIterationDao
+				.getConcreteIteration(id);
+		assertNull(concreteIterationTmp);
 
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#deleteProcess(woops2.model.process.Process)}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#deleteProcess(woops2.model.process.Process)}.
 	 */
 	public final void testDeleteConcreteIteration() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
 		// Save the ConcreteIteration into the database.
-		this.concreteIterationDao.saveOrUpdateConcreteIteration(this.concreteIteration) ;
-		String id = this.concreteIteration.getId() ;
+		this.concreteIterationDao
+				.saveOrUpdateConcreteIteration(this.concreteIteration);
+		String id = this.concreteIteration.getId();
 
-		// Test the method deleteConcreteIteration with an ConcreteIteration existing into the db.
-		this.concreteIterationDao.deleteConcreteIteration(this.concreteIteration) ;
+		// Test the method deleteConcreteIteration with an ConcreteIteration
+		// existing into the db.
+		this.concreteIterationDao
+				.deleteConcreteIteration(this.concreteIteration);
 
 		// See if this.ConcreteIteration is now absent in the db.
-		ConcreteIteration concreteIterationTmp = (ConcreteIteration) this.concreteIterationDao.getConcreteIteration(id) ;
-		assertNull(concreteIterationTmp) ;
+		ConcreteIteration concreteIterationTmp = (ConcreteIteration) this.concreteIterationDao
+				.getConcreteIteration(id);
+		assertNull(concreteIterationTmp);
 
-		// Test the method deleteConcreteIteration with an ConcreteIteration unexisting into the db.
+		// Test the method deleteConcreteIteration with an ConcreteIteration
+		// unexisting into the db.
 		// Normally here there are no exception thrown.
-		this.concreteIterationDao.deleteConcreteIteration(this.concreteIteration) ;
+		this.concreteIterationDao
+				.deleteConcreteIteration(this.concreteIteration);
 
 		// Rk: the tearDown method is called here.
 	}
--- woops2/trunk/test/wilos/test/hibernate/misc/concretephase/ConcretePhaseDaoTest.java
+++ woops2/trunk/test/wilos/test/hibernate/misc/concretephase/ConcretePhaseDaoTest.java
@@ -1,7 +1,5 @@
 package wilos.test.hibernate.misc.concretephase;
 
-
-
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -10,131 +8,143 @@ import wilos.model.misc.concretephase.ConcretePhase;
 import wilos.test.TestConfiguration;
 
 /**
- * 
+ *
  * @author Soosuske
  *
  */
-public class ConcretePhaseDaoTest extends TestCase{
-	private ConcretePhaseDao concretePhaseDao = null ;
+public class ConcretePhaseDaoTest extends TestCase {
 
-	private ConcretePhase concretePhase = null ;
-	
-	public static final String CONCRETENAME = "thisPhase" ;
+	private ConcretePhaseDao concretePhaseDao = null;
 
+	private ConcretePhase concretePhase = null;
+
+	public static final String CONCRETENAME = "thisPhase";
+
+	public ConcretePhaseDaoTest() {
+		// Get the ConcretePhaseDao Singleton for managing ConcretePhase data
+		this.concretePhaseDao = (ConcretePhaseDao) TestConfiguration
+				.getInstance().getApplicationContext().getBean(
+						"ConcretePhaseDao");
+	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#setUp()
 	 */
-	@ Override
+	@Override
 	protected void setUp() throws Exception {
-		super.setUp() ;
-
-		// Get the ConcretePhaseDao Singleton for managing ConcretePhase data
-		this.concretePhaseDao = (ConcretePhaseDao) TestConfiguration.getInstance().getApplicationContext().getBean("ConcretePhaseDao") ;
+		super.setUp();
 
 		// Create empty Concretephase
-		this.concretePhase = new ConcretePhase() ;
+		this.concretePhase = new ConcretePhase();
 
 		// Save the activity with the method to test.
-		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase) ;
+		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#tearDown()
 	 */
-	@ Override
+	@Override
 	protected void tearDown() throws Exception {
-		super.tearDown() ;
+		super.tearDown();
 
-		this.concretePhaseDao.deleteConcretePhase(this.concretePhase) ;
+		this.concretePhaseDao.deleteConcretePhase(this.concretePhase);
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#saveOrUpdateProcess(woops2.model.process.Process)}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#saveOrUpdateProcess(woops2.model.process.Process)}.
 	 */
 	public final void testSaveOrUpdateConcretePhase() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
-		//Save the activity with the method to test.
-		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase) ;
-		
+		// Save the activity with the method to test.
+		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase);
+
 		// Check the saving.
-		String id = this.concretePhase.getId() ;
-		ConcretePhase concretePhaseTmp = (ConcretePhase) this.concretePhaseDao.getConcretePhase(id) ;
-		assertNotNull(concretePhaseTmp) ;
-		
+		String id = this.concretePhase.getId();
+		ConcretePhase concretePhaseTmp = (ConcretePhase) this.concretePhaseDao
+				.getConcretePhase(id);
+		assertNotNull(concretePhaseTmp);
+
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#getAllProcesses()}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#getAllProcesses()}.
 	 */
 	public final void testGetAllConcretePhases() {
-		//Rk: the setUp method is called here.
-		
-		//Save the activity with the method to test.
-		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase) ;
-		
-		// Look if this activity is also into the database and look if the size of the set is >= 1.
-		List<ConcretePhase> concretePhases = this.concretePhaseDao.getAllConcretePhases() ;
-		assertNotNull(concretePhases) ;
-		assertTrue(concretePhases.size() >= 1) ;
+		// Rk: the setUp method is called here.
+
+		// Save the activity with the method to test.
+		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase);
+
+		// Look if this activity is also into the database and look if the size
+		// of the set is >= 1.
+		List<ConcretePhase> concretePhases = this.concretePhaseDao
+				.getAllConcretePhases();
+		assertNotNull(concretePhases);
+		assertTrue(concretePhases.size() >= 1);
 
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#getProcess(java.lang.String)}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#getProcess(java.lang.String)}.
 	 */
 	public final void testGetConcretePhase() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
 		// Add properties to the activity.
-		this.concretePhase.setConcreteName(CONCRETENAME) ;
-		
+		this.concretePhase.setConcreteName(CONCRETENAME);
 
 		// Save the activity into the database.
-		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase) ;
-		String id = this.concretePhase.getId() ;
+		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase);
+		String id = this.concretePhase.getId();
 
 		// Test the method getActivity with an existing activity.
-		ConcretePhase concretePhasesTmp = this.concretePhaseDao.getConcretePhase(id) ;
-		assertNotNull(concretePhasesTmp) ;
-		assertEquals("Name", concretePhasesTmp.getConcreteName(), CONCRETENAME) ;
-		
+		ConcretePhase concretePhasesTmp = this.concretePhaseDao
+				.getConcretePhase(id);
+		assertNotNull(concretePhasesTmp);
+		assertEquals("Name", concretePhasesTmp.getConcreteName(), CONCRETENAME);
 
 		// Test the method getActivity with an unexisting activity.
-		this.concretePhaseDao.deleteConcretePhase(this.concretePhase) ;
-		concretePhasesTmp = this.concretePhaseDao.getConcretePhase(id) ;
-		assertNull(concretePhasesTmp) ;
+		this.concretePhaseDao.deleteConcretePhase(this.concretePhase);
+		concretePhasesTmp = this.concretePhaseDao.getConcretePhase(id);
+		assertNull(concretePhasesTmp);
 
 		// Rk: the tearDown method is called here.
 	}
 
 	/*
-	 * Test method for {@link woops2.hibernate.process.ProcessDao#deleteProcess(woops2.model.process.Process)}.
+	 * Test method for
+	 * {@link woops2.hibernate.process.ProcessDao#deleteProcess(woops2.model.process.Process)}.
 	 */
 	public final void testDeleteConcretePhase() {
-		//Rk: the setUp method is called here.
+		// Rk: the setUp method is called here.
 
 		// Save the activity into the database.
-		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase) ;
-		String id = this.concretePhase.getId() ;
+		this.concretePhaseDao.saveOrUpdateConcretePhase(this.concretePhase);
+		String id = this.concretePhase.getId();
 
 		// Test the method deleteActivity with an activity existing into the db.
-		this.concretePhaseDao.deleteConcretePhase(this.concretePhase) ;
+		this.concretePhaseDao.deleteConcretePhase(this.concretePhase);
 
 		// See if this.activity is now absent in the db.
-		ConcretePhase concretePhaseTmp = (ConcretePhase) this.concretePhaseDao.getConcretePhase(id) ;
-		assertNull(concretePhaseTmp) ;
+		ConcretePhase concretePhaseTmp = (ConcretePhase) this.concretePhaseDao
+				.getConcretePhase(id);
+		assertNull(concretePhaseTmp);
 
-		// Test the method deleteActivity with an activity unexisting into the db.
+		// Test the method deleteActivity with an activity unexisting into the
+		// db.
 		// Normally here there are no exception thrown.
-		this.concretePhaseDao.deleteConcretePhase(this.concretePhase) ;
+		this.concretePhaseDao.deleteConcretePhase(this.concretePhase);
 
 		// Rk: the tearDown method is called here.
 	}
