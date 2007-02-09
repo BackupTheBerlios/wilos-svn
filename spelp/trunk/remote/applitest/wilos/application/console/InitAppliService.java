6ad5ea7d1ef127f09691120ce592251c286caf21
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-09 12:45:29 +0000

    [SPELP] mise a jour de l'appli de test

---
+++ spelp/trunk/remote/applitest/wilos/application/console/InitAppliService.java
@@ -0,0 +1,149 @@
+package wilos.application.console;
+
+import java.io.File;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import wilos.business.services.misc.concretetask.ConcreteTaskDescriptorService;
+import wilos.business.services.misc.wilosuser.ParticipantService;
+import wilos.business.services.spem2.process.ProcessService;
+import wilos.business.util.Security;
+import wilos.hibernate.misc.concreterole.ConcreteRoleDescriptorDao;
+import wilos.hibernate.misc.project.ProjectDao;
+import wilos.hibernate.spem2.process.ProcessDao;
+import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
+import wilos.model.misc.project.Project;
+import wilos.model.misc.wilosuser.Participant;
+import wilos.model.spem2.process.Process;
+
+@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+public class InitAppliService {
+	private ProcessService processService;
+	private ProjectDao projectDao;
+	private ProcessDao processDao;
+	private ParticipantService participantService;
+	private ConcreteRoleDescriptorDao concreteRoleDescriptorDao;
+	private ConcreteTaskDescriptorService concreteTaskDescriptorService;
+	
+	public void initAppli() {
+		Process openup = processService.spelpParsingXML(new File("applitest/wilos/application/console/openUP.xml"));
+		processService.saveProcess(openup);
+		Process scrum = processService.spelpParsingXML(new File("applitest/wilos/application/console/scrum.xml"));
+		processService.saveProcess(scrum);
+		
+		
+		// instanciation des projets
+	//	ProjectDao pm = (ProjectDao) factory.getBean("ProjectDao");
+		//ProcessDao p = (ProcessDao) factory.getBean("ProcessDao");
+
+		String s = processService.getProcessDao().getProcessFromGuid("_9llsAQAvEdubGMceRDupFQ").getId();
+		scrum = processDao.getProcess(s);
+		
+		Project project = new Project();
+		project.setConcreteName("Wilos");
+		project.setProcess(scrum);
+		projectDao.saveOrUpdateProject(project);
+
+		processService.projectInstanciation(project);
+
+		String s2 = processService.getProcessDao().getProcessFromGuid("_0uyGoMlgEdmt3adZL5Dmdw").getId();
+		openup = processDao.getProcess(s2);
+
+		Project project2 = new Project();
+		project2.setConcreteName("IceOpenUP");
+		project2.setProcess(openup);
+		projectDao.saveOrUpdateProject(project2);
+
+		processService.projectInstanciation(project2);
+		
+		project2 = processService.getProjectDao().getProject(project2.getId());
+		project= processService.getProjectDao().getProject(project.getId());
+		
+		// creation du participant 
+		// ParticipantService ps = (ParticipantService) factory.getBean("ParticipantService");
+		Participant pa = new Participant();
+		
+		pa.setLogin("test");
+	    pa.setPassword(Security.encode("testtest"));
+	    pa.setName("test");
+	    pa.setEmailAddress("test@test.com");
+	    pa.setFirstname("test");
+	    participantService.getParticipantDao().saveOrUpdateParticipant(pa);
+	    
+	    // affectation du particpant aux concreteRoles
+	    
+	   // ConcreteRoleDescriptorDao cs = (ConcreteRoleDescriptorDao) factory.getBean("ConcreteRoleDescriptorDao");
+	 	for (ConcreteRoleDescriptor crd : concreteRoleDescriptorDao.getAllConcreteRoleDescriptors()) {
+	    	pa.addConcreteRoleDescriptor(crd);
+	    }	   
+	   
+	 	participantService.getParticipantDao().saveOrUpdateParticipant(pa);
+		
+			
+		// affectation des concreteRoles aux concreteTask
+	   
+	   /* pa =  ps.getParticipantDao().getParticipant(pa.getLogin());
+	    if (pa != null) {
+		    ConcreteTaskDescriptorService ts = (ConcreteTaskDescriptorService) ctx.getBean("ConcreteTaskDescriptorService");
+			
+			List<ConcreteTaskDescriptor> lctds =  ts.getConcreteTaskDescriptorDao().getAllConcreteTaskDescriptors();
+				 
+		    for (ConcreteTaskDescriptor ctd : lctds) {	    
+		    	ts.affectedConcreteTaskDescriptor(ctd, pa);
+		    }
+	    }*/
+	    
+	}
+
+	public ConcreteRoleDescriptorDao getConcreteRoleDescriptorDao() {
+		return concreteRoleDescriptorDao;
+	}
+
+	public void setConcreteRoleDescriptorDao(
+			ConcreteRoleDescriptorDao concreteRoleDescriptorDao) {
+		this.concreteRoleDescriptorDao = concreteRoleDescriptorDao;
+	}
+
+	public ConcreteTaskDescriptorService getConcreteTaskDescriptorService() {
+		return concreteTaskDescriptorService;
+	}
+
+	public void setConcreteTaskDescriptorService(
+			ConcreteTaskDescriptorService concreteTaskDescriptorService) {
+		this.concreteTaskDescriptorService = concreteTaskDescriptorService;
+	}
+
+	public ParticipantService getParticipantService() {
+		return participantService;
+	}
+
+	public void setParticipantService(ParticipantService participantService) {
+		this.participantService = participantService;
+	}
+
+	public ProcessDao getProcessDao() {
+		return processDao;
+	}
+
+	public void setProcessDao(ProcessDao processDao) {
+		this.processDao = processDao;
+	}
+
+	public ProcessService getProcessService() {
+		return processService;
+	}
+
+	public void setProcessService(ProcessService processService) {
+		this.processService = processService;
+	}
+
+	public ProjectDao getProjectDao() {
+		return projectDao;
+	}
+
+	public void setProjectDao(ProjectDao projectDao) {
+		this.projectDao = projectDao;
+	}
+	
+}
--- spelp/trunk/remote/applitest/wilos/application/console/InitAppliTest.java
+++ spelp/trunk/remote/applitest/wilos/application/console/InitAppliTest.java
@@ -18,78 +18,12 @@ import wilos.model.spem2.process.Process;
 
 public class InitAppliTest {
 	public static void main(String[] args) {
-//		 Notre fabrique SPRING permettant l'accès aux beans déclarés
-		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		 Notre fabrique SPRING permettant l'accï¿½s aux beans dï¿½clarï¿½s
+		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContextTest.xml");
 		// import des processus
-		ProcessService am = (ProcessService) factory.getBean("ProcessService");
-
-		Process openup = am.spelpParsingXML(new File("applitest/wilos/application/console/openUP.xml"));
-		am.saveProcess(openup);
-		Process scrum = am.spelpParsingXML(new File("applitest/wilos/application/console/scrum.xml"));
-		am.saveProcess(scrum);
+		InitAppliService am = (InitAppliService) factory.getBean("InitAppliService");
+		am.initAppli();
 		
-		
-		// instanciation des projets
-		ProjectDao pm = (ProjectDao) factory.getBean("ProjectDao");
-		ProcessDao p = (ProcessDao) factory.getBean("ProcessDao");
-
-		String s = am.getProcessDao().getProcessFromGuid("_9llsAQAvEdubGMceRDupFQ").getId();
-		scrum = p.getProcess(s);
-		
-		Project project = new Project();
-		project.setConcreteName("Wilos");
-		project.setProcess(scrum);
-		pm.saveOrUpdateProject(project);
-
-		am.projectInstanciation(project);
-
-		String s2 = am.getProcessDao().getProcessFromGuid("_0uyGoMlgEdmt3adZL5Dmdw").getId();
-		openup = p.getProcess(s2);
-
-		Project project2 = new Project();
-		project2.setConcreteName("IceOpenUP");
-		project2.setProcess(openup);
-		pm.saveOrUpdateProject(project2);
-
-		am.projectInstanciation(project2);
-		
-		project2 = am.getProjectDao().getProject(project2.getId());
-		project= am.getProjectDao().getProject(project.getId());
-		
-		// creation du participant 
-		ParticipantService ps = (ParticipantService) factory.getBean("ParticipantService");
-		Participant pa = new Participant();
-		
-		pa.setLogin("test");
-	    pa.setPassword(Security.encode("testtest"));
-	    pa.setName("test");
-	    pa.setEmailAddress("test@test.com");
-	    pa.setFirstname("test");
-	    ps.getParticipantDao().saveOrUpdateParticipant(pa);
-	    
-	    // affectation du particpant aux concreteRoles
-	    
-	    ConcreteRoleDescriptorDao cs = (ConcreteRoleDescriptorDao) factory.getBean("ConcreteRoleDescriptorDao");
-	 	for (ConcreteRoleDescriptor crd : cs.getAllConcreteRoleDescriptors()) {
-	    	pa.addConcreteRoleDescriptor(crd);
-	    }	   
-	   
-	    ps.getParticipantDao().saveOrUpdateParticipant(pa);
-		
-			
-		// affectation des concreteRoles aux concreteTask
-	   
-	   /* pa =  ps.getParticipantDao().getParticipant(pa.getLogin());
-	    if (pa != null) {
-		    ConcreteTaskDescriptorService ts = (ConcreteTaskDescriptorService) ctx.getBean("ConcreteTaskDescriptorService");
-			
-			List<ConcreteTaskDescriptor> lctds =  ts.getConcreteTaskDescriptorDao().getAllConcreteTaskDescriptors();
-				 
-		    for (ConcreteTaskDescriptor ctd : lctds) {	    
-		    	ts.affectedConcreteTaskDescriptor(ctd, pa);
-		    }
-	    }*/
-	    
 	    
 	}
 }
