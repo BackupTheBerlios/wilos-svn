aa2a3a649c04a69ddca4d10662a170e2a0a3a7f6
garwind111 <garwind111@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-13 13:53:40 +0000

    [Woops2] tag 2154

---
+++ woops2/tags/Woops2_r2154/src/wilos/business/services/misc/concretetask/ConcreteTaskDescriptorService.java
@@ -0,0 +1,228 @@
+package wilos.business.services.misc.concretetask;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import wilos.business.services.misc.concreterole.ConcreteRoleDescriptorService;
+import wilos.business.services.spem2.role.RoleDescriptorService;
+import wilos.business.services.spem2.task.TaskDescriptorService;
+import wilos.hibernate.misc.concretetask.ConcreteTaskDescriptorDao;
+import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
+import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
+import wilos.model.misc.wilosuser.Participant;
+import wilos.model.spem2.role.RoleDescriptor;
+import wilos.model.spem2.task.TaskDescriptor;
+import wilos.utils.Constantes;
+import wilos.utils.Constantes.State;
+
+/**
+ * 
+ * @author Soosuske
+ * @author deder
+ * 
+ */
+@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+public class ConcreteTaskDescriptorService {
+
+	private ConcreteTaskDescriptorDao concreteTaskDescriptorDao;
+
+	private TaskDescriptorService taskDescriptorService;
+
+	private RoleDescriptorService roleDescriptorService;
+	
+	private ConcreteRoleDescriptorService concreteRoleDescriptorService;
+
+	
+	protected final Log logger = LogFactory.getLog(this.getClass());
+
+	/**
+	 * Return concreteTaskDescriptor for a project list
+	 * 
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public List<ConcreteTaskDescriptor> getAllConcreteTaskDescriptorsForProject(
+			String _projectId) {
+		return this.getConcreteTaskDescriptorDao()
+				.getAllConcreteTaskDescriptorsForProject(_projectId);
+	}
+
+	public ConcreteTaskDescriptor getConcreteTaskDescriptor(
+			String _concreteTaskDescriptorId) {
+		return this.getConcreteTaskDescriptorDao().getConcreteTaskDescriptor(
+				_concreteTaskDescriptorId);
+	}
+	
+	/**
+	 * Save the ConcreteTaskDescriptor modifications into database
+	 * @param _concreteTaskDescriptor
+	 */
+	public void updateConcreteTaskDescriptor(ConcreteTaskDescriptor _concreteTaskDescriptor)
+	{
+		this.concreteTaskDescriptorDao.saveOrUpdateConcreteTaskDescriptor(_concreteTaskDescriptor);
+	}
+
+	/**
+	 * Start the ConcreteTaskDescriptor and save into the data base changings
+	 * (i.e. State, realStartingDate).
+	 * 
+	 * @param _concreteTaskDescriptor
+	 *            The ConcreteTaskDescriptor to start.
+	 */
+	public void startConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor) {
+
+		_concreteTaskDescriptor.setState(State.STARTED);
+		try {
+			_concreteTaskDescriptor.setRealStartingDate(Constantes.DATE_FORMAT
+					.parse(Calendar.getInstance().getTime().toString()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		// save changings.
+		this.concreteTaskDescriptorDao
+				.saveOrUpdateConcreteTaskDescriptor(_concreteTaskDescriptor);
+	}
+
+	/**
+	 * When the user click on the button affected.
+	 * 
+	 * @param _concreteTaskDescriptor
+	 */
+	@Transactional(readOnly = false)
+	public ConcreteTaskDescriptor affectedConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor, Participant _user) {
+		ConcreteRoleDescriptor concreteRoleDescriptor = new ConcreteRoleDescriptor();
+
+		TaskDescriptor tmp = _concreteTaskDescriptor.getTaskDescriptor();
+		RoleDescriptor tmpRoleDescriptor = tmp.getMainRole();
+		RoleDescriptor rd = this.roleDescriptorService.getRoleDescriptorById(tmpRoleDescriptor.getId());
+		// recuperation des deux listes.
+		Set<ConcreteRoleDescriptor> listeRd = rd.getConcreteRoleDescriptors();
+		
+		// on parcours les deux liste afin de trouver le bon
+		// concreteRoledescriptor
+		for (ConcreteRoleDescriptor tmpListeRd : listeRd) {
+
+			ConcreteRoleDescriptor crd = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById(tmpListeRd.getId());
+			logger.debug("idddddddd "+crd.getParticipant().getWilosuser_id());
+			if(crd.getParticipant().getWilosuser_id().equals(_user.getWilosuser_id()))
+			{
+				concreteRoleDescriptor = tmpListeRd;
+			}
+			
+		}
+		
+		_concreteTaskDescriptor.addConcreteRoleDescriptor(concreteRoleDescriptor);
+		
+		return _concreteTaskDescriptor;
+	}
+	
+	public void affectedState(ConcreteTaskDescriptor _concreteTaskDescriptor)
+	{
+		_concreteTaskDescriptor.setState(State.READY);
+	
+		this.updateConcreteTaskDescriptor(_concreteTaskDescriptor);
+	}
+	
+	/**
+	 * Suspend the ConcreteTaskDescriptor and save into the data base changings
+	 * (i.e. State).
+	 * 
+	 * @param _concreteTaskDescriptor
+	 *            The ConcreteTaskDescriptor to start.
+	 */
+	public void suspendConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor) {
+		// update changings.
+		_concreteTaskDescriptor.setState(State.SUSPENDED);
+
+		// save changings.
+		this.concreteTaskDescriptorDao
+				.saveOrUpdateConcreteTaskDescriptor(_concreteTaskDescriptor);
+	}
+
+	/**
+	 * Finish the ConcreteTaskDescriptor and save into the data base changings
+	 * (i.e. State, realFinishingDate).
+	 * 
+	 * @param _concreteTaskDescriptor
+	 *            The ConcreteTaskDescriptor to start.
+	 */
+	public void finishConcreteTaskDescriptor(
+			ConcreteTaskDescriptor _concreteTaskDescriptor) {
+		// update changings.
+		_concreteTaskDescriptor.setState(State.FINISHED);
+		try {
+			_concreteTaskDescriptor.setRealFinishingDate(Constantes.DATE_FORMAT
+					.parse(Calendar.getInstance().getTime().toString()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		// save changings.
+		this.concreteTaskDescriptorDao
+				.saveOrUpdateConcreteTaskDescriptor(_concreteTaskDescriptor);
+	}
+
+	/**
+	 * Getter of taskDescriptorDao.
+	 * 
+	 * @return the taskDescriptorDao.
+	 */
+	public ConcreteTaskDescriptorDao getConcreteTaskDescriptorDao() {
+		return this.concreteTaskDescriptorDao;
+	}
+
+	
+	/**
+	 * Setter of taskDescriptorDao.
+	 * 
+	 * @param _taskDescriptorDao
+	 *            The taskDescriptorDao to set.
+	 */
+	public void setConcreteTaskDescriptorDao(
+			ConcreteTaskDescriptorDao _concreteTaskDescriptorDao) {
+		this.concreteTaskDescriptorDao = _concreteTaskDescriptorDao;
+	}
+
+	public RoleDescriptorService getRoleDescriptorService() {
+		return roleDescriptorService;
+	}
+
+	public void setRoleDescriptorService(
+			RoleDescriptorService roleDescriptorService) {
+		this.roleDescriptorService = roleDescriptorService;
+	}
+
+	public TaskDescriptorService getTaskDescriptorService() {
+		return taskDescriptorService;
+	}
+
+	public void setTaskDescriptorService(
+			TaskDescriptorService taskDescriptorService) {
+		this.taskDescriptorService = taskDescriptorService;
+	}
+
+	/**
+	 * @return the concreteRoleDescriptorService
+	 */
+	public ConcreteRoleDescriptorService getConcreteRoleDescriptorService() {
+		return concreteRoleDescriptorService;
+	}
+
+	/**
+	 * @param concreteRoleDescriptorService the concreteRoleDescriptorService to set
+	 */
+	public void setConcreteRoleDescriptorService(
+			ConcreteRoleDescriptorService concreteRoleDescriptorService) {
+		this.concreteRoleDescriptorService = concreteRoleDescriptorService;
+	}
+}
---
+++ woops2/tags/Woops2_r2154/src/wilos/presentation/web/viewer/ConcreteTaskViewerBean.java
@@ -0,0 +1,245 @@
+package wilos.presentation.web.viewer;
+
+import javax.faces.event.ActionEvent;
+
+import wilos.business.services.misc.concretetask.ConcreteTaskDescriptorService;
+import wilos.business.services.misc.wilosuser.ParticipantService;
+import wilos.business.services.presentation.web.WebSessionService;
+import wilos.model.misc.concretetask.ConcreteTaskDescriptor;
+import wilos.model.misc.wilosuser.Participant;
+
+public class ConcreteTaskViewerBean {
+
+	/* Services */
+
+	private WebSessionService webSessionService;
+
+	private ConcreteTaskDescriptorService concreteTaskDescriptorService;
+
+	private ParticipantService participantService;
+
+	/* Simple fields */
+
+	private ConcreteTaskDescriptor concreteTaskDescriptor;
+
+	private String concreteTaskDescriptorId = "";
+
+	private boolean visibleAffected;
+
+	private boolean visibleStart;
+	
+	private boolean visibleModifiable;
+
+	public void buildConcreteTaskDescriptor() {
+		this.concreteTaskDescriptor = new ConcreteTaskDescriptor();
+		if (!(this.concreteTaskDescriptorId.equals(""))
+				|| this.concreteTaskDescriptorId != null) {
+			this.concreteTaskDescriptor = this.concreteTaskDescriptorService
+					.getConcreteTaskDescriptor(this.concreteTaskDescriptorId);
+		}
+	}
+
+	/**
+	 * soosuske methodes for the buton affected
+	 */
+	public void affectedActionListener(ActionEvent event) {
+		String wilosUserId = (String) this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID) ;
+		Participant participant = this.participantService.getParticipant(wilosUserId);
+
+		this.concreteTaskDescriptor = this.concreteTaskDescriptorService.affectedConcreteTaskDescriptor(
+				this.concreteTaskDescriptor, participant);
+		this.concreteTaskDescriptorService.affectedState(this.concreteTaskDescriptor);
+	}
+
+	public boolean isVisibleAffected() {
+		String wilosUserId = (String) this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID) ;
+		Participant participant = this.participantService.getParticipant(wilosUserId);
+
+		boolean vis = this.affectedVisible(
+				this.concreteTaskDescriptor, participant);
+
+		return vis;
+	}
+
+	/**
+	 * action for button start
+	 *
+	 * @param event
+	 */
+	public void startActionListener(ActionEvent event) {
+		this.concreteTaskDescriptorService
+				.startConcreteTaskDescriptor(this.concreteTaskDescriptor);
+	}
+
+	public boolean isVisibleStart() {
+		String wilosUserId = (String) this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID) ;
+		Participant participant = this.participantService.getParticipant(wilosUserId);
+
+		boolean vis = this.startVisible(
+				this.concreteTaskDescriptor, participant);
+		if (vis) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * Visible ob affected buton
+	 */
+	private boolean affectedVisible(
+			ConcreteTaskDescriptor _concreteTaskDescriptor, Participant _user) {
+
+		boolean visi = true;
+
+		/*
+		 * FIXME TaskDescriptor td =
+		 * _concreteTaskDescriptor.getTaskDescriptor(); TaskDescriptor tmp =
+		 * this.taskDescriptorService.getTaskDescriptorDao().getTaskDescriptor(td.getId());
+		 * RoleDescriptor roleDescriptor = tmp.getMainRole(); RoleDescriptor
+		 * tmpRd =
+		 * this.roleDescriptorService.getRoleDescriptorDao().getRoleDescriptor(roleDescriptor.getId());
+		 * Set<Participant> part = tmpRd.getParticipants();
+		 *
+		 * for (Participant parti : part) {
+		 * if(parti.getWilosuser_id().equals(_user.getWilosuser_id())) { visi=
+		 * false; } }
+		 */
+
+		return visi;
+	}
+
+	/**
+	 * Visible ob affected buton
+	 */
+	private boolean startVisible(ConcreteTaskDescriptor _concreteTaskDescriptor,
+			Participant _user) {
+
+		boolean visi = true;
+
+		/*
+		 * FIXME TaskDescriptor td =
+		 * _concreteTaskDescriptor.getTaskDescriptor(); TaskDescriptor tmp =
+		 * this.taskDescriptorService.getTaskDescriptorDao().getTaskDescriptor(td.getId());
+		 * RoleDescriptor roleDescriptor = tmp.getMainRole(); RoleDescriptor
+		 * tmpRd =
+		 * this.roleDescriptorService.getRoleDescriptorDao().getRoleDescriptor(roleDescriptor.getId());
+		 * Set<Participant> part = tmpRd.getParticipants();
+		 * if(!_concreteTaskDescriptor.getState().equals("Started")) { for
+		 * (Participant parti : part) {
+		 * if(parti.getWilosuser_id().equals(_user.getWilosuser_id())) { visi=
+		 * false; } } }
+		 */
+		return visi;
+
+	}
+
+	public ConcreteTaskDescriptor getConcreteTaskDescriptor() {
+		return concreteTaskDescriptor;
+	}
+
+	public void setConcreteTaskDescriptor(
+			ConcreteTaskDescriptor concreteTaskDescriptor) {
+		this.concreteTaskDescriptor = concreteTaskDescriptor;
+	}
+
+	public ConcreteTaskDescriptorService getConcreteTaskDescriptorService() {
+		return concreteTaskDescriptorService;
+	}
+
+	public void setConcreteTaskDescriptorService(
+			ConcreteTaskDescriptorService concreteTaskDescriptorService) {
+		this.concreteTaskDescriptorService = concreteTaskDescriptorService;
+	}
+
+	public String getConcreteTaskDescriptorId() {
+		return concreteTaskDescriptorId;
+	}
+
+	public void setConcreteTaskDescriptorId(String concreteTaskDescriptorId) {
+		this.concreteTaskDescriptorId = concreteTaskDescriptorId;
+	}
+
+	public boolean getVisibleAffected() {
+		return this.visibleAffected;
+	}
+
+	public void setVisibleAffected(boolean visibleAffected) {
+		this.visibleAffected = visibleAffected;
+	}
+
+	public boolean getVisibleStart() {
+		return this.visibleStart;
+	}
+
+	public void setVisibleStart(boolean visibleStart) {
+		this.visibleStart = visibleStart;
+	}
+
+	/**
+	 * @return the webSessionService
+	 */
+	public WebSessionService getWebSessionService() {
+		return webSessionService ;
+	}
+
+	/**
+	 * Setter of webSessionService.
+	 *
+	 * @param webSessionService The webSessionService to set.
+	 */
+	public void setWebSessionService(WebSessionService webSessionService) {
+		this.webSessionService = webSessionService ;
+	}
+
+	/**
+	 * @return the participantService
+	 */
+	public ParticipantService getParticipantService() {
+		return participantService ;
+	}
+
+	/**
+	 * Setter of participantService.
+	 *
+	 * @param participantService The participantService to set.
+	 */
+	public void setParticipantService(ParticipantService participantService) {
+		this.participantService = participantService ;
+	}
+
+	/**
+	 * @return the visibleModifiable
+	 */
+	public boolean getVisibleModifiable() {
+		this.visibleModifiable = false;
+		if (this.visibleAffected && this.visibleStart)
+		{
+			String participantId = (String)this.getWebSessionService().getAttribute(this.webSessionService.WILOS_USER_ID);
+			Participant user = this.participantService.getParticipant(participantId);
+			//TODO PSI2 : verifier si la concretetask est bien affectée au participant via les roles
+			this.visibleModifiable = true;
+		}
+		else
+		{
+			this.visibleModifiable = false;
+		}
+		return this.visibleModifiable;
+	}
+
+	/**
+	 * @param visibleModifiable the visibleModifiable to set
+	 */
+	public void setVisibleModifiable(boolean _visibleModifiable) {
+		this.visibleModifiable = _visibleModifiable;
+	}
+	
+	/**
+	 * save the ConcreteTaskDescriptor
+	 * @param event
+	 */
+	public void updateActionListener(ActionEvent event)
+	{
+		this.concreteTaskDescriptorService.updateConcreteTaskDescriptor(this.concreteTaskDescriptor);
+	}
+}
