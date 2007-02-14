dd38c0537055fdc3c768e2d0f37f2707a242ffd3
soosuske <soosuske@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-14 14:23:12 +0000

    [Woops2]: Correction ConcreteTaskViewer.jspx

--- woops2/trunk/src/wilos/presentation/web/viewer/ConcreteTaskViewerBean.java
+++ woops2/trunk/src/wilos/presentation/web/viewer/ConcreteTaskViewerBean.java
@@ -34,14 +34,16 @@ public class ConcreteTaskViewerBean {
 	private boolean visibleAffected;
 
 	private boolean visibleStart;
-	
+
 	private boolean visibleStop;
-	
+
 	private boolean visibleSuspended;
-	
+
 	private boolean visibleModifiable;
-	
-    protected final Log logger = LogFactory.getLog(this.getClass()) ;
+
+	private boolean visibleReprendre;
+
+	protected final Log logger = LogFactory.getLog(this.getClass());
 
 	public void buildConcreteTaskDescriptor() {
 		this.concreteTaskDescriptor = new ConcreteTaskDescriptor();
@@ -56,12 +58,25 @@ public class ConcreteTaskViewerBean {
 	 * soosuske methodes for the buton affected
 	 */
 	public void affectedActionListener(ActionEvent event) {
-		String wilosUserId = (String) this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID) ;
-		Participant participant = this.participantService.getParticipant(wilosUserId);
+		String wilosUserId = (String) this.webSessionService
+				.getAttribute(WebSessionService.WILOS_USER_ID);
+		Participant participant = this.participantService
+				.getParticipant(wilosUserId);
 
-		this.concreteTaskDescriptor = this.concreteTaskDescriptorService.affectedConcreteTaskDescriptor(
-				this.concreteTaskDescriptor, participant);
-		this.concreteTaskDescriptorService.affectedState(this.concreteTaskDescriptor);
+		this.concreteTaskDescriptor = this.concreteTaskDescriptorService
+				.affectedConcreteTaskDescriptor(this.concreteTaskDescriptor,
+						participant);
+		this.concreteTaskDescriptorService
+				.affectedState(this.concreteTaskDescriptor);
+		ResourceBundle bundle = ResourceBundle.getBundle(
+				"wilos.resources.messages", FacesContext.getCurrentInstance()
+						.getApplication().getDefaultLocale());
+		FacesMessage message = new FacesMessage();
+		message.setSummary(bundle
+				.getString("concretetaskviewer.updateAffected"));
+		message.setSeverity(FacesMessage.SEVERITY_INFO);
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(null, message);
 	}
 
 	/**
@@ -69,12 +84,12 @@ public class ConcreteTaskViewerBean {
 	 */
 	public boolean getVisibleAffected() {
 		return (this.concreteTaskDescriptor.getConcreteRoleDescriptor() == null);
-	
+
 	}
 
 	/**
 	 * action for button start
-	 *
+	 * 
 	 * @param event
 	 */
 	public void startActionListener(ActionEvent event) {
@@ -86,23 +101,20 @@ public class ConcreteTaskViewerBean {
 		this.concreteTaskDescriptorService
 				.finishConcreteTaskDescriptor(this.concreteTaskDescriptor);
 	}
-	
+
 	public void suspendedActionListener(ActionEvent event) {
 		this.concreteTaskDescriptorService
 				.suspendConcreteTaskDescriptor(this.concreteTaskDescriptor);
 	}
-	
-	public boolean getVisibleStart() {
-		if(this.concreteTaskDescriptor.getState().equals("Suspended"))
-		{
-			return true;
-		}
-		else
-		{
-			return (!(this.concreteTaskDescriptor.getConcreteRoleDescriptor() == null) && !this.concreteTaskDescriptor.getState().equals("Started") && !this.concreteTaskDescriptor.getState().equals("Finished"));
-		}
-	}
 
+	public boolean getVisibleStart() {
+
+		return (!(this.concreteTaskDescriptor.getConcreteRoleDescriptor() == null)
+				&& !this.concreteTaskDescriptor.getState().equals("Started")
+				&& !this.concreteTaskDescriptor.getState().equals("Finished") && !this.concreteTaskDescriptor
+				.getState().equals("Suspended"));
+
+	}
 
 	public ConcreteTaskDescriptor getConcreteTaskDescriptor() {
 		return concreteTaskDescriptor;
@@ -130,11 +142,9 @@ public class ConcreteTaskViewerBean {
 		this.concreteTaskDescriptorId = concreteTaskDescriptorId;
 	}
 
-
 	public void setVisibleAffected(boolean visibleAffected) {
 		this.visibleAffected = visibleAffected;
 	}
-
 
 	public void setVisibleStart(boolean visibleStart) {
 		this.visibleStart = visibleStart;
@@ -144,32 +154,34 @@ public class ConcreteTaskViewerBean {
 	 * @return the webSessionService
 	 */
 	public WebSessionService getWebSessionService() {
-		return webSessionService ;
+		return webSessionService;
 	}
 
 	/**
 	 * Setter of webSessionService.
-	 *
-	 * @param webSessionService The webSessionService to set.
+	 * 
+	 * @param webSessionService
+	 *            The webSessionService to set.
 	 */
 	public void setWebSessionService(WebSessionService webSessionService) {
-		this.webSessionService = webSessionService ;
+		this.webSessionService = webSessionService;
 	}
 
 	/**
 	 * @return the participantService
 	 */
 	public ParticipantService getParticipantService() {
-		return participantService ;
+		return participantService;
 	}
 
 	/**
 	 * Setter of participantService.
-	 *
-	 * @param participantService The participantService to set.
+	 * 
+	 * @param participantService
+	 *            The participantService to set.
 	 */
 	public void setParticipantService(ParticipantService participantService) {
-		this.participantService = participantService ;
+		this.participantService = participantService;
 	}
 
 	/**
@@ -177,41 +189,51 @@ public class ConcreteTaskViewerBean {
 	 */
 	public boolean getVisibleModifiable() {
 		this.visibleModifiable = false;
-		if (!this.getVisibleAffected() && !this.getVisibleStart()&& !this.concreteTaskDescriptor.getState().equals("Finished"))
-		{
-			/*String participantId = (String)this.getWebSessionService().getAttribute(this.webSessionService.WILOS_USER_ID);
-			Participant user = this.participantService.getParticipant(participantId);*/
-			//TODO PSI2 : verifier si la concretetask est bien affectée au participant via les roles
+		if (!this.getVisibleAffected() && !this.getVisibleStart()
+				&& !this.concreteTaskDescriptor.getState().equals("Finished")
+				&& !this.concreteTaskDescriptor.getState().equals("Suspended")) {
+			/*
+			 * String participantId =
+			 * (String)this.getWebSessionService().getAttribute(this.webSessionService.WILOS_USER_ID);
+			 * Participant user =
+			 * this.participantService.getParticipant(participantId);
+			 */
+			// TODO PSI2 : verifier si la concretetask est bien affectée au
+			// participant via les roles
 			this.visibleModifiable = true;
-		}
-		else
-		{
+		} else {
 			this.visibleModifiable = false;
 		}
 		return this.visibleModifiable;
 	}
 
 	/**
-	 * @param visibleModifiable the visibleModifiable to set
+	 * @param visibleModifiable
+	 *            the visibleModifiable to set
 	 */
 	public void setVisibleModifiable(boolean _visibleModifiable) {
 		this.visibleModifiable = _visibleModifiable;
 	}
-	
+
 	/**
 	 * save the ConcreteTaskDescriptor
+	 * 
 	 * @param event
 	 */
-	public void updateActionListener(ActionEvent event)
-	{
-		this.concreteTaskDescriptorService.updateConcreteTaskDescriptor(this.concreteTaskDescriptor);
+	public void updateActionListener(ActionEvent event) {
+		this.concreteTaskDescriptorService
+				.updateConcreteTaskDescriptor(this.concreteTaskDescriptor);
 		this.buildConcreteTaskDescriptor();
-		ResourceBundle bundle = ResourceBundle.getBundle("wilos.resources.messages", FacesContext.getCurrentInstance().getApplication().getDefaultLocale()) ;
-		FacesMessage message = new FacesMessage() ;
-        message.setSummary(bundle.getString("concretetaskviewer.updateMessage")) ;
-        message.setSeverity(FacesMessage.SEVERITY_INFO) ;
-        FacesContext facesContext = FacesContext.getCurrentInstance() ;
-        facesContext.addMessage(null, message) ;
+		ResourceBundle bundle = ResourceBundle.getBundle(
+				"wilos.resources.messages", FacesContext.getCurrentInstance()
+						.getApplication().getDefaultLocale());
+		FacesMessage message = new FacesMessage();
+		message
+				.setSummary(bundle
+						.getString("concretetaskviewer.updateMessage"));
+		message.setSeverity(FacesMessage.SEVERITY_INFO);
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(null, message);
 
 	}
 
@@ -223,7 +245,8 @@ public class ConcreteTaskViewerBean {
 	}
 
 	/**
-	 * @param visibleStop the visibleStop to set
+	 * @param visibleStop
+	 *            the visibleStop to set
 	 */
 	public void setVisibleStop(boolean visibleStop) {
 		this.visibleStop = visibleStop;
@@ -232,14 +255,30 @@ public class ConcreteTaskViewerBean {
 	/**
 	 * @return the visibleSuspended
 	 */
-	public boolean isVisibleSuspended() {
+	public boolean getVisibleSuspended() {
 		return this.concreteTaskDescriptor.getState().equals("Started");
 	}
 
 	/**
-	 * @param visibleSuspended the visibleSuspended to set
+	 * @param visibleSuspended
+	 *            the visibleSuspended to set
 	 */
 	public void setVisibleSuspended(boolean visibleSuspended) {
 		this.visibleSuspended = visibleSuspended;
+	}
+
+	/**
+	 * @return the visibleReprendre
+	 */
+	public boolean getVisibleReprendre() {
+		return this.concreteTaskDescriptor.getState().equals("Suspended");
+	}
+
+	/**
+	 * @param visibleReprendre
+	 *            the visibleReprendre to set
+	 */
+	public void setVisibleReprendre(boolean visibleReprendre) {
+		this.visibleReprendre = visibleReprendre;
 	}
 }
