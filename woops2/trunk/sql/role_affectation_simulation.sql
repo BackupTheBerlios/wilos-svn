96942fe8190ce74ee5954ab3573eb8e5a24bd253
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-17 12:09:20 +0000

    [Woops2] Mise en commentaire des WebServices de SPELP et suppression du srcipt sql tmp.

--- woops2/trunk/sql/role_affectation_simulation.sql
+++
@@ -1,5 +0,0 @@
---on positionne tous les roles de toutes les taches concretes pour le participant parti
-UPDATE concreteroledescriptor
-SET `participant_id` = (SELECT wilosuser_id  
-						FROM wilosuser 
-						WHERE login LIKE '%parti%');
