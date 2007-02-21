32134390384047d5e9f4a2230bd920dc75f9d15a
sadasblackmilk <sadasblackmilk@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-21 00:32:34 +0000

    [PSI2] Preparation de la suppression du processus.

--- psi2/trunk/src/wilos/presentation/web/process/ProcessBean.java
+++ psi2/trunk/src/wilos/presentation/web/process/ProcessBean.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -26,7 +28,7 @@ public class ProcessBean {
 
 	private ProcessService processService ;
 
-	private List<HashMap<String, String>> processesList ;
+	private List<HashMap<String, Object>> processesList ;
 
 	private String processesListView ;
 
@@ -43,8 +45,27 @@ public class ProcessBean {
 	}
 
 	public void deleteProcess(ActionEvent e) {
+		ResourceBundle bundle = ResourceBundle.getBundle("wilos.resources.messages", FacesContext.getCurrentInstance().getApplication().getDefaultLocale());
+		FacesMessage message = new FacesMessage() ;
+		
 		String processId = (String)FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("processId");
-		//Process process = this.processService.getPro
+		for(Process process : this.processService.getProcessesList()){
+			if(process.getId().equals(processId)){
+				if(process.getProjects().size() == 0){
+					this.processService.getProcessDao().deleteProcess(process);
+					message.setSummary(bundle.getString("component.process.management.deletiondone")) ;
+					message.setSeverity(FacesMessage.SEVERITY_ERROR) ;
+					FacesContext facesContext = FacesContext.getCurrentInstance() ;
+					facesContext.addMessage(null, message) ;
+				}
+				else{
+					message.setSummary(bundle.getString("component.process.management.deletionforbidden")) ;
+					message.setSeverity(FacesMessage.SEVERITY_ERROR) ;
+					FacesContext facesContext = FacesContext.getCurrentInstance() ;
+					facesContext.addMessage(null, message) ;
+				}
+			}
+		}
 	}
 
 	/**
@@ -52,13 +73,14 @@ public class ProcessBean {
 	 * 
 	 * @return the processesList.
 	 */
-	public List<HashMap<String, String>> getProcessesList() {
+	public List<HashMap<String, Object>> getProcessesList() {
 		 if(this.processesList == null){
-			this.processesList = new ArrayList<HashMap<String, String>>() ;
+			this.processesList = new ArrayList<HashMap<String, Object>>() ;
 			for(Process process : this.processService.getProcessesList()){
-				HashMap<String, String> processDescription = new HashMap<String, String>() ;
+				HashMap<String, Object> processDescription = new HashMap<String, Object>() ;
 				processDescription.put("presentationName", process.getPresentationName()) ;
 				processDescription.put("id", process.getId()) ;
+				processDescription.put("isEditable", new Boolean(false)) ;
 				this.processesList.add(processDescription) ;
 			}
 			return this.processesList ;
@@ -67,20 +89,25 @@ public class ProcessBean {
 	}
 	
 	public void editName(ActionEvent e){
-		this.isEditable = true;
+		String processId = (String)FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("processEditId");
+		for(HashMap<String,Object> processDescription : this.processesList){
+			if(((String)processDescription.get("id")).equals(processId)){
+				processDescription.put("isEditable", new Boolean(true));
+			}
+		}
 	}
 	
 	public void saveName(ActionEvent e){		
 		String processId = (String)FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("processSaveId");
 		Process process = this.processService.getProcessDao().getProcess(processId);
 		for(Iterator iter = this.getProcessesList().iterator(); iter.hasNext();){
-			HashMap<String,String> processDescription = (HashMap<String,String>) iter.next() ;
+			HashMap<String,Object> processDescription = (HashMap<String,Object>) iter.next() ;
 			if(((String)processDescription.get("id")).equals(processId)){
-				process.setPresentationName(processDescription.get("presentationName"));
+				process.setPresentationName((String)processDescription.get("presentationName"));
 				this.processService.getProcessDao().saveOrUpdateProcess(process);
+				processDescription.put("isEditable", new Boolean(false));
 			}
 		}
-		this.isEditable = false;
 	}
 
 	/**
@@ -89,7 +116,7 @@ public class ProcessBean {
 	 * @param _processesList
 	 *            The processesList to set.
 	 */
-	public void setProcessesList(List<HashMap<String, String>> _processesList) {
+	public void setProcessesList(List<HashMap<String, Object>> _processesList) {
 		this.processesList = _processesList ;
 	}
 
