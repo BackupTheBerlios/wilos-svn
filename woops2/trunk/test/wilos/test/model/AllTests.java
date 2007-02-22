3a7c2769b61456c2b59425b82ef8ff8a4b1b28ed
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-22 21:57:37 +0000

    [Woops2] Passage a JUnit4.1 juste dans test.model.spem2. Il faut le faire dans tous les autres packages.

--- woops2/trunk/test/wilos/test/model/AllTests.java
+++ woops2/trunk/test/wilos/test/model/AllTests.java
@@ -6,14 +6,14 @@ import junit.framework.TestSuite ;
 
 /**
  * @author deder
- * 
+ *
  */
 public class AllTests {
 
 	public static Test suite () {
 		TestSuite suite = new TestSuite("Test for wilos.test.model") ;
 		// $JUnit-BEGIN$
-		suite.addTest(wilos.test.model.spem2.AllTests.suite()) ;
+		//suite.addTest(wilos.test.model.spem2.TestSuite.suite()) ;
 		suite.addTest(wilos.test.model.misc.AllTests.suite()) ;
 		// $JUnit-END$
 		return suite ;
