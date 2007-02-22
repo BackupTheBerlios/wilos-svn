7b1ed23e8768526739faf0acb3e7ad68be24757d
l3isi25 <l3isi25@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-22 21:55:04 +0000

    [PSI2]Ajout de l'affection sur les concrete roles mais problèmes (minimes) de mise en page sur le viewer des concrete roles

--- psi2/trunk/src/wilos/presentation/web/viewer/ConcreteRoleViewerBean.java
+++ psi2/trunk/src/wilos/presentation/web/viewer/ConcreteRoleViewerBean.java
@@ -1,14 +1,29 @@
 package wilos.presentation.web.viewer;
 
-import wilos.business.services.misc.concreterole.ConcreteRoleDescriptorService;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import wilos.business.services.misc.concreterole.ConcreteRoleDescriptorService ;
+import wilos.business.services.misc.role.ConcreteRoleAffectationService;
+import wilos.business.services.misc.wilosuser.ParticipantService;
+import wilos.business.services.presentation.web.WebSessionService;
 import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
+import wilos.model.misc.wilosuser.Participant;
 
 public class ConcreteRoleViewerBean {
 	private ConcreteRoleDescriptor concreteRoleDescriptor;
 
+	private WebSessionService webSessionService;
+	
 	private ConcreteRoleDescriptorService concreteRoleDescriptorService;
 
 	private String concreteRoleDescriptorId = "";
+	
+	private ParticipantService participantService ;
+	
+	private String selectAffectedRoleView;
 
 	public void buildConcreteRoleModel() {
 		this.concreteRoleDescriptor = new ConcreteRoleDescriptor();
@@ -16,7 +31,27 @@ public class ConcreteRoleViewerBean {
 			this.concreteRoleDescriptor = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById(this.concreteRoleDescriptorId);
 		}
 	}
+	
+	public void affectParticipantToARole()
+	{
+		
+		ResourceBundle bundle = ResourceBundle.getBundle(
+				"wilos.resources.messages", FacesContext.getCurrentInstance()
+						.getApplication().getDefaultLocale());
 
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		FacesMessage message = new FacesMessage();
+			message.setSummary(bundle
+					.getString("concreteRoleViewer.success"));
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			facesContext.addMessage(null, message);
+		
+		String wilosUserId=(String)this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID);
+		Participant user = this.participantService.getParticipant(wilosUserId);
+		this.concreteRoleDescriptor.setParticipant(user);
+		this.concreteRoleDescriptorService.getConcreteRoleDescriptorDao().saveOrUpdateConcreteRoleDescriptor(concreteRoleDescriptor);
+		
+	}
 	public ConcreteRoleDescriptor getConcreteRoleDescriptor() {
 		return concreteRoleDescriptor;
 	}
@@ -41,6 +76,37 @@ public class ConcreteRoleViewerBean {
 	public void setConcreteRoleDescriptorService(
 			ConcreteRoleDescriptorService concreteRoleDescriptorService) {
 		this.concreteRoleDescriptorService = concreteRoleDescriptorService;
+	}
+
+	public String getSelectAffectedRoleView() {
+		if (this.concreteRoleDescriptor.getParticipant()==null)
+		{
+			this.selectAffectedRoleView="roleNotAffected";
+		}else
+		{
+			this.selectAffectedRoleView="roleAffectedTo";
+		}
+		return selectAffectedRoleView;
+	}
+
+	public void setSelectAffectedRoleView(String selectAffectedRoleView) {
+		this.selectAffectedRoleView = selectAffectedRoleView;
+	}
+
+	public ParticipantService getParticipantService() {
+		return participantService;
+	}
+
+	public void setParticipantService(ParticipantService participantService) {
+		this.participantService = participantService;
+	}
+
+	public WebSessionService getWebSessionService() {
+		return webSessionService;
+	}
+
+	public void setWebSessionService(WebSessionService webSessionService) {
+		this.webSessionService = webSessionService;
 	}
 
 }
