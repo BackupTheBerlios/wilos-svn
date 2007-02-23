caf059f1f132f1597c3851d94d7d1e0c4d59da85
mastasushi <mastasushi@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-23 16:41:35 +0000

    [SPELP] Mise a jour de la JComboBox lang

--- spelp/trunk/wizard/src/wilos/presentation/assistant/view/main/OptionFrame.java
+++ spelp/trunk/wizard/src/wilos/presentation/assistant/view/main/OptionFrame.java
@@ -1,19 +1,33 @@
 package wilos.presentation.assistant.view.main;
 
+import java.awt.Component;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Locale;
+import java.util.Vector;
 
-import javax.swing.*;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
 
-import wilos.presentation.assistant.control.WizardControler;
+import wilos.presentation.assistant.control.OptionsParser;
 import wilos.presentation.assistant.ressources.Bundle;
 
 public class OptionFrame extends JDialog {
 	private JPanel panneau = null;
+	private JLabel langText = null;
 	private JComboBox lang = null;
-	private JTextField delay = null;
+	private JLabel delayText = null;
+	private JSpinner delay = null;
 	private JButton ok = null;
 	private JButton cancel = null;
 	private JButton apply = null;
@@ -31,13 +45,13 @@ public class OptionFrame extends JDialog {
 	 * @return void
 	 */
 	private void initialize() {
-		 int x = (int)(Toolkit.getDefaultToolkit().getScreenSize().width/2-(605/2));
+		int x = (int)(Toolkit.getDefaultToolkit().getScreenSize().width/2-(605/2));
 		int y = (int)(Toolkit.getDefaultToolkit().getScreenSize().height/2-(220/2)) ;
 		
 		//this.setSize(400, 300);
 		this.setTitle(Bundle.getText("optionFrame.title"));
 		this.setModal(true);
-		this.setBounds(x, y, 420, 220);
+		this.setBounds(x, y, 420, 200);
 		this.setResizable(false);
 		this.setContentPane(getOptionPane());
 		this.setVisible(true);
@@ -51,8 +65,10 @@ public class OptionFrame extends JDialog {
 		if (panneau == null) {
 			panneau = new JPanel();
 			panneau.setLayout(null);
-			panneau.add(getDelay(), null);
+			panneau.add(getLangText(), null);
 			panneau.add(getLang(), null);
+			panneau.add(getDelayText(), null);
+			//panneau.add(getDelay(), null);
 			panneau.add(getOk(), null);
 			panneau.add(getCancel(), null);
 			panneau.add(getApply(), null);
@@ -61,16 +77,17 @@ public class OptionFrame extends JDialog {
 	}
 	
 	/**
-	 * this method creates the cancel button with its dispose function
+	 * this method creates the JLabel for the language
 	 * 
 	 * @return
 	 */
-	private JComboBox getLang()
+	private JLabel getLangText()
 	{
-		if (lang ==null)
+		if (langText ==null)
 		{
-			lang = new JComboBox();
-			lang.setBounds(new Rectangle(50,20,80,20));
+			langText = new JLabel();
+			langText.setBounds(new Rectangle(50,20,80,20));
+			langText.setText(Bundle.getText("optionFrame.lang"));
 			/*
 			lang.addActionListener(new ActionListener()
 			{
@@ -80,19 +97,67 @@ public class OptionFrame extends JDialog {
     			}				
 			});*/
 		}
-		return lang;
+		return langText;
 	}
+	
 	/**
-	 * this method creates the cancel button with its dispose function
+	 * this method creates the JComboBox for the language
 	 * 
 	 * @return
 	 */
-	private JTextField getDelay()
+	private JComboBox getLang()
 	{
-		if (delay ==null)
+		if (lang == null)
 		{
-			delay = new JTextField();
-			delay.setBounds(new Rectangle(150,100,160,20));
+			//Locale[] locs = Locale.getAvailableLocales();
+			
+			Vector<Locale> locs = new Vector<Locale>();
+			
+			File locsDir = new File("src/wilos/presentation/assistant/ressources");
+			File [] ficLocs = locsDir.listFiles(new FileFilter(){
+
+				public boolean accept (File file){
+			      String nomFichier = file.getName().toLowerCase(); 
+
+			      return nomFichier.endsWith(".properties");
+				}
+			});
+			for (File f : ficLocs)
+			{
+				locs.add (new Locale (f.getName().substring(6,8)));
+			}
+			
+			lang = new JComboBox(locs);
+			lang.setRenderer(new LocaleRenderer());
+			lang.setBounds(new Rectangle(250,20,80,20));
+			OptionsParser op = new OptionsParser ();
+			lang.setSelectedItem(new Locale(op.getLocale().getLanguage()));
+		}
+		return lang;
+	}
+	
+	
+	private class LocaleRenderer extends DefaultListCellRenderer{
+		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+			Locale l = (Locale) value;
+			String localeValue = l.getDisplayLanguage();
+			super.getListCellRendererComponent(list, localeValue, index, isSelected, cellHasFocus);
+			return this;
+		}
+	}
+	
+	/**
+	 * this method creates the label of the JTextField delay
+	 * 
+	 * @return
+	 */
+	private JLabel getDelayText()
+	{
+		if (delayText ==null)
+		{
+			delayText = new JLabel();
+			delayText.setBounds(new Rectangle(50,100,160,20));
+			delayText.setText(Bundle.getText("optionFrame.delay"));
 //			delay.paramString(Bundle.getText("optionFrame.delay"));
 			/*
 			delay.addActionListener(new ActionListener()
@@ -103,11 +168,52 @@ public class OptionFrame extends JDialog {
     			}				
 			});*/
 		}
-		return delay;
+		return delayText;
 	}
 	
 	/**
-	 * this method creates the cancel button with its dispose function
+	 * this method creates the JTextField delay
+	 * 
+	 * @return
+	 */
+	private JSpinner getDelay()
+	{
+		if (delay != null)
+		{		
+			delay = new JSpinner();
+/*
+			//Utilisation d un champ de saisie qui verifie et formate
+			delay.setInputVerifier(new Verifier());
+			delay.setHorizontalAlignment(SwingConstants.RIGHT);*/		
+			
+			delay.setBounds(new Rectangle(250,100,100,20));
+			
+		}	
+		return delay;
+	}
+	/*
+	class Verifier extends InputVerifier{
+		public boolean verify (JComponent input){
+			if (input instanceof JFormattedTextField)
+				JFormattedTextField ftf = (JFormattedTextField) input;
+				JFormattedTextField.AbstractFormatter = ftf.getFormatter();
+				if (formater != null){
+						String text = ftf.getText();
+					try{
+						formatter.stringToValue(text);
+						return true;
+					}
+					catch (ParseException pe){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}*/
+	
+	/**
+	 * this method creates the ok button
 	 * 
 	 * @return
 	 */
@@ -116,15 +222,37 @@ public class OptionFrame extends JDialog {
 		if (ok ==null)
 		{
 			ok = new JButton();
-			ok.setBounds(new Rectangle(50,140,80,20));
+			ok.setBounds(new Rectangle(50,140,100,20));
 			ok.setText(Bundle.getText("optionFrame.ok"));
-			ok.addActionListener(new ActionListener()
+			/*ok.addActionListener(new ActionListener()
 			{
-				public void actionPerformed(ActionEvent e) 
+				/*public void actionPerformed(ActionEvent e) 
     			{
-					dispose();
-    			}				
-			});
+					//WizardOptions wo = new WizardOptions(lang.get);
+					JOptionPane opt1 = new JOptionPane();
+					
+					boolean valide = true;
+					
+					for (int i=0;i<servs.getRowCount()&&valide;i++)
+					{
+						if ((String)servs.getValueAt(i,1)==null)
+						{
+							valide = false;
+							opt1.showMessageDialog(opt1,Bundle.getText("serversFrame.error"),"Error",JOptionPane.ERROR_MESSAGE);
+							opt1.setVisible(true);
+						}
+						else
+						{
+							new_list_serv.add(new WizardServer((String)servs.getValueAt(i,0),(String)servs.getValueAt(i,1),i));
+						}
+					}
+					if (valide)
+					{
+						LoginPanel.list_serv.saveServersList(new_list_serv);
+						serverDialog.dispose();
+					}
+    			}			
+			});*/
 		}
 		return ok;
 	}
@@ -139,7 +267,7 @@ public class OptionFrame extends JDialog {
 		if (cancel ==null)
 		{
 			cancel = new JButton();
-			cancel.setBounds(new Rectangle(160,140,80,20));
+			cancel.setBounds(new Rectangle(160,140,100,20));
 			cancel.setText(Bundle.getText("optionFrame.cancel"));
 			cancel.addActionListener(new ActionListener()
 			{
@@ -153,7 +281,7 @@ public class OptionFrame extends JDialog {
 	}
 
 	/**
-	 * this method creates the cancel button with its dispose function
+	 * this method creates the apply button
 	 * 
 	 * @return
 	 */
@@ -162,7 +290,7 @@ public class OptionFrame extends JDialog {
 		if (apply ==null)
 		{
 			apply = new JButton();
-			apply.setBounds(new Rectangle(270,140,80,20));
+			apply.setBounds(new Rectangle(270,140,100,20));
 			apply.setText(Bundle.getText("optionFrame.apply"));
 			apply.addActionListener(new ActionListener()
 			{
@@ -173,7 +301,5 @@ public class OptionFrame extends JDialog {
 			});
 		}
 		return apply;
-	}
-	
-	
+	}	
 }
