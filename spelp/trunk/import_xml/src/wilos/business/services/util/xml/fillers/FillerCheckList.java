14d8abe85dcb57315d3d5c179300efa39340df06
toine <toine@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-24 17:58:45 +0000

    [SPELP] ajout de la classe FillerCheckList

---
+++ spelp/trunk/import_xml/src/wilos/business/services/util/xml/fillers/FillerCheckList.java
@@ -0,0 +1,73 @@
+package wilos.business.services.util.xml.fillers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import wilos.business.services.util.xml.parser.XMLParser;
+import wilos.model.spem2.checklist.CheckList;
+import wilos.model.spem2.guide.Guidance;
+import wilos.model.spem2.section.Section;
+
+public class FillerCheckList extends FillerGuidance {
+	
+	private static String Name = "name" ;
+	private static String Id = "id";
+	
+	
+	public FillerCheckList(CheckList _checklist, Node aNode) {
+		super(_checklist, aNode);
+		fill();
+	}
+
+	private void fill() {
+		Set<Section> sections = new HashSet<Section>();		
+		NodeList l = myNode.getChildNodes();
+		
+		//((Guidance) myElement).setType(type);
+		String aName = "";
+		String aDescriptionSection = "";
+		String mainDescription = "";
+		String aId = "";
+		
+		// for each node by the list
+		for (int i=0; i<l.getLength(); i++) {
+			if (l.item(i).getNodeName().equals(XMLParser.presentation)) {
+				NodeList listItems = l.item(i).getChildNodes();
+				for (int j = 0; j < listItems.getLength();j++) {
+					// case where there is a MainDescription
+					if (listItems.item(j).getNodeName().equals(XMLParser.maindescription)) {
+						mainDescription = listItems.item(j).getTextContent();
+					}
+					// case where there are different sections
+					if (listItems.item(j).getNodeName().equals(XMLParser.section)) {						
+						aName = listItems.item(j).getAttributes().getNamedItem(Name).getNodeValue();
+						aId = listItems.item(j).getAttributes().getNamedItem(Id).getNodeValue();						
+						// for the description
+						NodeList listDescription = listItems.item(j).getChildNodes();
+						// for each node: get the description by the XML file
+						for (int k = 0; k < listDescription.getLength();k++) {
+							if (listDescription.item(k).getNodeName().equals(XMLParser.description)) {
+								aDescriptionSection = listDescription.item(k).getTextContent();
+							}
+						}
+						// create a new instance of Section to fill it
+						Section aSection = new Section();
+						// set the different attributs
+						aSection.setName(aName);
+						aSection.setGuid(aId);
+						aSection.setDescription(aDescriptionSection);
+						
+						// add the fill element in the list of sections
+						sections.add(aSection);						
+					}
+				}
+			}			
+		}		
+		// set the list of sections by the element
+		((CheckList) myElement).setSections(sections);
+		((CheckList) myElement).setDescription(mainDescription);
+	}
+}
