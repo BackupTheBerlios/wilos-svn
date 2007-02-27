cfa339eaff36ee8b6c6417a8aadede534a6fb4b4
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-27 13:26:47 +0000

    [Woops2] Refactoring des viewers bean.

--- woops2/trunk/src/wilos/presentation/web/tree/ConcreteRoleDescriptorNode.java
+++ woops2/trunk/src/wilos/presentation/web/tree/ConcreteRoleDescriptorNode.java
@@ -8,7 +8,6 @@ import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
 
 public class ConcreteRoleDescriptorNode extends DefaultMutableTreeNode {
 
-
 	private static final long serialVersionUID = -3831221193711582741L;
 
 	/**
@@ -18,7 +17,8 @@ public class ConcreteRoleDescriptorNode extends DefaultMutableTreeNode {
 	 * @param _parentTree
 	 */
 	public ConcreteRoleDescriptorNode(
-			ConcreteRoleDescriptor _concreteRoleDescriptor, HashMap<String, Object> _treeMap) {
+			ConcreteRoleDescriptor _concreteRoleDescriptor,
+			HashMap<String, Object> _treeMap) {
 		super();
 		WilosObjectNode iceUserObject = new WilosObjectNode(this);
 		this.setUserObject(iceUserObject);
@@ -27,15 +27,17 @@ public class ConcreteRoleDescriptorNode extends DefaultMutableTreeNode {
 		iceUserObject.setLeaf(true);
 
 		// manage icon.
-		iceUserObject.setLeafIcon("images/tree/icon_roledescriptor.gif");
+		if (_concreteRoleDescriptor.getParticipant() == null)
+			iceUserObject.setLeafIcon("images/tree/icon_roledescriptor.gif");
+		else
+			iceUserObject.setLeafIcon("images/tree/icon_roledescriptor_locked.gif");
 
 		// node information
 		iceUserObject.setId(_concreteRoleDescriptor.getId());
 		iceUserObject.setPageId(WilosObjectNode.CONCRETEROLENODE);
-		
+
 		// add the concreteRoleDescriptor object with his id in the treeMap
 		_treeMap.put(iceUserObject.getId(), _concreteRoleDescriptor);
 	}
 
 }
-
