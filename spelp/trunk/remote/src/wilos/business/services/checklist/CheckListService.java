91d34e3ea4d64ea98d3d6104432f83e98f311531
castel <castel@07d201f1-7922-0410-a939-aa91563164ab>
2007-02-27 14:54:19 +0000

    [SPELP] ajout de la methode getSections dans ChecklistService

--- spelp/trunk/remote/src/wilos/business/services/checklist/CheckListService.java
+++ spelp/trunk/remote/src/wilos/business/services/checklist/CheckListService.java
@@ -1,14 +1,17 @@
 package wilos.business.services.checklist;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import wilos.hibernate.spem2.checklist.CheckListDao;
 import wilos.model.spem2.checklist.CheckList;
+import wilos.model.spem2.section.Section;
 
 @ Transactional (readOnly = false, propagation = Propagation.REQUIRED)
 public class CheckListService {
@@ -16,6 +19,14 @@ public class CheckListService {
 
 	protected final Log logger = LogFactory.getLog(this.getClass()) ;
 
+	public Set<Section> getSections(CheckList _checklist) {
+		Set<Section> result = new HashSet<Section>();
+		for (Section s :checkListDao.getCheckList(_checklist.getId()).getSections()) {
+			result.add(s);
+		}		
+		return result;
+	}
+	
 	public CheckList getCheckList(String _id) {
 		return this.checkListDao.getCheckList(_id) ;
 	}
