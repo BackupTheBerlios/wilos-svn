b7fc0ef77afd2a54a6f32cbb9e0cf32bd998aa3f
themorpheus <themorpheus@07d201f1-7922-0410-a939-aa91563164ab>
2007-03-01 23:53:46 +0000

    [Woops2] poulou : Ajout d'un SortedSet ligne 113 pour fabriquer le war ds les temps dsl kakamikaze

--- psi2/trunk/src/wilos/presentation/web/role/RolesInstanciationBean.java
+++ psi2/trunk/src/wilos/presentation/web/role/RolesInstanciationBean.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.SortedSet;
 
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
