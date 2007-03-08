0fcd48c38a2150bc1e1e69eb4a66edccef5d7673
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-03-08 17:58:56 +0000

    [Woops2] Mise en place de la GPL. (src.model)

--- woops2/trunk/src/wilos/model/spem2/activity/Activity.java
+++ woops2/trunk/src/wilos/model/spem2/activity/Activity.java
@@ -1,5 +1,20 @@
+/*
+ * Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
+ * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-package wilos.model.spem2.activity ;
+package wilos.model.spem2.activity;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,37 +30,38 @@ import wilos.model.spem2.guide.Guidance;
 import wilos.model.spem2.workbreakdownelement.WorkBreakdownElement;
 
 /**
- *
- * An Activity is a Work Breakdown Element and Work Definition which supports the nesting and
- * logical grouping of related Breakdown Elements forming breakdown structures. Although Activity is
- * a concrete meta-class, other classes which represent breakdown structures derive from it; such as
- * Phase, Iteration, Delivery Process, or Capability Pattern.
- *
+ * 
+ * An Activity is a Work Breakdown Element and Work Definition which supports
+ * the nesting and logical grouping of related Breakdown Elements forming
+ * breakdown structures. Although Activity is a concrete meta-class, other
+ * classes which represent breakdown structures derive from it; such as Phase,
+ * Iteration, Delivery Process, or Capability Pattern.
+ * 
  * @author deder.
  * @author morpheus
  * @author garwind
  */
 public class Activity extends WorkBreakdownElement implements Cloneable {
 
-	private SortedSet<BreakdownElement> breakdownElements ;
+	private SortedSet<BreakdownElement> breakdownElements;
 
 	private Set<ConcreteActivity> concreteActivities;
 
 	private Set<Guidance> guidances;
-	
+
 	private String alternatives;
-	
+
 	private String howToStaff;
-	
+
 	private String purpose;
 
 	/**
 	 * Constructor.
-	 *
+	 * 
 	 */
 	public Activity() {
-		super() ;
-		this.breakdownElements = new TreeSet<BreakdownElement>() ;
+		super();
+		this.breakdownElements = new TreeSet<BreakdownElement>();
 		this.concreteActivities = new HashSet<ConcreteActivity>();
 		this.guidances = new HashSet<Guidance>();
 		this.howToStaff = "";
@@ -55,52 +71,60 @@ public class Activity extends WorkBreakdownElement implements Cloneable {
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see woops2.model.workbreakdownelement.WorkBreakdownElement#equals(java.lang.Object)
 	 */
 	public boolean equals(Object obj) {
-		if(obj instanceof Activity == false){
-			return false ;
+		if (obj instanceof Activity == false) {
+			return false;
 		}
-		if(this == obj){
-			return true ;
+		if (this == obj) {
+			return true;
 		}
-		Activity activity = (Activity) obj ;
-		return new EqualsBuilder().appendSuper(super.equals(activity)).append(this.breakdownElements, activity.breakdownElements).append(this.concreteActivities,activity.concreteActivities).append(this.guidances, activity.guidances).append(this.alternatives,activity.alternatives).append(this.howToStaff,activity.howToStaff).append(this.purpose,activity.purpose).isEquals() ;
+		Activity activity = (Activity) obj;
+		return new EqualsBuilder().appendSuper(super.equals(activity)).append(
+				this.breakdownElements, activity.breakdownElements).append(
+				this.concreteActivities, activity.concreteActivities).append(
+				this.guidances, activity.guidances).append(this.alternatives,
+				activity.alternatives).append(this.howToStaff,
+				activity.howToStaff).append(this.purpose, activity.purpose)
+				.isEquals();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see woops2.model.workbreakdownelement.WorkBreakdownElement#hashCode()
 	 */
 	public int hashCode() {
-		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).append(this.alternatives).append(this.howToStaff).append(this.purpose).toHashCode() ;
+		return new HashCodeBuilder(17, 37).appendSuper(super.hashCode())
+				.append(this.alternatives).append(this.howToStaff).append(
+						this.purpose).toHashCode();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see java.lang.Object#clone()
 	 */
-	@ Override
+	@Override
 	public Activity clone() throws CloneNotSupportedException {
-		Activity activity = new Activity() ;
-		activity.copy(this) ;
-		return activity ;
+		Activity activity = new Activity();
+		activity.copy(this);
+		return activity;
 	}
 
 	/**
 	 * Copy the object.
-	 *
+	 * 
 	 * @param _activity
 	 *            The Activity to copy.
 	 */
 	protected void copy(final Activity _activity) {
-		super.copy(_activity) ;
-		this.breakdownElements.addAll(_activity.getBreakdownElements()) ;
-		this.concreteActivities.addAll(_activity.getConcreteActivities()) ;
-		this.guidances.addAll(_activity.getGuidances()) ;
+		super.copy(_activity);
+		this.breakdownElements.addAll(_activity.getBreakdownElements());
+		this.concreteActivities.addAll(_activity.getConcreteActivities());
+		this.guidances.addAll(_activity.getGuidances());
 		this.alternatives = _activity.getAlternatives();
 		this.howToStaff = _activity.getHowToStaff();
 		this.purpose = _activity.getPurpose();
@@ -108,55 +132,57 @@ public class Activity extends WorkBreakdownElement implements Cloneable {
 
 	/**
 	 * Add a breakdownElement to the breakdownElement collection of an activity
-	 *
+	 * 
 	 * @param _breakdownElement
 	 *            The BreakdownElement to add.
 	 */
 	public void addBreakdownElement(BreakdownElement _breakdownElement) {
-		this.getBreakdownElements().add(_breakdownElement) ;
-		_breakdownElement.getSuperActivities().add(this) ;
+		this.getBreakdownElements().add(_breakdownElement);
+		_breakdownElement.getSuperActivities().add(this);
 	}
 
 	/**
-	 * Add a breakdownElement collection to the breakdownElement collection of an activity
-	 *
+	 * Add a breakdownElement collection to the breakdownElement collection of
+	 * an activity
+	 * 
 	 * @param _breakdownElements
 	 *            The set of BreakdownElement to add.
 	 */
-	public void addAllBreakdownElements(SortedSet<BreakdownElement> _breakdownElements) {
-		for(BreakdownElement bde : _breakdownElements){
-			bde.addSuperActivity(this) ;
+	public void addAllBreakdownElements(
+			SortedSet<BreakdownElement> _breakdownElements) {
+		for (BreakdownElement bde : _breakdownElements) {
+			bde.addSuperActivity(this);
 		}
 	}
 
 	/**
 	 * Remove from an activity one of these breakdownElements
-	 *
+	 * 
 	 * @param _breakdownElement
 	 *            The BreakdownElement to remove.
 	 */
 	public void removeBreakdownElement(BreakdownElement _breakdownElement) {
-		_breakdownElement.getSuperActivities().remove(this) ;
-		this.getBreakdownElements().remove(_breakdownElement) ;
+		_breakdownElement.getSuperActivities().remove(this);
+		this.getBreakdownElements().remove(_breakdownElement);
 	}
 
-	public void addConcreteActivity(ConcreteActivity _concreteActivity){
+	public void addConcreteActivity(ConcreteActivity _concreteActivity) {
 		this.concreteActivities.add(_concreteActivity);
 		_concreteActivity.setActivity(this);
 	}
 
-	public void addAllConcreteActivity(Set<ConcreteActivity> _concreteActivities){
-		for (ConcreteActivity ca : _concreteActivities){
+	public void addAllConcreteActivity(Set<ConcreteActivity> _concreteActivities) {
+		for (ConcreteActivity ca : _concreteActivities) {
 			this.addConcreteActivity(ca);
 		}
 	}
 
-	public void removeConcreteActivity(ConcreteActivity _concreteActivity){
+	public void removeConcreteActivity(ConcreteActivity _concreteActivity) {
 		this.concreteActivities.remove(_concreteActivity);
 		_concreteActivity.setActivity(null);
 	}
 
-	public void removeAllConcreteActivities(){
+	public void removeAllConcreteActivities() {
 
 		for (ConcreteActivity tmp : this.concreteActivities) {
 			tmp.setActivity(null);
@@ -164,26 +190,24 @@ public class Activity extends WorkBreakdownElement implements Cloneable {
 		this.concreteActivities.clear();
 	}
 
-
 	/**
 	 * Remove from an activity all its breakdownElements
-	 *
+	 * 
 	 */
 	public void removeAllBreakdownElements() {
-		for(BreakdownElement bde : this.getBreakdownElements())
-			bde.getSuperActivities().remove(this) ;
-		this.getBreakdownElements().clear() ;
+		for (BreakdownElement bde : this.getBreakdownElements())
+			bde.getSuperActivities().remove(this);
+		this.getBreakdownElements().clear();
 	}
 
 	/**
 	 * Getter of breakDownElements.
-	 *
+	 * 
 	 * @return the breakDownElements.
 	 */
 	public SortedSet<BreakdownElement> getBreakdownElements() {
-		return this.breakdownElements ;
+		return this.breakdownElements;
 	}
-
 
 	/*
 	 * connection to guidances
@@ -193,12 +217,10 @@ public class Activity extends WorkBreakdownElement implements Cloneable {
 		this.guidances.remove(_guidance);
 	}
 
-
 	public void addGuidance(Guidance _guidance) {
 		this.guidances.add(_guidance);
 		_guidance.getActivities().add(this);
 	}
-
 
 	public void removeAllGuidances() {
 		for (Guidance guidance : this.guidances) {
@@ -216,12 +238,13 @@ public class Activity extends WorkBreakdownElement implements Cloneable {
 
 	/**
 	 * Setter of breakDownElements.
-	 *
+	 * 
 	 * @param _breakDownElements
 	 *            The breakDownElements to set.
 	 */
-	public void setBreakdownElements(SortedSet<BreakdownElement> _breakDownElements) {
-		//this.breakdownElements.addAll(_breakDownElements);
+	public void setBreakdownElements(
+			SortedSet<BreakdownElement> _breakDownElements) {
+		// this.breakdownElements.addAll(_breakDownElements);
 		this.breakdownElements = _breakDownElements;
 	}
 
