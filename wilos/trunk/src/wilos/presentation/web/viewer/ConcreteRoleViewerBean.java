5ee8d8e3465d16659e1bd78a4a787416320b9f62
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-03-10 15:55:42 +0000

    [Wilos] Merge des junits des 3 groupes.

--- wilos/trunk/src/wilos/presentation/web/viewer/ConcreteRoleViewerBean.java
+++ wilos/trunk/src/wilos/presentation/web/viewer/ConcreteRoleViewerBean.java
@@ -1,18 +1,19 @@
 /*
-Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
-Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ * Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
+ * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU
-General Public License as published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program; if not,
-write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
-*/
 package wilos.presentation.web.viewer;
 
 import java.util.ResourceBundle;
@@ -20,66 +21,63 @@ import java.util.ResourceBundle;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-import wilos.business.services.misc.concreterole.ConcreteRoleDescriptorService ;
+import wilos.business.services.misc.concreterole.ConcreteRoleDescriptorService;
 import wilos.business.services.misc.wilosuser.ParticipantService;
 import wilos.business.services.presentation.web.WebSessionService;
 import wilos.model.misc.concreterole.ConcreteRoleDescriptor;
 import wilos.model.misc.wilosuser.Participant;
-import wilos.presentation.web.tree.TreeBean;
 
-public class ConcreteRoleViewerBean {
+public class ConcreteRoleViewerBean extends ViewerBean {
+
 	private ConcreteRoleDescriptor concreteRoleDescriptor;
 
-	private WebSessionService webSessionService;
-	
 	private ConcreteRoleDescriptorService concreteRoleDescriptorService;
 
 	private String concreteRoleDescriptorId = "";
-	
-	private ParticipantService participantService ;
-	
+
+	private ParticipantService participantService;
+
 	private String selectAffectedRoleView;
 
 	/**
 	 * TODO method description
-	 *
+	 * 
 	 */
 	public void buildConcreteRoleModel() {
 		this.concreteRoleDescriptor = new ConcreteRoleDescriptor();
-		if (!(concreteRoleDescriptorId.equals("")) || concreteRoleDescriptorId != null) {
-			this.concreteRoleDescriptor = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById(this.concreteRoleDescriptorId);
+		if (!(concreteRoleDescriptorId.equals(""))
+				|| concreteRoleDescriptorId != null) {
+			this.concreteRoleDescriptor = this.concreteRoleDescriptorService
+					.getConcreteRoleDescriptorById(this.concreteRoleDescriptorId);
 		}
 	}
-	
+
 	/**
 	 * TODO method description
-	 *
+	 * 
 	 */
-	public void affectParticipantToARole()
-	{
+	public void affectParticipantToARole() {
 		ResourceBundle bundle = ResourceBundle.getBundle(
 				"wilos.resources.messages", FacesContext.getCurrentInstance()
 						.getApplication().getDefaultLocale());
 
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		FacesMessage message = new FacesMessage();
-			message.setSummary(bundle
-					.getString("concreteRoleViewer.success"));
-			message.setSeverity(FacesMessage.SEVERITY_ERROR);
-			facesContext.addMessage(null, message);
-		
-		String wilosUserId=(String)this.webSessionService.getAttribute(WebSessionService.WILOS_USER_ID);
+		message.setSummary(bundle.getString("concreteRoleViewer.success"));
+		message.setSeverity(FacesMessage.SEVERITY_ERROR);
+		facesContext.addMessage(null, message);
+
+		String wilosUserId = (String) super.getWebSessionService()
+				.getAttribute(WebSessionService.WILOS_USER_ID);
 		Participant user = this.participantService.getParticipant(wilosUserId);
 		this.concreteRoleDescriptor.setParticipant(user);
-		this.concreteRoleDescriptorService.getConcreteRoleDescriptorDao().saveOrUpdateConcreteRoleDescriptor(concreteRoleDescriptor);
-		
-		//refresh the tree
-		FacesContext context = FacesContext.getCurrentInstance();
-		TreeBean tb = (TreeBean) context.getApplication().getVariableResolver()
-				.resolveVariable(context, "TreeBean");
-		tb.refreshProjectTree();
+		this.concreteRoleDescriptorService.getConcreteRoleDescriptorDao()
+				.saveOrUpdateConcreteRoleDescriptor(concreteRoleDescriptor);
+
+		// refresh the tree
+		super.refreshProjectTree();
 	}
-	
+
 	public ConcreteRoleDescriptor getConcreteRoleDescriptor() {
 		return concreteRoleDescriptor;
 	}
@@ -107,12 +105,10 @@ public class ConcreteRoleViewerBean {
 	}
 
 	public String getSelectAffectedRoleView() {
-		if (this.concreteRoleDescriptor.getParticipant()==null)
-		{
-			this.selectAffectedRoleView="roleNotAffected";
-		}else
-		{
-			this.selectAffectedRoleView="roleAffectedTo";
+		if (this.concreteRoleDescriptor.getParticipant() == null) {
+			this.selectAffectedRoleView = "roleNotAffected";
+		} else {
+			this.selectAffectedRoleView = "roleAffectedTo";
 		}
 		return selectAffectedRoleView;
 	}
@@ -128,13 +124,4 @@ public class ConcreteRoleViewerBean {
 	public void setParticipantService(ParticipantService participantService) {
 		this.participantService = participantService;
 	}
-
-	public WebSessionService getWebSessionService() {
-		return webSessionService;
-	}
-
-	public void setWebSessionService(WebSessionService webSessionService) {
-		this.webSessionService = webSessionService;
-	}
-
 }
