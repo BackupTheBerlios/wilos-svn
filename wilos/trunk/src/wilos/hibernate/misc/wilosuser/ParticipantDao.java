16394658f4ad1485d76b32bdf8f1c3ac7ead89a0
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-03-10 15:36:29 +0000

    [Wilos] Merge des junits des 3 groupes.

--- wilos/trunk/src/wilos/hibernate/misc/wilosuser/ParticipantDao.java
+++ wilos/trunk/src/wilos/hibernate/misc/wilosuser/ParticipantDao.java
@@ -1,18 +1,19 @@
 /*
-Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
-Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ * Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
+ * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU
-General Public License as published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program; if not,
-write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
-*/
 package wilos.hibernate.misc.wilosuser;
 
 import java.util.ArrayList;
@@ -53,6 +54,7 @@ public class ParticipantDao extends HibernateDaoSupport {
 	 * Return a list of elements.
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public Set<ConcreteRoleDescriptor> getAllConcreteRoleDescriptors() {
 		Set<ConcreteRoleDescriptor> loadAll = new HashSet<ConcreteRoleDescriptor>();
 		loadAll.addAll(this.getHibernateTemplate().loadAll(ConcreteRoleDescriptor.class));
@@ -88,6 +90,7 @@ public class ParticipantDao extends HibernateDaoSupport {
 	 * 
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public Set<Participant> getAllParticipants() {
 		Set<Participant> loadAll = new HashSet<Participant>();
 		loadAll.addAll(this.getHibernateTemplate().loadAll(Participant.class));
