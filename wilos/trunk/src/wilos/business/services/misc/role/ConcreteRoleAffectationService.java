6d90e6aefd1c0e29d98fc987a0ab776dd9e62a3c
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-03-14 23:39:21 +0000

    [Woops2] clean des property des bean spring (qd XDao et XService,  on ne laisse juste XService).

--- wilos/trunk/src/wilos/business/services/misc/role/ConcreteRoleAffectationService.java
+++ wilos/trunk/src/wilos/business/services/misc/role/ConcreteRoleAffectationService.java
@@ -1,18 +1,18 @@
 /*
-Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
-Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU
-General Public License as published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program; if not,
-write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
-*/
+ * Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
+ * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package wilos.business.services.misc.role;
 
 import java.util.ArrayList;
@@ -43,31 +43,36 @@ import wilos.model.misc.wilosuser.Participant;
 public class ConcreteRoleAffectationService {
 
 	private ParticipantDao participantDao;
+
 	private ConcreteRoleDescriptorService concreteRoleDescriptorService;
 
 	protected final Log logger = LogFactory.getLog(this.getClass());
-	
+
 	@Transactional(readOnly = true)
-	public List<ConcreteRoleDescriptor> getAllConcreteRolesDescriptorsForActivity(String _activityId, String _projectId){
+	public List<ConcreteRoleDescriptor> getAllConcreteRolesDescriptorsForActivity(
+			String _activityId, String _projectId) {
 		List<ConcreteRoleDescriptor> concreteRDList = new ArrayList<ConcreteRoleDescriptor>();
-		List<ConcreteRoleDescriptor> globalCRD = this.concreteRoleDescriptorService.getAllConcreteRoleDescriptorsForProject(_projectId);
-		for(ConcreteRoleDescriptor concreteRD : globalCRD){
-			concreteRD = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById(concreteRD.getId());
+		List<ConcreteRoleDescriptor> globalCRD = this.concreteRoleDescriptorService
+				.getAllConcreteRoleDescriptorsForProject(_projectId);
+		for (ConcreteRoleDescriptor concreteRD : globalCRD) {
+			concreteRD = this.concreteRoleDescriptorService
+					.getConcreteRoleDescriptorById(concreteRD.getId());
 			List<ConcreteActivity> globalCA = new ArrayList<ConcreteActivity>();
 			globalCA.addAll(concreteRD.getSuperConcreteActivities());
-			for(Iterator iter = globalCA.iterator(); iter.hasNext();){
-				ConcreteBreakdownElement ca = (ConcreteBreakdownElement) iter.next() ;
-				if(ca.getId().equals(_activityId)){
+			for (Iterator iter = globalCA.iterator(); iter.hasNext();) {
+				ConcreteBreakdownElement ca = (ConcreteBreakdownElement) iter
+						.next();
+				if (ca.getId().equals(_activityId)) {
 					concreteRDList.add(concreteRD);
 				}
 			}
 		}
 		return concreteRDList;
 	}
-		
+
 	/**
 	 * Getter of participantDao.
-	 *
+	 * 
 	 * @return the participantDao.
 	 */
 	public ParticipantDao getParticipantDao() {
@@ -86,66 +91,73 @@ public class ConcreteRoleAffectationService {
 
 	/**
 	 * Save roles affectation for a participant.
-	 *
-	 * @return the page name where navigation has to be redirected to
-	 * TODO: voir un peu ce retour qui craint
+	 * 
+	 * @return the page name where navigation has to be redirected to TODO: voir
+	 *         un peu ce retour qui craint
 	 */
-	public String saveParticipantConcreteRoles(HashMap<String, Object> rolesParticipant,String _wilosUserId) {
-		Participant currentParticipant = this.participantDao.getParticipantById(_wilosUserId);
-		
-		ConcreteRoleDescriptor concreteRoleDescriptor = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById((String)rolesParticipant.get("concreteId"));
-			if(!(Boolean)rolesParticipant.get("not_allowed")){
-				if((Boolean)rolesParticipant.get("affected")){
-					currentParticipant.addConcreteRoleDescriptor(concreteRoleDescriptor);
-				}
-				else{
-					currentParticipant.removeConcreteRoleDescriptor(concreteRoleDescriptor);
-				}
-				this.concreteRoleDescriptorService.getConcreteRoleDescriptorDao().saveOrUpdateConcreteRoleDescriptor(concreteRoleDescriptor);
+	public String saveParticipantConcreteRoles(
+			HashMap<String, Object> rolesParticipant, String _wilosUserId) {
+		Participant currentParticipant = this.participantDao
+				.getParticipantById(_wilosUserId);
+
+		ConcreteRoleDescriptor concreteRoleDescriptor = this.concreteRoleDescriptorService
+				.getConcreteRoleDescriptorById((String) rolesParticipant
+						.get("concreteId"));
+		if (!(Boolean) rolesParticipant.get("not_allowed")) {
+			if ((Boolean) rolesParticipant.get("affected")) {
+				currentParticipant
+						.addConcreteRoleDescriptor(concreteRoleDescriptor);
+			} else {
+				currentParticipant
+						.removeConcreteRoleDescriptor(concreteRoleDescriptor);
 			}
+			this.concreteRoleDescriptorService.getConcreteRoleDescriptorDao()
+					.saveOrUpdateConcreteRoleDescriptor(concreteRoleDescriptor);
+		}
 		return "";
 	}
 
 	/**
 	 * Getter of concreteRoleDescriptorService.
-	 *
+	 * 
 	 * @return the concreteRoleDescriptorService.
 	 */
 	public ConcreteRoleDescriptorService getConcreteRoleDescriptorService() {
-		return this.concreteRoleDescriptorService ;
+		return this.concreteRoleDescriptorService;
 	}
 
 	/**
 	 * Setter of concreteRoleDescriptorService.
-	 *
-	 * @param _concreteRoleDescriptorService The concreteRoleDescriptorService to set.
+	 * 
+	 * @param _concreteRoleDescriptorService
+	 *            The concreteRoleDescriptorService to set.
 	 */
-	public void setConcreteRoleDescriptorService(ConcreteRoleDescriptorService _concreteRoleDescriptorService) {
-		this.concreteRoleDescriptorService = _concreteRoleDescriptorService ;
+	public void setConcreteRoleDescriptorService(
+			ConcreteRoleDescriptorService _concreteRoleDescriptorService) {
+		this.concreteRoleDescriptorService = _concreteRoleDescriptorService;
 	}
 
-	
 	/**
 	 * TODO Method description
-	 *
+	 * 
 	 * @param _wilosUserId
 	 * @param _concreteId
 	 * @return
 	 */
 	@Transactional(readOnly = true)
-	public HashMap<String,Boolean> getParticipantAffectationForConcreteRoleDescriptor(String _wilosUserId, String _concreteId) {
-		HashMap<String,Boolean> roleStatus = new HashMap<String,Boolean>();
-		ConcreteRoleDescriptor crd = this.concreteRoleDescriptorService.getConcreteRoleDescriptorById(_concreteId);
-		if(crd.getParticipant() != null){
+	public HashMap<String, Boolean> getParticipantAffectationForConcreteRoleDescriptor(
+			String _wilosUserId, String _concreteId) {
+		HashMap<String, Boolean> roleStatus = new HashMap<String, Boolean>();
+		ConcreteRoleDescriptor crd = this.concreteRoleDescriptorService
+				.getConcreteRoleDescriptorById(_concreteId);
+		if (crd.getParticipant() != null) {
 			roleStatus.put("affected", new Boolean(true));
-			if(crd.getParticipant().getWilosuser_id().equals(_wilosUserId)){
+			if (crd.getParticipant().getWilosuser_id().equals(_wilosUserId)) {
 				roleStatus.put("not_allowed", new Boolean(false));
-			}
-			else{
+			} else {
 				roleStatus.put("not_allowed", new Boolean(true));
 			}
-		}
-		else{
+		} else {
 			roleStatus.put("affected", new Boolean(false));
 			roleStatus.put("not_allowed", new Boolean(false));
 		}
