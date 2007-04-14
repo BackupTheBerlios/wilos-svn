0ba8682f31f36ebc6753224a8b07c7dbf5c1b737
deder <deder@07d201f1-7922-0410-a939-aa91563164ab>
2007-04-14 19:54:16 +0000

    [Wilos] Controle sur le fait de ne pas pouvoir importer plus d'une fois un processus.

--- trunk/wilos/src/wilos/presentation/web/upload/XmlFileImportBean.java
+++ trunk/wilos/src/wilos/presentation/web/upload/XmlFileImportBean.java
@@ -1,6 +1,6 @@
 /*
  * Wilos Is a cLever process Orchestration Software - http://wilos.berlios.de
- * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <aubry@irit.fr>
+ * Copyright (C) 2006-2007 Paul Sabatier University, IUP ISI (Toulouse, France) <massie@irit.fr>
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation; either version 2 of the License,
@@ -22,6 +22,7 @@ import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.EventObject;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.faces.application.FacesMessage;
@@ -51,7 +52,7 @@ public class XmlFileImportBean {
 	private File file = null;
 
 	private ProcessService processService;
-	
+
 	private WebSessionService webSessionService;
 
 	protected final Log logger = LogFactory.getLog(this.getClass());
@@ -60,7 +61,7 @@ public class XmlFileImportBean {
 
 	private String contentType = "";
 
-	private String uploadStatus = "" ;
+	private String uploadStatus = "";
 
 	public String getUploadStatus() {
 		return uploadStatus;
@@ -91,71 +92,72 @@ public class XmlFileImportBean {
 	}
 
 	public void uploadFileActionListener(ActionEvent event) {
-		
+
 		InputFile inputFile = (InputFile) event.getSource();
-		
-		if(inputFile.getFile() == null && file == null)
-		{
+
+		if (inputFile.getFile() == null && file == null) {
 			// stop the progress bar
 			this.percent = -1;
 			ResourceBundle bundle = ResourceBundle.getBundle(
-					"wilos.resources.messages", FacesContext.getCurrentInstance()
-							.getApplication().getDefaultLocale());
+					"wilos.resources.messages", FacesContext
+							.getCurrentInstance().getApplication()
+							.getDefaultLocale());
 			FacesMessage message = new FacesMessage();
-			message
-					.setSummary(bundle
-							.getString("XmlFileImportBean.noFile"));
+			message.setSummary(bundle.getString("XmlFileImportBean.noFile"));
 			message.setSeverity(FacesMessage.SEVERITY_INFO);
 			FacesContext facesContext = FacesContext.getCurrentInstance();
 			facesContext.addMessage(null, message);
-		}
-		else
-		{
+		} else {
 			if (inputFile.getStatus() == InputFile.SAVED) {
 				fileName = inputFile.getFileInfo().getFileName();
 				contentType = inputFile.getFileInfo().getContentType();
 				setFile(inputFile.getFile());
 			}
-	
-			if (! contentType.equalsIgnoreCase("application/zip")) {
-				if (! contentType.equalsIgnoreCase("text/xml")) {
+
+			if (!contentType.equalsIgnoreCase("application/zip")) {
+				if (!contentType.equalsIgnoreCase("text/xml")) {
 					ResourceBundle bundle = ResourceBundle.getBundle(
-							"wilos.resources.messages", FacesContext.getCurrentInstance()
-									.getApplication().getDefaultLocale());
+							"wilos.resources.messages", FacesContext
+									.getCurrentInstance().getApplication()
+									.getDefaultLocale());
 					FacesMessage message = new FacesMessage();
 					message
 							.setSummary(bundle
 									.getString("XmlFileImportBean.noGoodExtensionFile"));
 					message.setSeverity(FacesMessage.SEVERITY_INFO);
-					FacesContext facesContext = FacesContext.getCurrentInstance();
+					FacesContext facesContext = FacesContext
+							.getCurrentInstance();
 					facesContext.addMessage(null, message);
-					file.delete() ;
+					file.delete();
 					this.percent = -1;
-					return ;
-				}
-				else {
+					return;
+				} else {
 					// xml upload ok
 					ResourceBundle bundle = ResourceBundle.getBundle(
-							"wilos.resources.messages", FacesContext.getCurrentInstance()
-									.getApplication().getDefaultLocale());
-					uploadStatus = bundle.getString("XmlFileImportBean.xmlFileUploadOk") ;
+							"wilos.resources.messages", FacesContext
+									.getCurrentInstance().getApplication()
+									.getDefaultLocale());
+					uploadStatus = bundle
+							.getString("XmlFileImportBean.xmlFileUploadOk");
 				}
-			}
-			else {
+			} else {
 				// zip upload ok
 				ResourceBundle bundle = ResourceBundle.getBundle(
-						"wilos.resources.messages", FacesContext.getCurrentInstance()
-								.getApplication().getDefaultLocale());
-				uploadStatus = bundle.getString("XmlFileImportBean.zipFileUploadOk") ;
+						"wilos.resources.messages", FacesContext
+								.getCurrentInstance().getApplication()
+								.getDefaultLocale());
+				uploadStatus = bundle
+						.getString("XmlFileImportBean.zipFileUploadOk");
 			}
-	
+
 			if (inputFile.getStatus() == InputFile.INVALID) {
 				// stop the progress bar
 				this.percent = -1;
-				//inputFile.getFileInfo().getException().printStackTrace();
+				// inputFile.getFileInfo().getException().printStackTrace();
 				ResourceBundle bundle = ResourceBundle.getBundle(
-						"wilos.resources.messages", FacesContext.getCurrentInstance()
-								.getApplication().getDefaultLocale());
+						"wilos.resources.messages", FacesContext
+								.getCurrentInstance().getApplication()
+								.getDefaultLocale());
 				FacesMessage message = new FacesMessage();
 				message
 						.setSummary(bundle
@@ -163,145 +165,171 @@ public class XmlFileImportBean {
 				message.setSeverity(FacesMessage.SEVERITY_INFO);
 				FacesContext facesContext = FacesContext.getCurrentInstance();
 				facesContext.addMessage(null, message);
-			}
-			else
-			{
-	
-			if (inputFile.getStatus() == InputFile.SIZE_LIMIT_EXCEEDED) {
-				inputFile.getFileInfo().getException().printStackTrace();
-			}
-	
-			if (inputFile.getStatus() == InputFile.UNKNOWN_SIZE) {
-				inputFile.getFileInfo().getException().printStackTrace();
-			}
-			ExternalContext extCtx = FacesContext.getCurrentInstance()
-					.getExternalContext();
-			logger.debug("### fichier uploade = " + file.getPath() + " => "
-					+ file.getName() + " ###");
-			try {
-				logger.debug("### getCanonicalPath = " + file.getCanonicalPath());
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			logger.debug("### getAbsoluteFile = " + file.getAbsoluteFile());
-			logger.debug("### getRequestContextPath = "
-					+ extCtx.getRequestContextPath());
-			logger.debug("### getRequestPathInfo = " + extCtx.getRequestPathInfo());
-			extCtx.getResourceAsStream("");
-	
-			// generating current date for file's dedicated directory
-			Format formatter = new SimpleDateFormat("_yyyy.MM.dd.HH.mm.ss");
-		    String stringDateId = formatter.format(new Date());
-			logger.debug("### XmlFileImportBean ### date generation -> date="	
-													+stringDateId);
-			
-			try {			
-				// creating new directory
-				File targetDirForUploadedFile = 
-					new File(this.file.getAbsolutePath()
-							.substring(0, this.file.getAbsolutePath()
-														.lastIndexOf(File.separator))
-							+File.separator+this.file.getName()+stringDateId) ;
-				targetDirForUploadedFile.mkdirs() ;
-				
-				if (targetDirForUploadedFile.isDirectory()) {
-					logger.debug("### XmlFileImportBean ### target directory -> path="+
-									targetDirForUploadedFile.getAbsolutePath());
-					
-					// moving uploaded file to dedicated directory
-					File targetFileInNewDir = new File(targetDirForUploadedFile
-														.getAbsolutePath()
-												+File.separator
-												+this.file.getName()) ;
-					
-					if (!this.file.renameTo(targetFileInNewDir)) {
-						throw new Exception("failed to move file") ;
+			} else {
+
+				if (inputFile.getStatus() == InputFile.SIZE_LIMIT_EXCEEDED) {
+					inputFile.getFileInfo().getException().printStackTrace();
+				}
+
+				if (inputFile.getStatus() == InputFile.UNKNOWN_SIZE) {
+					inputFile.getFileInfo().getException().printStackTrace();
+				}
+				ExternalContext extCtx = FacesContext.getCurrentInstance()
+						.getExternalContext();
+				logger.debug("### fichier uploade = " + file.getPath() + " => "
+						+ file.getName() + " ###");
+				try {
+					logger.debug("### getCanonicalPath = "
+							+ file.getCanonicalPath());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				logger.debug("### getAbsoluteFile = " + file.getAbsoluteFile());
+				logger.debug("### getRequestContextPath = "
+						+ extCtx.getRequestContextPath());
+				logger.debug("### getRequestPathInfo = "
+						+ extCtx.getRequestPathInfo());
+				extCtx.getResourceAsStream("");
+
+				// generating current date for file's dedicated directory
+				Format formatter = new SimpleDateFormat("_yyyy.MM.dd.HH.mm.ss");
+				String stringDateId = formatter.format(new Date());
+				logger
+						.debug("### XmlFileImportBean ### date generation -> date="
+								+ stringDateId);
+
+				try {
+					// creating new directory
+					File targetDirForUploadedFile = new File(this.file
+							.getAbsolutePath().substring(
+									0,
+									this.file.getAbsolutePath().lastIndexOf(
+											File.separator))
+							+ File.separator
+							+ this.file.getName()
+							+ stringDateId);
+					targetDirForUploadedFile.mkdirs();
+
+					if (targetDirForUploadedFile.isDirectory()) {
+						logger
+								.debug("### XmlFileImportBean ### target directory -> path="
+										+ targetDirForUploadedFile
+												.getAbsolutePath());
+
+						// moving uploaded file to dedicated directory
+						File targetFileInNewDir = new File(
+								targetDirForUploadedFile.getAbsolutePath()
+										+ File.separator + this.file.getName());
+
+						if (!this.file.renameTo(targetFileInNewDir)) {
+							throw new Exception("failed to move file");
+						} else {
+							this.file = targetFileInNewDir;
+							logger.debug("### XmlFileImportBean ### "
+									+ "file moved successfully -> file path="
+									+ this.file.getAbsolutePath());
+						}
+
 					} else {
-						this.file = targetFileInNewDir ;
-						logger.debug("### XmlFileImportBean ### " +
-									"file moved successfully -> file path="+
-										this.file.getAbsolutePath());
+						logger
+								.error("### XmlFileImportBean ### target directory -> "
+										+ "directory has not been created!");
 					}
-					
-				} else {
-					logger.error("### XmlFileImportBean ### target directory -> " +
-													"directory has not been created!");
+				} catch (Exception e) {
+					logger.error("### XmlFileImportBean ### file move -> " + e);
 				}
-			}
-			catch (Exception e) {
-				logger.error("### XmlFileImportBean ### file move -> " + e);
-			}
-					
-			try {
-				Process p = processService.spelpParsingXML(file);
-				//mise en place du controle du process
-				if(p != null)
-				{
-					// save the process
-					logger.debug("### XmlFileImportBean ### action -> id=" + p.getId());
-					/* id = */
-					String managerId = (String) this.webSessionService
-					.getAttribute(WebSessionService.WILOS_USER_ID);
-					this.processService.saveProcess(p, managerId);
-					// stop the progress bar
-					this.percent = -1;
-					ResourceBundle bundle = ResourceBundle.getBundle(
-							"wilos.resources.messages", FacesContext.getCurrentInstance()
-									.getApplication().getDefaultLocale());
-					FacesMessage message = new FacesMessage();
-					message
-							.setSummary(bundle
-									.getString("XmlFileImportBean.processok"));
-					message.setSeverity(FacesMessage.SEVERITY_INFO);
-					FacesContext facesContext = FacesContext.getCurrentInstance();
-					facesContext.addMessage(null, message);
-					
-					
-					/* Forwards to the list of processes */
-					FacesContext context = FacesContext.getCurrentInstance();
-					ActionBean actionbean = (ActionBean) context.getApplication()
-							.getVariableResolver().resolveVariable(context, "ActionBean");
-					MenuBean menuBean = (MenuBean) facesContext.getExternalContext()
-							.getSessionMap().get("menu");
-					menuBean.getSelectedPanel().setTemplateName(actionbean.getProcessManagerMain());
-					menuBean.getSelectedPanel().setTemplateNameForARole(actionbean.getManageProcesses());
+
+				try {
+					Process p = processService.spelpParsingXML(file);
+
+					// check if the process already exists.
+					boolean alreadyExists = false;
+					List<Process> list = processService.getProcessesList();
+					for (Process tmp : list)
+						if (tmp.getGuid().equals(p.getGuid()))
+							alreadyExists = true;
+
+					// mise en place du controle du process
+					if ((p != null) && (!alreadyExists)) {
+						// save the process
+						logger.debug("### XmlFileImportBean ### action -> id="
+								+ p.getId());
+						/* id = */
+						String managerId = (String) this.webSessionService
+								.getAttribute(WebSessionService.WILOS_USER_ID);
+						this.processService.saveProcess(p, managerId);
+						// stop the progress bar
+						this.percent = -1;
+						ResourceBundle bundle = ResourceBundle.getBundle(
+								"wilos.resources.messages", FacesContext
+										.getCurrentInstance().getApplication()
+										.getDefaultLocale());
+						FacesMessage message = new FacesMessage();
+						message.setSummary(bundle
+								.getString("XmlFileImportBean.processok"));
+						message.setSeverity(FacesMessage.SEVERITY_INFO);
+						FacesContext facesContext = FacesContext
+								.getCurrentInstance();
+						facesContext.addMessage(null, message);
+
+						/* Forwards to the list of processes */
+						FacesContext context = FacesContext
+								.getCurrentInstance();
+						ActionBean actionbean = (ActionBean) context
+								.getApplication().getVariableResolver()
+								.resolveVariable(context, "ActionBean");
+						MenuBean menuBean = (MenuBean) facesContext
+								.getExternalContext().getSessionMap().get(
+										"menu");
+						menuBean.getSelectedPanel().setTemplateName(
+								actionbean.getProcessManagerMain());
+						menuBean.getSelectedPanel().setTemplateNameForARole(
+								actionbean.getManageProcesses());
+					} else {
+						this.percent = -1;
+						ResourceBundle bundle = ResourceBundle.getBundle(
+								"wilos.resources.messages", FacesContext
+										.getCurrentInstance().getApplication()
+										.getDefaultLocale());
+						FacesMessage message = new FacesMessage();
+						if (alreadyExists) {
+							message
+									.setSummary(bundle
+											.getString("XmlFileImportBean.processAlreadyExists"));
+							message.setSeverity(FacesMessage.SEVERITY_INFO);
+						} else {
+							message
+									.setSummary(bundle
+											.getString("XmlFileImportBean.processNull"));
+							message.setSeverity(FacesMessage.SEVERITY_INFO);
+						}
+
+						FacesContext facesContext = FacesContext
+								.getCurrentInstance();
+						facesContext.addMessage(null, message);
+					}
+				} catch (Exception e) {
+					logger.error("### XmlFileImportBean ### action -> " + e);
 				}
-				else
-				{
-					this.percent = -1;
-					ResourceBundle bundle = ResourceBundle.getBundle(
-							"wilos.resources.messages", FacesContext.getCurrentInstance()
-									.getApplication().getDefaultLocale());
-					FacesMessage message = new FacesMessage();
-					message
-							.setSummary(bundle
-									.getString("XmlFileImportBean.processNull"));
-					message.setSeverity(FacesMessage.SEVERITY_INFO);
-					FacesContext facesContext = FacesContext.getCurrentInstance();
-					facesContext.addMessage(null, message);
-				}
-			} catch (Exception e) {
-				logger.error("### XmlFileImportBean ### action -> " + e);
-			}
 			}
 			state = PersistentFacesState.getInstance();
-			
+
 		}
 	}
 
 	public void progressListener(EventObject event) {
-			// start the progress bar
-			this.percent = 1;
-			try {
-				if (state != null) {
-					state.render();
-				} else {
-					System.out.println("state is null");
-				}
-	
-			} catch (RenderingException ee) {
-				System.out.println(ee.getMessage());
-			}	
+		// start the progress bar
+		this.percent = 1;
+		try {
+			if (state != null) {
+				state.render();
+			} else {
+				System.out.println("state is null");
+			}
+
+		} catch (RenderingException ee) {
+			System.out.println(ee.getMessage());
+		}
 	}
 
 	public void setFileName(String fileName) {
@@ -345,7 +373,8 @@ public class XmlFileImportBean {
 	}
 
 	/**
-	 * @param _webSessionService the webSessionService to set
+	 * @param _webSessionService
+	 *            the webSessionService to set
 	 */
 	public void setWebSessionService(WebSessionService _webSessionService) {
 		this.webSessionService = _webSessionService;
